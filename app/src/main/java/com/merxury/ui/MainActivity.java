7efe2e46d05680c98178982a9d592a68792a520e
Mercury <350699171@qq.com>
2017-12-31 21:07:33 +0800

    Added method to get 3rd party app list and sys app list

--- app/src/main/java/com/merxury/ui/MainActivity.java
+++ app/src/main/java/com/merxury/ui/MainActivity.java
@@ -2,16 +2,15 @@ package com.merxury.ui;
 
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
 import android.content.pm.ServiceInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.merxury.blocker.R;
 import com.merxury.function.ApplicationComponents;
+import com.stericson.RootTools.RootTools;
 
 import java.util.List;
 
@@ -28,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         List<PackageInfo> info = ApplicationComponents.getApplicationList(this);
+        List<PackageInfo> thirdPartyApplicationList = ApplicationComponents.getThirdPartyApplicationList(this);
+        List<PackageInfo> systemApplicationList = ApplicationComponents.getSystemApplicationList(this);
+
+
+        for(PackageInfo info1: info) {
+            String packageName = info1.packageName;
+            ActivityInfo[] activities = ApplicationComponents.getActivitiyList(this, packageName);
+            ActivityInfo[] receiver = ApplicationComponents.getReceiverList(this, packageName);
+            ProviderInfo[] providers = ApplicationComponents.getProviderList(this, packageName);
+            ServiceInfo[] services = ApplicationComponents.getServiceList(this, packageName);
+        }
     }
 
 }
