603d6212513a8859ce3ff1bd525133a8944ff6b6
Mercury <350699171@qq.com>
2018-01-01 15:57:53 +0800

    finished component control class

--- app/src/main/java/com/merxury/function/root/ComponentController.java
+++ app/src/main/java/com/merxury/function/root/ComponentController.java
@@ -2,13 +2,15 @@ package com.merxury.function.root;
 
 import android.util.Log;
 
-import com.stericson.RootShell.exceptions.RootDeniedException;
+import com.merxury.exception.ProcessUnexpectedTerminateException;
 import com.stericson.RootShell.execution.Command;
 import com.stericson.RootShell.execution.Shell;
 import com.stericson.RootTools.RootTools;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Function;
 
 /**
  * Created by Mercury on 2017/12/31.
@@ -19,97 +21,74 @@ public class ComponentController {
     private static final String TAG = "ComponentController";
     private static final String DISABLE_COMPONENT_TEMPLATE = "pm disable %s/.%s";
     private static final String ENABLE_COMPONENT_TEMPLATE = "pm enable %s/.%s";
-    private static final String FAILED_EXCEPTION = "java.lang.IllegalArgumentException";
+    private static final String FAILED_EXCEPTION_MSG = "java.lang.IllegalArgumentException";
 
     private Shell shell;
 
-    /**
-     * a method to disable components
-     *
-     * @param packageName package name
-     * @param componentName component name
-     * @return true : disabled component successfully
-     *          false: cannot disable component
-     */
-    public boolean disableComponent(String packageName, String componentName) {
-        String comm = String.format(DISABLE_COMPONENT_TEMPLATE, packageName, componentName);
-        boolean success;
-        Command command = new Command(0, comm){
-            boolean success;
-            @Override
-            public void commandOutput(int id, String line) {
-                //TODO get output
-                Log.d(TAG, "commandOutput");
-                Log.d(TAG, line);
-                if(line.contains(FAILED_EXCEPTION)) {
-                    this.success = false;
-                } else {
-                    this.success = true;
-                }
-                super.commandOutput(id, line);
-            }
-
-            @Override
-            public void commandTerminated(int id, String reason) {
-                Log.d(TAG, "commandTerminated");
-                this.success = false;
-                synchronized (ComponentController.this) {
-                    ComponentController.this.notify();
-                }
-                super.commandTerminated(id, reason);
-            }
-
-            @Override
-            public void commandCompleted(int id, int exitcode) {
-                Log.d(TAG, "commandCompleted");
-                synchronized (ComponentController.this) {
-                    ComponentController.this.notify();
-                }
-                super.commandCompleted(id, exitcode);
-            }
-        };
-
-        try {
-            RootTools.getShell(true).add(command);
-            synchronized (this){
-                wait();
-            }
-        }catch (RootDeniedException | TimeoutException | InterruptedException |IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return false;
-    }
-
-    public boolean enableComponent(String packageName, String componentName) {
-        String comm = String.format(DISABLE_COMPONENT_TEMPLATE, packageName, componentName);
-        Command command = new Command(0, comm);
-        try {
-            shell.add(command);
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e(TAG, e.getMessage());
-        }
-        return false;
-    }
-
-    private boolean
-
-    private static class ComponentControllerHolder {
-        private static final ComponentController INSTANCE = new ComponentController();
-    }
-
     private ComponentController() {
         RootTools.debugMode = true;
-        try {
-            shell = RootTools.getShell(true);
-        } catch (RootDeniedException | TimeoutException | IOException e) {
-            e.printStackTrace();
-        }
     }
-
 
     public static ComponentController getInstance() {
         return ComponentControllerHolder.INSTANCE;
+    }
+
+    /**
+     * a method to change a component's state
+     *
+     * @param packageName   package name
+     * @param componentName component name
+     * @param state         true: enable component
+     *                      false: disable component
+     * @return true : changed component state successfully
+     * false: cannot disable component
+     */
+    public boolean switchComponent(String packageName, String componentName, boolean state) {
+        final String comm;
+        if (state) {
+            comm = String.format(ENABLE_COMPONENT_TEMPLATE, packageName, componentName);
+        } else {
+            comm = String.format(DISABLE_COMPONENT_TEMPLATE, packageName, componentName);
+        }
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
+                Command command = new Command(0, comm) {
+                    @Override
+                    public void commandOutput(int id, String line) {
+                        //TODO get output
+                        Log.d(TAG, "commandOutput");
+                        Log.d(TAG, line);
+                        emitter.onNext(line);
+                        super.commandOutput(id, line);
+                    }
+
+                    @Override
+                    public void commandTerminated(int id, String reason) {
+                        String msg = "commandTerminated";
+                        Log.d(TAG, msg);
+                        emitter.onError(new ProcessUnexpectedTerminateException(msg));
+                        super.commandTerminated(id, reason);
+                    }
+
+                    @Override
+                    public void commandCompleted(int id, int exitcode) {
+                        Log.d(TAG, "commandCompleted");
+                        emitter.onComplete();
+                        super.commandCompleted(id, exitcode);
+                    }
+                };
+                RootTools.getShell(true).add(command);
+            }
+        }).map(new Function<String, Boolean>() {
+            @Override
+            public Boolean apply(String s) throws Exception {
+                return !s.contains(FAILED_EXCEPTION_MSG);
+            }
+        }).blockingFirst();
+    }
+
+    private static class ComponentControllerHolder {
+        private static final ComponentController INSTANCE = new ComponentController();
     }
 }
