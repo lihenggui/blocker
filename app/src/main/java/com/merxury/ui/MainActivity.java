d64671d00d871e9ec9f395375f755a272ab036f0
Mercury <350699171@qq.com>
2018-03-11 11:35:31 +0800

    finish controller template code cleanup add progressbar for loading fragments

--- app/src/main/java/com/merxury/ui/MainActivity.java
+++ app/src/main/java/com/merxury/ui/MainActivity.java
@@ -2,32 +2,21 @@ package com.merxury.ui;
 
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
-import android.app.Activity;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.ShareCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 import com.merxury.adapter.FragmentAdapter;
 import com.merxury.blocker.R;
 import com.merxury.fragment.AppListFragment;
-import com.merxury.service.ShellService;
-import com.merxury.utils.BindServiceHelper;
-import com.merxury.utils.ContextUtils;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 
@@ -35,17 +24,13 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String PACKAGE_NAME = "package_name";
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
     @BindView(R.id.app_viewpager)
     ViewPager mViewPager;
     @BindView(R.id.app_kind_tabs)
     TabLayout mTabLayout;
-
-    private AlertDialog mAlertDialog;
-    private BindServiceHelper mBindServiceHelper;
-
-    public static final String PACKAGE_NAME = "package_name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -148,9 +133,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private int getColorForTab(int position) {
-        if (position == 0) return ContextCompat.getColor(this, R.color.md_blue_700);
-        else if (position == 1) return ContextCompat.getColor(this, R.color.md_red_700);
-        else return ContextCompat.getColor(this, R.color.md_grey_700);
+        if (position == 0) {
+            return ContextCompat.getColor(this, R.color.md_blue_700);
+        } else if (position == 1) {
+            return ContextCompat.getColor(this, R.color.md_red_700);
+        } else {
+            return ContextCompat.getColor(this, R.color.md_grey_700);
+        }
     }
 
     private void changeColor(int color) {
@@ -159,91 +148,4 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setStatusBarColor(color);
     }
 
-    private void startShell(final Context context, final String... command) {
-        final StringBuilder sb = new StringBuilder();
-        mAlertDialog = new AlertDialog.Builder(context)
-                .setView(R.layout.dialog_shell)
-                .setCancelable(false)
-                .setPositiveButton(android.R.string.ok, null)
-                .setNeutralButton(R.string.send_command, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Activity activity = ContextUtils.getActivity(context);
-                        if (activity == null) {
-                            return;
-                        }
-
-                        ShareCompat.IntentBuilder.from(activity)
-                                .setText(sb.toString())
-                                .setType("text/plain")
-                                .setChooserTitle(R.string.send_command)
-                                .startChooser();
-                    }
-                })
-                .show();
-
-        mAlertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
-        mAlertDialog.getButton(AlertDialog.BUTTON_NEUTRAL).setVisibility(View.GONE);
-
-        final TextView textView = mAlertDialog.findViewById(android.R.id.text1);
-        if (textView == null) {
-            return;
-        }
-
-        textView.setText(R.string.starting_shell);
-        mBindServiceHelper.bind(new BindServiceHelper.OnServiceConnectedListener() {
-            @Override
-            public void onServiceConnected(IBinder binder) {
-                ShellService.ShellServiceBinder service = (ShellService.ShellServiceBinder) binder;
-
-                service.run(command, 0, new ShellService.Listener() {
-                    @Override
-                    public void onFailed() {
-                        mBindServiceHelper.unbind();
-
-                        if (mAlertDialog == null) {
-                            return;
-                        }
-                        mAlertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
-                        textView.setText(R.string.cannot_start_no_root);
-                    }
-
-                    public void onCommandResult(int commandCode, int exitCode) {
-                        mBindServiceHelper.unbind();
-
-                        if (mAlertDialog == null) {
-                            return;
-                        }
-
-                        mAlertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
-
-                        if (exitCode != 0) {
-                            sb.append('\n').append("Send this to developer may help solve the problem.");
-                            mAlertDialog.getButton(AlertDialog.BUTTON_NEUTRAL).setVisibility(View.VISIBLE);
-
-                        }
-                    }
-
-                    public void onLine(String line) {
-                        if (mAlertDialog == null) {
-                            return;
-                        }
-
-                        if (sb.length() > 0) {
-                            sb.append('\n');
-                        }
-
-                        sb.append(line);
-
-                        textView.setText(sb.toString());
-                    }
-                });
-            }
-        });
-    }
-
-    private void startServer(Context context) {
-
-        // startShell(context, ServerLauncher.COMMAND_ROOT);
-    }
 }
