4b1b8ba01c75486a6cea84e3db447895a60dca95
Mercury <350699171@qq.com>
2018-03-27 17:26:03 +0800

    add error handling to prevent crash while clicking the switch button

--- app/src/main/java/com/merxury/ui/component/ComponentPresenter.kt
+++ app/src/main/java/com/merxury/ui/component/ComponentPresenter.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.ComponentInfo
 import android.content.pm.PackageManager
-import android.util.Log
 import com.merxury.core.ApplicationComponents
 import com.merxury.core.IController
 import com.merxury.core.root.ComponentControllerProxy
@@ -12,6 +11,7 @@ import com.merxury.core.root.EControllerMethod
 import io.reactivex.Single
 import io.reactivex.SingleOnSubscribe
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiConsumer
 import io.reactivex.schedulers.Schedulers
 
 class ComponentPresenter(val pm: PackageManager, val view: ComponentContract.View) : ComponentContract.Presenter, IController {
@@ -47,32 +47,59 @@ class ComponentPresenter(val pm: PackageManager, val view: ComponentContract.Vie
                 })
     }
 
+    @SuppressLint("CheckResult")
     override fun switchComponent(packageName: String, componentName: String, state: Int): Boolean {
         Single.create((SingleOnSubscribe<Boolean> { emitter ->
-            emitter.onSuccess(controller.switchComponent(packageName, componentName, state))
+            try {
+                val result = controller.switchComponent(packageName, componentName, state)
+                emitter.onSuccess(result)
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
         })).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({}, { error: Throwable? ->
-                    Log.e(TAG, error?.message);
+                .subscribe(BiConsumer { result, error ->
+                    if (result) {
+
+                    } else {
+
+                    }
+                    error?.printStackTrace()
                 })
         return true
     }
 
+    @SuppressLint("CheckResult")
     override fun enableComponent(componentInfo: ComponentInfo): Boolean {
         Single.create((SingleOnSubscribe<Boolean> { emitter ->
-            emitter.onSuccess(controller.enableComponent(componentInfo))
+            try {
+                val result = controller.enableComponent(componentInfo)
+                emitter.onSuccess(result)
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
         })).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe()
+                .subscribe(BiConsumer { result, error ->
+                    error?.printStackTrace()
+                })
         return true
     }
 
+    @SuppressLint("CheckResult")
     override fun disableComponent(componentInfo: ComponentInfo): Boolean {
         Single.create((SingleOnSubscribe<Boolean> { emitter ->
-            emitter.onSuccess(controller.disableComponent(componentInfo))
+            try {
+                val result = controller.disableComponent(componentInfo)
+                emitter.onSuccess(result)
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
         })).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe()
+                .subscribe(BiConsumer { result, error ->
+                    error?.printStackTrace()
+                })
         return true
     }
 
