9f65e9868a992246512dff8388ef41f93715ced1
Mercury <350699171@qq.com>
2018-03-29 15:07:38 +0800

    update comparator type sort componentinfo by name default

--- app/src/main/java/com/merxury/ui/component/ComponentContract.kt
+++ app/src/main/java/com/merxury/ui/component/ComponentContract.kt
@@ -1,7 +1,6 @@
 package com.merxury.ui.component
 
 import android.content.pm.ComponentInfo
-import android.content.pm.PackageManager
 import com.merxury.core.IController
 import com.merxury.ui.base.BasePresenter
 import com.merxury.ui.base.BaseView
@@ -22,7 +21,7 @@ interface ComponentContract {
 
     interface Presenter : BasePresenter, IController {
         var currentComparator: EComponentComparatorType
-        fun loadComponents(pm: PackageManager, packageName: String, type: EComponentType)
+        fun loadComponents(packageName: String, type: EComponentType)
         fun sortComponentList(components: List<ComponentInfo>, type: EComponentComparatorType): List<ComponentInfo>
     }
 }
--- app/src/main/java/com/merxury/ui/component/EComponentComparatorType.kt
+++ app/src/main/java/com/merxury/ui/component/EComponentComparatorType.kt
@@ -2,5 +2,7 @@ package com.merxury.ui.component
 
 enum class EComponentComparatorType {
     NAME_ASCENDING,
-    NAME_DESCENDING
+    NAME_DESCENDING,
+    PACKAGE_NAME_ASCENDING,
+    PACKAGE_NAME_DESCENDING
 }
