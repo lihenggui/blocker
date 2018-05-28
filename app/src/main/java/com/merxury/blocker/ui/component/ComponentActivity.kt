c29583c03ef360f7f57e392021a8402d150bbb4c
Mercury <350699171@qq.com>
2018-05-28 21:45:57 +0800

    added version description for minsdkversion and targetsdkversion

--- app/src/main/java/com/merxury/blocker/ui/component/ComponentActivity.kt
+++ app/src/main/java/com/merxury/blocker/ui/component/ComponentActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.SparseArray
 import android.view.MenuItem
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
@@ -86,8 +87,8 @@ class ComponentActivity : AppCompatActivity(), IActivityView {
     private fun showApplicationBriefInfo(application: Application) {
         app_info_app_name.text = getString(R.string.application_label, application.label)
         app_info_app_package_name.text = getString(R.string.package_name, application.packageName)
-        app_info_target_sdk_version.text = getString(R.string.target_sdk_version, application.targetSdkVersion)
-        app_info_min_sdk_version.text = getString(R.string.min_sdk_version, application.minSdkVersion)
+        app_info_target_sdk_version.text = getString(R.string.target_sdk_version, CODENAME.get(application.targetSdkVersion, UNKNOWN))
+        app_info_min_sdk_version.text = getString(R.string.min_sdk_version, CODENAME.get(application.minSdkVersion, UNKNOWN))
         Glide.with(this)
                 .load(application.getApplicationIcon(packageManager))
                 .transition(DrawableTransitionOptions().crossFade())
@@ -130,4 +131,39 @@ class ComponentActivity : AppCompatActivity(), IActivityView {
         }
     }
 
+    companion object {
+        private val CODENAME: SparseArray<String> = SparseArray(32)
+        private const val UNKNOWN = "Unknown"
+
+        init {
+            CODENAME.put(1, "Android 1.0")
+            CODENAME.put(2, "Android 1.1")
+            CODENAME.put(3, "Android 1.5 (Cupcake)")
+            CODENAME.put(4, "Android 1.6 (Donut)")
+            CODENAME.put(5, "Android 2.0 (Eclair)")
+            CODENAME.put(6, "Android 2.0.1 (Eclair)")
+            CODENAME.put(7, "Android 2.1 (Eclair)")
+            CODENAME.put(8, "Android 2.2 (Froyo)")
+            CODENAME.put(9, "Android 2.3 (Gingerbread)")
+            CODENAME.put(10, "Android 2.3.3 (Gingerbread)")
+            CODENAME.put(11, "Android 3.0 (Honeycomb)")
+            CODENAME.put(12, "Android 3.1 (Honeycomb)")
+            CODENAME.put(13, "Android 3.2 (Honeycomb)")
+            CODENAME.put(14, "Android 4.0.1 (ICS)")
+            CODENAME.put(15, "Android 4.0.3 (ICS)")
+            CODENAME.put(16, "Android 4.1 (Jelly Bean)")
+            CODENAME.put(17, "Android 4.2 (Jelly Bean)")
+            CODENAME.put(18, "Android 4.3 (Jelly Bean)")
+            CODENAME.put(19, "Android 4.4 (KitKat)")
+            CODENAME.put(21, "Android 5.0 (Lollipop)")
+            CODENAME.put(22, "Android 5.1 (Lollipop)")
+            CODENAME.put(23, "Android 6.0 (Marshmallow)")
+            CODENAME.put(24, "Android 7.0 (Nougat)")
+            CODENAME.put(25, "Android 7.1 (Nougat)")
+            CODENAME.put(26, "Android 8.0 (Oreo)")
+            CODENAME.put(27, "Android 8.1 (Oreo)")
+            CODENAME.put(28, "Android 9.0 (P)")
+            // Reference : https://source.android.com/setup/start/build-numbers
+        }
+    }
 }
