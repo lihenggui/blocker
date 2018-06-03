e73c45c56dbe385edf4664d8221b0d3f71be7012
Merxury <350699171@qq.com>
2018-06-03 20:10:16 +0800

    added support for 4.4

--- app/src/main/java/com/merxury/blocker/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/merxury/blocker/ui/settings/SettingsActivity.kt
@@ -11,16 +11,6 @@ import android.preference.*
 import android.view.MenuItem
 import com.merxury.blocker.R
 
-/**
- * A [PreferenceActivity] that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- *
- * See [Android Design: Settings](http://developer.android.com/design/patterns/settings.html)
- * for design guidelines and the [Settings API Guide](http://developer.android.com/guide/topics/ui/settings.html)
- * for more information on developing a Settings UI.
- */
 class SettingsActivity : AppCompatPreferenceActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
