96c0a15ccc052cb4ae614aaebb173c6ee0f65662
Mercury <350699171@qq.com>
2018-06-06 22:26:28 +0800

    added new preferences settings

--- app/src/main/java/com/merxury/blocker/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/merxury/blocker/ui/settings/SettingsActivity.kt
@@ -1,46 +1,13 @@
 package com.merxury.blocker.ui.settings
 
-import android.annotation.TargetApi
-import android.content.Context
-import android.content.res.Configuration
-import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceActivity
-import android.preference.PreferenceFragment
 import com.merxury.blocker.R
-import com.merxury.blocker.ui.settings.about.AboutFragment
-import com.merxury.blocker.ui.settings.general.GeneralPreferenceFragment
 
-class SettingsActivity : AppCompatPreferenceActivity() {
+class SettingsActivity : PreferenceActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupActionBar()
-    }
-
-    private fun setupActionBar() {
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    override fun onIsMultiPane(): Boolean {
-        return isXLargeTablet(this)
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    override fun onBuildHeaders(target: List<PreferenceActivity.Header>) {
-        loadHeadersFromResource(R.xml.pref_headers, target)
-    }
-
-    override fun isValidFragment(fragmentName: String): Boolean {
-        return PreferenceFragment::class.java.name == fragmentName
-                || GeneralPreferenceFragment::class.java.name == fragmentName
-                || AboutFragment::class.java.name == fragmentName
-    }
-
-
-    companion object {
-        private fun isXLargeTablet(context: Context): Boolean {
-            return context.resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK >= Configuration.SCREENLAYOUT_SIZE_XLARGE
-        }
+        addPreferencesFromResource(R.xml.preferences)
     }
 }
