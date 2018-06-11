bfe8e7b0fe9862673dde6919d6e0666404f9dae6
Merxury <350699171@qq.com>
2018-06-11 22:29:16 +0800

    update app version & add field for entity

--- app/src/main/java/com/merxury/blocker/ui/settings/entity/BlockerRule.kt
+++ app/src/main/java/com/merxury/blocker/ui/settings/entity/BlockerRule.kt
@@ -2,6 +2,7 @@ package com.merxury.blocker.ui.settings.entity
 
 data class BlockerRule(
         var packageName: String = "",
+        var versionName: String = "",
         var versionCode: Int = -1,
         var components: List<ComponentRule> = ArrayList()
 )
