fa49b0e3e27da9c5dd063e3ac379414329b94409
Merxury <350699171@qq.com>
2018-06-14 11:43:22 +0800

    finished import rules function

--- app/src/main/java/com/merxury/blocker/ui/settings/SettingsPresenter.kt
+++ app/src/main/java/com/merxury/blocker/ui/settings/SettingsPresenter.kt
@@ -1,16 +1,25 @@
 package com.merxury.blocker.ui.settings
 
-import android.Manifest
-import android.app.Activity
 import android.content.Context
+import android.util.Log
 import com.google.gson.Gson
+import com.google.gson.JsonIOException
+import com.google.gson.JsonSyntaxException
+import com.google.gson.stream.JsonReader
 import com.merxury.blocker.core.ComponentControllerProxy
 import com.merxury.blocker.core.IController
+import com.merxury.blocker.core.root.EControllerMethod
+import com.merxury.blocker.ui.component.EComponentType
+import com.merxury.blocker.ui.settings.entity.BlockerRule
 import com.merxury.blocker.util.PreferenceUtil
-import com.tbruyelle.rxpermissions2.RxPermissions
+import com.merxury.ifw.IntentFirewall
+import com.merxury.ifw.IntentFirewallImpl
+import com.merxury.ifw.entity.ComponentType
 import io.reactivex.Observable
 import io.reactivex.ObservableOnSubscribe
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileReader
 
 class SettingsPresenter(private val settingsView: SettingsContract.SettingsView) : SettingsContract.SettingsPresenter {
 
@@ -22,19 +31,56 @@ class SettingsPresenter(private val settingsView: SettingsContract.SettingsView)
     }
 
     override fun exportAllRules(folder: String) {
-        RxPermissions(context as Activity)
-                .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .subscribe({ granted ->
-                    if (granted) {
-                        exportBlockerRule(folder)
-                    } else {
 
-                    }
-                })
     }
 
     override fun importAllRules(folder: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val destFolder = File(folder)
+        if (!destFolder.exists()) {
+            destFolder.mkdirs()
+        }
+        val rules = destFolder.listFiles { dir -> dir.extension == "json" }
+        var successCount = 0
+        var failCount = 0
+        Observable.create(ObservableOnSubscribe<Unit> { emitter ->
+            val gson = Gson()
+            rules.forEach {
+                try {
+                    val jsonReader = JsonReader(FileReader(it))
+                    val appRule = gson.fromJson<BlockerRule>(jsonReader, BlockerRule::class.java)
+                            ?: return@forEach
+                    var ifwController: IntentFirewall? = null
+                    // Detects if contains IFW rules, if exists, create a new one.
+                    appRule.components.forEach ifwDetection@{
+                        if (it.method == EControllerMethod.IFW) {
+                            ifwController = IntentFirewallImpl.getInstance(context!!, appRule.packageName)
+                            return@ifwDetection
+                        }
+                    }
+                    appRule.components.forEach {
+                        when (it.method) {
+                            EControllerMethod.IFW -> {
+                                when (it.type) {
+                                    EComponentType.RECEIVER -> ifwController?.add(it.packageName, it.name, ComponentType.BROADCAST)
+                                    EComponentType.SERVICE -> ifwController?.add(it.packageName, it.name, ComponentType.SERVICE)
+                                    EComponentType.ACTIVITY -> ifwController?.add(it.packageName, it.name, ComponentType.ACTIVITY)
+                                    else -> controller.disable(it.packageName, it.name)
+                                }
+                            }
+                            else -> controller.disable(it.packageName, it.name)
+                        }
+                    }
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                    Log.e(TAG, ex.message)
+                    when (ex) {
+                        is FileNotFoundException, is JsonIOException, is JsonSyntaxException -> {
+                        }
+                    }
+                    emitter.onError(ex)
+                }
+            }
+        })
     }
 
     override fun exportAllIFWRules(folder: String) {
@@ -49,19 +95,6 @@ class SettingsPresenter(private val settingsView: SettingsContract.SettingsView)
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    private fun exportBlockerRule(folder: String) {
-        val destFolder = File(folder)
-        if (!destFolder.exists()) {
-            destFolder.mkdirs()
-        }
-        val rules = destFolder.listFiles { dir -> dir.extension == "json" }
-        var successCount = 0
-        var failCount = 0
-        Observable.create(ObservableOnSubscribe<Unit> { emitter ->
-            val gson = Gson()
-        })
-    }
-
     override fun start(context: Context) {
         this.context = context
     }
@@ -70,4 +103,7 @@ class SettingsPresenter(private val settingsView: SettingsContract.SettingsView)
         this.context = null
     }
 
+    companion object {
+        const val TAG = "SettingsPresenter"
+    }
 }
