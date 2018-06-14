5a1652d78b71a750c00d6fdf23e1384722f84ec3
Mercury <350699171@qq.com>
2018-06-14 10:46:58 +0800

    add permission detection for presenter

--- app/src/main/java/com/merxury/blocker/ui/settings/SettingsPresenter.kt
+++ app/src/main/java/com/merxury/blocker/ui/settings/SettingsPresenter.kt
@@ -1,11 +1,18 @@
 package com.merxury.blocker.ui.settings
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
+import com.google.gson.Gson
 import com.merxury.blocker.core.ComponentControllerProxy
 import com.merxury.blocker.core.IController
 import com.merxury.blocker.util.PreferenceUtil
+import com.tbruyelle.rxpermissions2.RxPermissions
+import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
+import java.io.File
 
-class SettingsPresenter : SettingsContract.SettingsPresenter {
+class SettingsPresenter(private val settingsView: SettingsContract.SettingsView) : SettingsContract.SettingsPresenter {
 
     private var context: Context? = null
 
@@ -15,7 +22,15 @@ class SettingsPresenter : SettingsContract.SettingsPresenter {
     }
 
     override fun exportAllRules(folder: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        RxPermissions(context as Activity)
+                .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe({ granted ->
+                    if (granted) {
+                        exportBlockerRule(folder)
+                    } else {
+
+                    }
+                })
     }
 
     override fun importAllRules(folder: String) {
@@ -32,6 +47,19 @@ class SettingsPresenter : SettingsContract.SettingsPresenter {
 
     override fun resetIFW() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun exportBlockerRule(folder: String) {
+        val destFolder = File(folder)
+        if (!destFolder.exists()) {
+            destFolder.mkdirs()
+        }
+        val rules = destFolder.listFiles { dir -> dir.extension == "json" }
+        var successCount = 0
+        var failCount = 0
+        Observable.create(ObservableOnSubscribe<Unit> { emitter ->
+            val gson = Gson()
+        })
     }
 
     override fun start(context: Context) {
