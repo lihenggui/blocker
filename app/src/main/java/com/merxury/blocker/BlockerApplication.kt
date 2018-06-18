5b632022d3e17b74a38f107107ab2693649282a0
Merxury <350699171@qq.com>
2018-06-18 23:34:49 +0800

    added notification channel

--- app/src/main/java/com/merxury/blocker/BlockerApplication.kt
+++ app/src/main/java/com/merxury/blocker/BlockerApplication.kt
@@ -1,15 +1,31 @@
 package com.merxury.blocker
 
 import android.annotation.SuppressLint
+import android.annotation.TargetApi
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import com.merxury.blocker.core.shizuku.ShizukuClientWrapper
 
 class BlockerApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        ShizukuClientWrapper.initialize(this)
         context = this
+        ShizukuClientWrapper.initialize(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "processing_progress_indicator"
+            val channelName = context.getString(R.string.processing_progress_indicator)
+            createNotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String, importance: Int) {
+        val channel = NotificationChannel(channelId, channelName, importance)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 
     companion object {
