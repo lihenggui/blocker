dc849717e511cde100c5892d818b041cb5b6f5b7
Mercury <350699171@qq.com>
2018-07-08 17:10:24 +0800

    sync code

--- app/src/main/java/com/merxury/blocker/base/BasePresenter.kt
+++ app/src/main/java/com/merxury/blocker/base/BasePresenter.kt
@@ -3,6 +3,8 @@ package com.merxury.blocker.base
 import android.content.Context
 
 interface BasePresenter {
+    fun requestPermissions()
+    fun onPermissionsResult()
     fun start(context: Context)
     fun destroy()
 }
--- app/src/main/java/com/merxury/blocker/base/BaseView.kt
+++ app/src/main/java/com/merxury/blocker/base/BaseView.kt
@@ -1,5 +1,8 @@
 package com.merxury.blocker.base
 
+import android.app.Activity
+
 interface BaseView<T> {
     var presenter: T
+    fun getViewActivity(): Activity
 }
