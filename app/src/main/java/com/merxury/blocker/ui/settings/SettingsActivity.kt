16d33ba4594603001d089eaba12cfc2c99b22b9a
Merxury <350699171@qq.com>
2018-10-07 19:08:41 +0800

    Migrate to preference support library

--- app/src/main/java/com/merxury/blocker/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/merxury/blocker/ui/settings/SettingsActivity.kt
@@ -1,13 +1,28 @@
 package com.merxury.blocker.ui.settings
 
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.merxury.blocker.R
+import com.merxury.blocker.util.setupActionBar
 
-class SettingsActivity : AppCompatPreferenceActivity() {
+class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fragmentManager.beginTransaction()
-                .replace(android.R.id.content, PreferenceFragment())
-                .commit()
+        setContentView(R.layout.activity_settings)
+        setupActionBar(R.id.toolbar) {
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
