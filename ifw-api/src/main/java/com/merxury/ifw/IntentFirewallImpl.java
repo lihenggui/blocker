e50e7ca5860bddd11a7cd6d49a6ac5e7a52d4c2b
Merxury <350699171@qq.com>
2018-10-09 21:02:40 +0800

    Bug fix: delete file if there is no rule presented

--- ifw-api/src/main/java/com/merxury/ifw/IntentFirewallImpl.java
+++ ifw-api/src/main/java/com/merxury/ifw/IntentFirewallImpl.java
@@ -63,10 +63,12 @@ public class IntentFirewallImpl implements IntentFirewall {
     public void save() throws Exception {
         ensureNoEmptyTag();
         if (rules.getActivity() == null && rules.getBroadcast() == null && rules.getService() == null) {
+            // If there is no rules presented, delete rule file (if exists)
+            FileUtils.delete(destPath, true);
             return;
         }
-        Serializer serializer = new Persister();
         File file = new File(tmpPath);
+        Serializer serializer = new Persister();
         if (file.exists()) {
             if (!file.delete()) {
                 throw new IOException("Cannot delete file: " + tmpPath);
