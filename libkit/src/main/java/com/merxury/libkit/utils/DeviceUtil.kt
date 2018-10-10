02ec47ac8d8c1258658c65549c1f9163c81d479c
Merxury <350699171@qq.com>
2018-10-10 22:27:05 +0800

    Add Doze related commands

---
+++ libkit/src/main/java/com/merxury/libkit/utils/DeviceUtil.kt
@@ -0,0 +1,18 @@
+package com.merxury.libkit.utils
+
+import com.merxury.libkit.RootCommand
+
+object DeviceUtil {
+    fun enterLowPowerConsumptionMode() {
+        RootCommand.runBlockingCommand("dumpsys battery unplug")
+        RootCommand.runBlockingCommand("dumpsys deviceidle step")
+    }
+
+    fun forceDoze() {
+        RootCommand.runBlockingCommand("dumpsys deviceidle force-idle")
+    }
+
+    fun exitDoze() {
+        RootCommand.runBlockingCommand("dumpsys deviceidle disable")
+    }
+}
