1339bc38267c56b07ed2539bb788feb8cf2d1366
Merxury <350699171@qq.com>
2018-10-21 20:38:50 +0800

    Add detection for screen state

--- libkit/src/main/java/com/merxury/libkit/utils/DeviceUtil.kt
+++ libkit/src/main/java/com/merxury/libkit/utils/DeviceUtil.kt
@@ -1,6 +1,12 @@
 package com.merxury.libkit.utils
 
+import android.content.Context
+import android.hardware.display.DisplayManager
+import android.os.Build
+import android.os.PowerManager
+import android.view.Display
 import com.merxury.libkit.RootCommand
+
 
 object DeviceUtil {
     fun enterLowPowerConsumptionMode() {
@@ -14,5 +20,22 @@ object DeviceUtil {
 
     fun exitDoze() {
         RootCommand.runBlockingCommand("shell input keyevent KEYCODE_WAKEUP")
+    }
+
+    @SuppressWarnings("deprecation")
+    fun isScreenOn(context: Context): Boolean {
+        return if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+            val dm = context.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+            var screenOn = false
+            for (display in dm.displays) {
+                if (display.state != Display.STATE_OFF) {
+                    screenOn = true
+                }
+            }
+            screenOn
+        } else {
+            val pm = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+            pm.isScreenOn
+        }
     }
 }
