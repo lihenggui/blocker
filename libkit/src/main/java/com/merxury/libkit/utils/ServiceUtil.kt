baccc562613410206503641fee14fe2d3b123eb1
Merxury <350699171@qq.com>
2018-11-07 22:29:14 +0800

    Refine service detection

--- libkit/src/main/java/com/merxury/libkit/utils/ServiceUtil.kt
+++ libkit/src/main/java/com/merxury/libkit/utils/ServiceUtil.kt
@@ -3,8 +3,21 @@ package com.merxury.libkit.utils
 import com.merxury.libkit.RootCommand
 
 object ServiceUtil {
+    private var packageName = ""
+    private var serviceList = ""
     fun isServiceRunning(packageName: String, serviceName: String): Boolean {
-        val output = RootCommand.runBlockingCommand("dumpsys activity services $packageName/$serviceName")
-        return output.contains("pid=")
+        if (this.packageName != packageName) {
+            getRunningServiceList(packageName)
+        }
+        val regex = SERVICE_REGEX.format(packageName, serviceName).toRegex()
+        return serviceList.contains(regex)
     }
+
+    private fun getRunningServiceList(packageName: String) {
+        this.packageName = packageName
+        serviceList = RootCommand.runBlockingCommand("dumpsys activity services $packageName")
+    }
+
+    private const val SERVICE_REGEX = """ServiceRecord\{(.*?) %s\/%s\}"""
+
 }
