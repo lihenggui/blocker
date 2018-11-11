8bf8c45fc88fcb286c69eff16fba5d030513b944
Merxury <350699171@qq.com>
2018-11-11 11:37:21 +0800

    Bug fix: If app=null, service will not be in running state

--- libkit/src/main/java/com/merxury/libkit/utils/ServiceHelper.kt
+++ libkit/src/main/java/com/merxury/libkit/utils/ServiceHelper.kt
@@ -3,7 +3,8 @@ package com.merxury.libkit.utils
 import com.merxury.libkit.RootCommand
 
 class ServiceHelper(private val packageName: String) {
-    private lateinit var serviceList: String
+    private var serviceInfo: String = ""
+    private val serviceList: MutableList<String> = mutableListOf()
 
     fun isServiceRunning(serviceName: String): Boolean {
         val shortName = if (serviceName.startsWith(packageName)) {
@@ -13,14 +14,34 @@ class ServiceHelper(private val packageName: String) {
         }
         val fullRegex = SERVICE_REGEX.format(packageName, serviceName).toRegex()
         val shortRegex = SERVICE_REGEX.format(packageName, shortName).toRegex()
-        return serviceList.contains(fullRegex) || serviceList.contains(shortRegex)
+        serviceList.forEach {
+            if (it.contains(fullRegex) || it.contains(shortRegex)) {
+                if (it.contains("app=ProcessRecord{")) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     fun refresh() {
-        serviceList = RootCommand.runBlockingCommand("dumpsys activity services -p $packageName")
+        serviceList.clear()
+        serviceInfo = RootCommand.runBlockingCommand("dumpsys activity services -p $packageName")
+        parseServiceInfo()
+    }
+
+    private fun parseServiceInfo() {
+        if (serviceInfo.contains("(nothing)")) {
+            return
+        }
+        val list = serviceInfo.split("\n[\n]+".toRegex()).toMutableList()
+        if (list.lastOrNull()?.contains("Connection bindings to services") == true) {
+            list.removeAt(list.size - 1)
+        }
+        serviceList.addAll(list)
     }
 
     companion object {
-        private const val SERVICE_REGEX = """\s+?\*\sServiceRecord\{(.*?) %s\/%s\}"""
+        private const val SERVICE_REGEX = """ServiceRecord\{(.*?) %s\/%s\}"""
     }
 }
