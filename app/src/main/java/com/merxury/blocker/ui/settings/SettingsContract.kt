4b0e2c5cb12d49e857f7278847738869495ce6e3
Merxury <350699171@qq.com>
2018-12-15 10:09:22 +0800

    Use coroutine to rewrite settings presenter

--- app/src/main/java/com/merxury/blocker/ui/settings/SettingsContract.kt
+++ app/src/main/java/com/merxury/blocker/ui/settings/SettingsContract.kt
@@ -3,6 +3,7 @@ package com.merxury.blocker.ui.settings
 import androidx.annotation.StringRes
 import com.merxury.blocker.base.BasePresenter
 import com.merxury.blocker.base.BaseView
+import kotlinx.coroutines.Job
 
 interface SettingsContract : BaseView<SettingsContract.SettingsPresenter> {
     interface SettingsView {
@@ -15,11 +16,11 @@ interface SettingsContract : BaseView<SettingsContract.SettingsPresenter> {
     }
 
     interface SettingsPresenter : BasePresenter {
-        fun exportAllRules()
-        fun importAllRules()
-        fun exportAllIfwRules()
-        fun importAllIfwRules()
-        fun importMatRules(filePath: String?)
-        fun resetIFW()
+        fun exportAllRules(): Job
+        fun importAllRules(): Job
+        fun exportAllIfwRules(): Job
+        fun importAllIfwRules(): Job
+        fun importMatRules(filePath: String?): Job
+        fun resetIFW(): Job
     }
 }
