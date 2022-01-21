a28ad7010a3de9be03ddde37532f32973e03b1b9
Mercury <350699171@qq.com>
2022-01-21 23:06:51 +0800

    Remove unnecessary code

--- app/src/main/java/com/merxury/blocker/util/NotificationUtil.kt
+++ app/src/main/java/com/merxury/blocker/util/NotificationUtil.kt
@@ -17,44 +17,13 @@ object NotificationUtil {
         val channelId = PROCESSING_INDICATOR_CHANNEL_ID
         val channelName = context.getString(R.string.processing_progress_indicator)
         val channel =
-            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH).apply {
+            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW).apply {
                 setSound(null, null)
                 vibrationPattern = null
             }
         val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
         manager?.createNotificationChannel(channel)
     }
-
-
-//    fun createProcessingNotification(context: Context, total: Int) {
-//        builder = NotificationCompat.Builder(context, PROCESSING_INDICATOR_CHANNEL_ID)
-//                .setSmallIcon(R.mipmap.ic_launcher)
-//                .setContentTitle(context.getString(R.string.processing))
-//                .setPriority(NotificationCompat.PRIORITY_LOW)
-//                .setProgress(total, 0, true)
-//                .setOnlyAlertOnce(true)
-//                .setAutoCancel(true)
-//        val notificationManager = NotificationManagerCompat.from(context)
-//        notificationManager.notify(PROCESSING_NOTIFICATION_ID, builder.build())
-//    }
-
-//    fun finishProcessingNotification(context: Context, count: Int) {
-//        // {Count} components were set, {count} succeeded,{count} failed
-//        builder.setContentTitle(context.getString(R.string.done))
-//                .setContentText(context.getString(R.string.notification_done, count))
-//                .setSubText("Blocker")
-//                .setProgress(0, 0, false)
-//        val notificationManager = NotificationManagerCompat.from(context)
-//        notificationManager.notify(PROCESSING_NOTIFICATION_ID, builder.build())
-//    }
-
-//    fun updateProcessingNotification(context: Context, appLabel: String, current: Int, total: Int) {
-//        builder.setProgress(total, current, false)
-//                .setContentText(context.getString(R.string.processing_indicator, current, total))
-//                .setSubText(appLabel)
-//        val notificationManager = NotificationManagerCompat.from(context)
-//        notificationManager.notify(PROCESSING_NOTIFICATION_ID, builder.build())
-//    }
 
     fun cancelNotification(context: Context) {
         (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).cancel(
