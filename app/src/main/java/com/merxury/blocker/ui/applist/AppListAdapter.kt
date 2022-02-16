2579e040b2906044d8e607e4f69af64cfe75ac29
Mercury <350699171@qq.com>
2022-02-16 14:18:05 +0800

    Add running service info in the application list

--- app/src/main/java/com/merxury/blocker/ui/applist/AppListAdapter.kt
+++ app/src/main/java/com/merxury/blocker/ui/applist/AppListAdapter.kt
@@ -1,7 +1,9 @@
 package com.merxury.blocker.ui.applist
 
 import android.annotation.SuppressLint
+import android.content.ComponentName
 import android.content.Context
+import android.content.pm.PackageManager
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleCoroutineScope
@@ -10,10 +12,14 @@ import com.elvishew.xlog.XLog
 import com.merxury.blocker.R
 import com.merxury.blocker.databinding.AppListItemBinding
 import com.merxury.blocker.util.AppIconCache
+import com.merxury.ifw.IntentFirewallImpl
 import com.merxury.libkit.entity.Application
+import com.merxury.libkit.utils.ApplicationUtil
+import com.merxury.libkit.utils.ServiceHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AppListAdapter(val lifecycleScope: LifecycleCoroutineScope) :
     RecyclerView.Adapter<AppListAdapter.AppListViewHolder>() {
@@ -26,6 +32,7 @@ class AppListAdapter(val lifecycleScope: LifecycleCoroutineScope) :
     private var list: MutableList<Application> = mutableListOf()
     private var appState: MutableList<AppState> = mutableListOf()
     private var loadIconJob: Job? = null
+    private var loadServiceStatusJob: Job? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppListViewHolder {
         val context = parent.context
@@ -61,6 +68,9 @@ class AppListAdapter(val lifecycleScope: LifecycleCoroutineScope) :
         if (loadIconJob?.isActive == true) {
             loadIconJob?.cancel()
         }
+        if (loadServiceStatusJob?.isActive == true) {
+            loadServiceStatusJob?.cancel()
+        }
     }
 
     inner class AppListViewHolder(
@@ -89,6 +99,53 @@ class AppListAdapter(val lifecycleScope: LifecycleCoroutineScope) :
             }
             binding.appName.text = app.label
             binding.versionCode.text = app.versionName
+            binding.serviceStatus.text =
+                context.getString(R.string.acquiring_service_status_please_wait)
+            // Load service status
+            lifecycleScope.launch(Dispatchers.IO) {
+                loadServiceStatusJob = launch {
+                    // Find from adapter's cache first
+                    val cachedResult = appState.find { it.packageName == app.packageName }
+                    val result: AppState
+                    if (cachedResult == null) {
+                        result = getServiceStatus(context.packageManager, app.packageName)
+                        appState.add(result)
+                    } else {
+                        result = cachedResult
+                    }
+                    withContext(Dispatchers.Main) {
+                        binding.serviceStatus.text = context.getString(
+                            R.string.service_status_template,
+                            result.running,
+                            result.blocked,
+                            result.total
+                        )
+                    }
+                }
+            }
+        }
+
+        private suspend fun getServiceStatus(pm: PackageManager, packageName: String): AppState {
+            val serviceHelper = ServiceHelper(packageName)
+            serviceHelper.refresh()
+            val ifwImpl = IntentFirewallImpl.getInstance(context, packageName)
+            val services = ApplicationUtil.getServiceList(pm, packageName)
+            var running = 0
+            var blocked = 0
+            for (service in services) {
+                if (!ifwImpl.getComponentEnableState(packageName, service.name) ||
+                    !ApplicationUtil.checkComponentIsEnabled(
+                        pm,
+                        ComponentName(packageName, service.name)
+                    )
+                ) {
+                    blocked++
+                }
+                if (serviceHelper.isServiceRunning(service.name)) {
+                    running++
+                }
+            }
+            return AppState(running, blocked, services.count(), packageName)
         }
     }
 }
--- app/src/main/java/com/merxury/blocker/ui/applist/AppListFragment.kt
+++ app/src/main/java/com/merxury/blocker/ui/applist/AppListFragment.kt
@@ -37,7 +37,7 @@ class AppListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
         viewModel?.appList?.observe(viewLifecycleOwner) { adapter.updateAppList(it) }
-        viewModel?.loadData(requireContext(), true)
+        viewModel?.loadData(requireContext(), false)
     }
 
     override fun onDestroy() {
