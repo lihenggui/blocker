961218022976f0f693b443c255e2c47859bfa2a5
Mercury <350699171@qq.com>
2022-02-24 22:49:40 +0800

    Add Shizuku permission request

--- app/src/main/java/com/merxury/blocker/ui/detail/AppDetailActivity.kt
+++ app/src/main/java/com/merxury/blocker/ui/detail/AppDetailActivity.kt
@@ -2,12 +2,19 @@ package com.merxury.blocker.ui.detail
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.elvishew.xlog.XLog
 import com.google.android.material.tabs.TabLayoutMediator
+import com.merxury.blocker.R
+import com.merxury.blocker.core.root.EControllerMethod
 import com.merxury.blocker.databinding.ActivityAppDetailBinding
+import com.merxury.blocker.util.PreferenceUtil
 import com.merxury.libkit.entity.Application
+import rikka.shizuku.Shizuku
 
 class AppDetailActivity : AppCompatActivity() {
     private var _app: Application? = null
@@ -22,6 +29,43 @@ class AppDetailActivity : AppCompatActivity() {
         fetchAppInfo()
         initToolbar()
         initViewPager()
+        initShizuku()
+    }
+
+    private fun initShizuku() {
+        if (PreferenceUtil.getControllerType(this) != EControllerMethod.SHIZUKU) {
+            return
+        }
+        logger.i("Request Shizuku permission")
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            logger.e("Shizuku does not support Android 5.1 or below")
+            return
+        }
+        if (Shizuku.isPreV11()) {
+            // Pre-v11 is unsupported
+            return
+        }
+        val shizukuPermissionGranted =
+            Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED
+        when {
+            shizukuPermissionGranted -> {
+                logger.d("Shizuku permission was already granted")
+                return
+            }
+            Shizuku.shouldShowRequestPermissionRationale() -> {
+                // Users choose "Deny and don't ask again"
+                AlertDialog.Builder(this)
+                    .setTitle(R.string.permission_required)
+                    .setMessage(R.string.shizuku_permission_required_message)
+                    .show()
+                logger.e("User denied Shizuku permission")
+                return
+            }
+            else -> {
+                logger.d("Request Shizuku permission")
+                Shizuku.requestPermission(REQUEST_CODE_PERMISSION)
+            }
+        }
     }
 
     private fun initToolbar() {
@@ -49,6 +93,7 @@ class AppDetailActivity : AppCompatActivity() {
 
     companion object {
         private const val EXTRA_APP = "EXTRA_APP"
+        private const val REQUEST_CODE_PERMISSION = 101
 
         fun start(context: Context, app: Application) {
             Intent(context, AppDetailActivity::class.java).apply {
