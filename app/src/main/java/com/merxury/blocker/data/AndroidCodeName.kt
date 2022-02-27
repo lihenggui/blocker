e98952d37a4545995abd4638d8bec5555c68e067
Mercury <350699171@qq.com>
2022-02-27 13:06:48 +0800

    Show app info in details screen

---
+++ app/src/main/java/com/merxury/blocker/data/AndroidCodeName.kt
@@ -0,0 +1,47 @@
+package com.merxury.blocker.data
+
+import android.util.SparseArray
+
+object AndroidCodeName {
+    private val array: SparseArray<String> = SparseArray(32)
+
+    init {
+        array.put(1, "Android 1.0")
+        array.put(2, "Android 1.1")
+        array.put(3, "Android 1.5")
+        array.put(4, "Android 1.6")
+        array.put(5, "Android 2.0")
+        array.put(6, "Android 2.0.1")
+        array.put(7, "Android 2.1")
+        array.put(8, "Android 2.2")
+        array.put(9, "Android 2.3")
+        array.put(10, "Android 2.3.3")
+        array.put(11, "Android 3.0")
+        array.put(12, "Android 3.1")
+        array.put(13, "Android 3.2")
+        array.put(14, "Android 4.0.1")
+        array.put(15, "Android 4.0.3")
+        array.put(16, "Android 4.1")
+        array.put(17, "Android 4.2")
+        array.put(18, "Android 4.3")
+        array.put(19, "Android 4.4")
+        array.put(21, "Android 5.0")
+        array.put(22, "Android 5.1")
+        array.put(23, "Android 6.0")
+        array.put(24, "Android 7.0")
+        array.put(25, "Android 7.1")
+        array.put(26, "Android 8.0")
+        array.put(27, "Android 8.1")
+        array.put(28, "Android P")
+        array.put(29, "Android 10")
+        array.put(30, "Android 11")
+        array.put(31, "Android 12")
+        array.put(32, "Android 12L")
+        array.put(33, "Android 13")
+        // Reference : https://source.android.com/setup/start/build-numbers
+    }
+
+    fun getCodeName(code: Int): String {
+        return array.get(code) ?: "Android API $code"
+    }
+}
--- app/src/main/java/com/merxury/blocker/ui/detail/appinfo/AppInfoFragment.kt
+++ app/src/main/java/com/merxury/blocker/ui/detail/appinfo/AppInfoFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.merxury.blocker.R
+import com.merxury.blocker.data.AndroidCodeName
 import com.merxury.blocker.databinding.AppInfoFragmentBinding
 import com.merxury.blocker.util.AppIconCache
 import com.merxury.libkit.entity.Application
+import java.text.DateFormat
+import java.util.*
+
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class AppInfoFragment : Fragment() {
     private var _binding: AppInfoFragmentBinding? = null
@@ -30,6 +36,7 @@ class AppInfoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         app = arguments?.getParcelable("app")!!
         showHeader()
+        showInfo()
     }
 
     override fun onDestroyView() {
@@ -52,6 +59,34 @@ class AppInfoFragment : Fragment() {
             appInfo.uid / 100000,
             binding.appIcon
         )
+    }
+
+    private fun showInfo() {
+        lifecycleScope.launch {
+            val targetSdkVersion = app.packageInfo?.applicationInfo?.targetSdkVersion ?: 0
+            val targetSdkName = AndroidCodeName.getCodeName(targetSdkVersion)
+            binding.itemTargetSdkVersion.setSummary(
+                getString(
+                    R.string.sdk_version_with_name_template,
+                    targetSdkVersion,
+                    targetSdkName
+                )
+            )
+            val minSdkVersion = app.getMinSdkVersion()
+            val minSdkName = AndroidCodeName.getCodeName(minSdkVersion)
+            binding.itemMinSdkVersion.setSummary(
+                getString(
+                    R.string.sdk_version_with_name_template,
+                    minSdkVersion,
+                    minSdkName
+                )
+            )
+            val lastUpdateTime = app.lastUpdateTime ?: Date(0)
+            val formatter = DateFormat.getDateTimeInstance()
+            binding.itemLastUpdateTime.setSummary(formatter.format(lastUpdateTime))
+            val dataDir = app.packageInfo?.applicationInfo?.dataDir
+            binding.itemDataDir.setSummary(dataDir)
+        }
     }
 
     companion object {
