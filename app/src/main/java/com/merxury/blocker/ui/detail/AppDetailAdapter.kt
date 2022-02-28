01f2789ebcb134ee74ec6f725868e87aac2ddf39
Mercury <350699171@qq.com>
2022-02-28 15:23:06 +0800

    Showing tab info

--- app/src/main/java/com/merxury/blocker/ui/detail/AppDetailAdapter.kt
+++ app/src/main/java/com/merxury/blocker/ui/detail/AppDetailAdapter.kt
@@ -3,6 +3,7 @@ package com.merxury.blocker.ui.detail
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.merxury.blocker.R
 import com.merxury.blocker.ui.detail.appinfo.AppInfoFragment
 import com.merxury.blocker.ui.detail.component.ComponentFragment
 import com.merxury.blocker.ui.detail.component.EComponentType
@@ -24,5 +25,15 @@ class AppDetailAdapter(activity: FragmentActivity, app: Application) :
 
     override fun createFragment(position: Int): Fragment {
         return list[position]
+    }
+
+    companion object {
+        val titles = listOf(
+            R.string.app_info,
+            R.string.service,
+            R.string.receiver,
+            R.string.activity,
+            R.string.content_provider
+        )
     }
 }
