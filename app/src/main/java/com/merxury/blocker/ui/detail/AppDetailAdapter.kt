58ac6fe9466bd5fab62af4cf066777aff3067aac
Mercury <350699171@qq.com>
2022-02-28 12:14:22 +0800

    Add list for showing component info

--- app/src/main/java/com/merxury/blocker/ui/detail/AppDetailAdapter.kt
+++ app/src/main/java/com/merxury/blocker/ui/detail/AppDetailAdapter.kt
@@ -4,12 +4,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.merxury.blocker.ui.detail.appinfo.AppInfoFragment
+import com.merxury.blocker.ui.detail.component.ComponentFragment
+import com.merxury.blocker.ui.detail.component.EComponentType
 import com.merxury.libkit.entity.Application
 
 class AppDetailAdapter(activity: FragmentActivity, app: Application) :
     FragmentStateAdapter(activity) {
     private val list = listOf(
         AppInfoFragment.newInstance(app),
+        ComponentFragment.newInstance(app.packageName, EComponentType.SERVICE),
+        ComponentFragment.newInstance(app.packageName, EComponentType.RECEIVER),
+        ComponentFragment.newInstance(app.packageName, EComponentType.ACTIVITY),
+        ComponentFragment.newInstance(app.packageName, EComponentType.PROVIDER),
     )
 
     override fun getItemCount(): Int {
