0249835184e92f7105d4b557e103a7062beb9c21
Mercury <350699171@qq.com>
2022-03-08 15:44:38 +0800

    WIP: Advanced search

--- app/src/main/java/com/merxury/blocker/ui/home/advsearch/AdvSearchFragment.kt
+++ app/src/main/java/com/merxury/blocker/ui/home/advsearch/AdvSearchFragment.kt
@@ -1,6 +1,33 @@
 package com.merxury.blocker.ui.home.advsearch
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.merxury.blocker.databinding.AdvSearchFragmentBinding
 
 class AdvSearchFragment : Fragment() {
+    private lateinit var binding: AdvSearchFragmentBinding
+    private var viewModel: AdvSearchViewModel? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[AdvSearchViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = AdvSearchFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel?.load(requireContext())
+    }
 }
