4f9dc3cb548e866772c114472c9887a0cadcb4a2
Mercury <350699171@qq.com>
2022-03-08 15:44:38 +0800

    WIP: Advanced search

--- app/src/main/java/com/merxury/blocker/ui/home/advsearch/AdvSearchViewModel.kt
+++ app/src/main/java/com/merxury/blocker/ui/home/advsearch/AdvSearchViewModel.kt
@@ -1,18 +1,30 @@
 package com.merxury.blocker.ui.home.advsearch
 
 import android.content.Context
+import android.content.pm.ComponentInfo
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.elvishew.xlog.XLog
+import com.merxury.blocker.core.ComponentControllerProxy
+import com.merxury.blocker.core.IController
+import com.merxury.blocker.core.root.EControllerMethod
 import com.merxury.blocker.ui.detail.component.ComponentData
+import com.merxury.ifw.IntentFirewall
+import com.merxury.ifw.IntentFirewallImpl
 import com.merxury.libkit.entity.Application
+import com.merxury.libkit.entity.getSimpleName
 import com.merxury.libkit.utils.ApplicationUtil
+import com.merxury.libkit.utils.ServiceHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 class AdvSearchViewModel : ViewModel() {
+    private val logger = XLog.tag("AdvSearchViewModel")
     private val _appList = MutableLiveData<List<Application>>()
     val appList: LiveData<List<Application>> = _appList
     private val _total = MutableLiveData<Int>()
@@ -23,36 +35,71 @@ class AdvSearchViewModel : ViewModel() {
     val isLoading: LiveData<Boolean> = _isLoading
     private val _currentProcessApplication = MutableLiveData<Application>()
     val currentProcessApplication: LiveData<Application> = _currentProcessApplication
-    private val _finalData = MutableLiveData<Pair<Application, List<ComponentData>>>()
-    val finalData: LiveData<Pair<Application, List<ComponentData>>> = _finalData
+    private val _finalData = MutableLiveData<MutableList<Pair<Application, List<ComponentData>>>>()
+    val finalData: LiveData<MutableList<Pair<Application, List<ComponentData>>>> = _finalData
 
+    @OptIn(ExperimentalTime::class)
     fun load(context: Context) {
-        viewModelScope.launch {
-            val appList = ApplicationUtil.getThirdPartyApplicationList(context)
-            processData(context, appList)
+        val timeToLoad = measureTime {
+            viewModelScope.launch {
+                val appList = ApplicationUtil.getThirdPartyApplicationList(context)
+                processData(context, appList)
+            }
         }
+        logger.i("load time: $timeToLoad")
     }
 
     private suspend fun processData(context: Context, appList: List<Application>) {
         notifyDataProcessing(appList)
         val result = mutableListOf<Pair<Application, List<ComponentData>>>()
         withContext(Dispatchers.Default) {
+            val pmController = ComponentControllerProxy.getInstance(EControllerMethod.PM, context)
             appList.forEachIndexed { index, application ->
+                val packageName = application.packageName
+                val ifwController = IntentFirewallImpl(packageName).load()
                 _currentProcessApplication.postValue(application)
                 _current.postValue(index)
                 val components = mutableListOf<ComponentData>()
-                val activities =
-                    ApplicationUtil.getActivityList(context.packageManager, application.packageName)
-                val services =
-                    ApplicationUtil.getServiceList(context.packageManager, application.packageName)
-                val providers =
-                    ApplicationUtil.getProviderList(context.packageManager, application.packageName)
-                val receivers =
-                    ApplicationUtil.getReceiverList(context.packageManager, application.packageName)
-                activities.map {
-
-                }
+                val serviceHelper = ServiceHelper(packageName)
+                serviceHelper.refresh()
+                val activities = ApplicationUtil
+                    .getActivityList(context.packageManager, packageName)
+                    .convertToComponentDataList(ifwController, pmController, null)
+                val services = ApplicationUtil.getServiceList(context.packageManager, packageName)
+                    .convertToComponentDataList(
+                        ifwController,
+                        pmController,
+                        ServiceHelper(packageName)
+                    )
+                val providers = ApplicationUtil.getProviderList(context.packageManager, packageName)
+                    .convertToComponentDataList(ifwController, pmController, null)
+                val receivers = ApplicationUtil.getReceiverList(context.packageManager, packageName)
+                    .convertToComponentDataList(ifwController, pmController, null)
+                components.plus(activities)
+                    .plus(services)
+                    .plus(providers)
+                    .plus(receivers)
+                result.add(Pair(application, components))
             }
+        }
+        _finalData.postValue(result)
+        _isLoading.postValue(false)
+    }
+
+    private suspend fun List<ComponentInfo>.convertToComponentDataList(
+        ifwController: IntentFirewall,
+        pmController: IController,
+        serviceHelper: ServiceHelper?
+    ): List<ComponentData> {
+        return this.map {
+            ComponentData(
+                name = it.name,
+                simpleName = it.getSimpleName(),
+                packageName = it.packageName,
+                ifwBlocked = !ifwController.getComponentEnableState(it.packageName, it.name),
+                pmBlocked = !pmController.checkComponentEnableState(it.packageName, it.name),
+                isRunning = serviceHelper?.isServiceRunning(it.name) ?: false
+            )
         }
     }
 
