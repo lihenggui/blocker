77ca26303fe25c709b8a566d0d974d50d3bac1bc
Mercury <350699171@qq.com>
2022-03-08 20:18:14 +0800

    WIP: Advanced search

--- app/src/main/java/com/merxury/blocker/ui/home/advsearch/AdvSearchFragment.kt
+++ app/src/main/java/com/merxury/blocker/ui/home/advsearch/AdvSearchFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.elvishew.xlog.XLog
 import com.merxury.blocker.databinding.AdvSearchFragmentBinding
 
 class AdvSearchFragment : Fragment() {
+    private val logger = XLog.tag("AdvSearchFragment")
     private lateinit var binding: AdvSearchFragmentBinding
     private var viewModel: AdvSearchViewModel? = null
+    private var totalCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +32,25 @@ class AdvSearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel?.load(requireContext())
+        viewModel?.currentProcessApplication?.observe(viewLifecycleOwner) {
+            binding.processingName.text = it.packageName
+        }
+        viewModel?.total?.observe(viewLifecycleOwner) {
+            if (it == 0) {
+                // Do something that shows no apps was installed
+                return@observe
+            }
+            if (it > 0) {
+                logger.i("totalCount: $totalCount")
+                totalCount = it
+            }
+        }
+        viewModel?.current?.observe(viewLifecycleOwner) {
+            logger.i("current: $it")
+            if (totalCount > 0) {
+                val progress = (it * 100 / totalCount)
+                binding.progressBar.setProgressCompat(progress, false)
+            }
+        }
     }
 }
