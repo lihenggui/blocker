56ccc655d6993bf0d7264a946916edfad9772844
Mercury <350699171@qq.com>
2022-03-23 14:41:28 +0800

    Reduce ViewPager's sensitivity

--- app/src/main/java/com/merxury/blocker/util/Exts.kt
+++ app/src/main/java/com/merxury/blocker/util/Exts.kt
@@ -1,3 +1,17 @@
 package com.merxury.blocker.util
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+
 fun <T> unsafeLazy(initializer: () -> T): Lazy<T> = lazy(LazyThreadSafetyMode.NONE, initializer)
+
+fun ViewPager2.reduceDragSensitivity(f: Int = 4) {
+    val recyclerViewField = ViewPager2::class.java.getDeclaredField("mRecyclerView")
+    recyclerViewField.isAccessible = true
+    val recyclerView = recyclerViewField.get(this) as RecyclerView
+
+    val touchSlopField = RecyclerView::class.java.getDeclaredField("mTouchSlop")
+    touchSlopField.isAccessible = true
+    val touchSlop = touchSlopField.get(recyclerView) as Int
+    touchSlopField.set(recyclerView, touchSlop * f)       // "8" was obtained experimentally
+}
