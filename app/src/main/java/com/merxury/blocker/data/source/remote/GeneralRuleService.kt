11966255709f3cc566ba08db4cb288635397007c
lihenggui <mercuryleee@gmail.com>
2022-06-19 13:23:55 -0700

    Use correct file in the cloud configuration

--- app/src/main/java/com/merxury/blocker/data/source/remote/GeneralRuleService.kt
+++ app/src/main/java/com/merxury/blocker/data/source/remote/GeneralRuleService.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface GeneralRuleService {
-    @GET("zh-cn/general.sh")
+    @GET("zh-cn/general.json")
     suspend fun getOnlineRules(): Response<List<GeneralRule>>
 }
