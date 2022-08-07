7a618f14880499740cd2a672550f029530900459
lihenggui <lihenggui@users.noreply.github.com>
2022-08-06 22:33:56 -0700

    Replace GitLab with JihuLab

--- app/src/main/java/com/merxury/blocker/data/source/OnlineSourceType.kt
+++ app/src/main/java/com/merxury/blocker/data/source/OnlineSourceType.kt
@@ -2,5 +2,5 @@ package com.merxury.blocker.data.source
 
 enum class OnlineSourceType(val baseUrl: String) {
     GITHUB("https://raw.githubusercontent.com/lihenggui/blocker-general-rules/main/"),
-    GITLAB("https://gitlab.com/mercuryli/blocker-general-rules/-/raw/main/")
+    GITLAB("https://jihulab.com/mercuryli/blocker-general-rules/-/raw/main/")
 }
