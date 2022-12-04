14c44b21eea490167a1ec61c829bbbb0bceeae51
lihenggui <lihenggui@users.noreply.github.com>
2022-12-03 16:50:25 -0800

    Remove ui-test-hilt-manifest

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -13,13 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// TODO: Remove once https://youtrack.jetbrains.com/issue/KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("blocker.android.library")
-    kotlin("kapt")
-    id("dagger.hilt.android.plugin")
+    id("kotlin")
 }
 
 dependencies {
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.compiler)
+    implementation(libs.kotlinx.datetime)
 }
