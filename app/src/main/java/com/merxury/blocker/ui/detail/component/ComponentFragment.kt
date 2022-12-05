6d0cb7caa54471f76c82a2a5f3684862414a6381
lihenggui <lihenggui@users.noreply.github.com>
2022-12-04 22:00:45 -0800

    Remove wildcard imports

--- app/src/main/java/com/merxury/blocker/ui/detail/component/ComponentFragment.kt
+++ app/src/main/java/com/merxury/blocker/ui/detail/component/ComponentFragment.kt
@@ -7,7 +7,12 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SearchView
