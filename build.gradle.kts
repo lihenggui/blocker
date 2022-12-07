2d4c0af9a09da3cc8dec7eb9bd8c6401d433a7fc
lihenggui <lihenggui@users.noreply.github.com>
2022-12-07 13:20:36 -0800

    Suppress warning: "xx can't be called in this context by implicit receiver. Use the explicit one if necessary"
    Change-Id: Idbbbd289fec5fd57935b02417f46645a89b5a1ce

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ buildscript {
 
 }
 
+// TODO: Remove once https://youtrack.jetbrains.com/issue/KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm) apply false
