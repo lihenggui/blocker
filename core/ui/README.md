eff159abc3658fb8ee66d303682468840e919af2
juliewww <julie.www333@gmail.com>
2022-12-07 10:37:28 +0800

    Add feature and core/ui

---
+++ core/ui/README.md
@@ -0,0 +1,1 @@
+# :core:ui module
---
+++ feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/AppDetailScreen.kt
@@ -0,0 +1,13 @@
+package com.merxury.blocker.feature.appdetail
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppDetailRoute(
+    onBackClick: () -> Unit,
+) {
+}
+
+@Composable
+fun AppDetailScreen() {
+}
