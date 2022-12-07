eee6e338e921d80ebb81d5cadf7aaf2f070f82bf
lihenggui <lihenggui@users.noreply.github.com>
2022-12-07 14:40:52 -0800

    Move IFW StorageUtils to correct project
    Change-Id: Icac23914d540defb839a726c7f9300b2ef7b1330

--- core/ifw-api/src/main/java/com/merxury/ifw/util/StorageUtils.java
+++ core/ifw-api/src/main/java/com/merxury/ifw/util/StorageUtils.java
@@ -14,7 +14,7 @@
  *   limitations under the License.
  */
 
-package com.merxury.blocker.core.utils;
+package com.merxury.ifw.util;
 
 import android.util.Log;
 
@@ -22,22 +22,6 @@ import androidx.annotation.NonNull;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
-
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 public class StorageUtils {
     private static final String IFW_FOLDER = "/ifw";
