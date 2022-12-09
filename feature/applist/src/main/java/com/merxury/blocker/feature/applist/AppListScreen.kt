e4e957f8ccfe41b559ee7404fa148329d5b6d99e
juliewww <julie.www333@gmail.com>
2022-12-09 15:35:25 +0800

    AppListRoute and pullToRefresh

--- feature/applist/src/main/java/com/merxury/blocker/feature/applist/AppListScreen.kt
+++ feature/applist/src/main/java/com/merxury/blocker/feature/applist/AppListScreen.kt
@@ -1,54 +1,132 @@
 package com.merxury.blocker.feature.applist
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.merxury.blocker.core.designsystem.component.BlockerLoadingWheel
+import com.merxury.blocker.feature.applist.R.string
 
+@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun AppListRoute(
     navigateToAppDetail: (String) -> Unit,
     modifier: Modifier = Modifier,
+    viewModel: AppListViewModel = hiltViewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    AppListScreen(
+        uiState = uiState,
+        navigateToAppDetail = navigateToAppDetail,
+        isRefreshing = uiState is AppListUiState.Loading,
+        onRefresh = { /*TODO*/ },
+        modifier = modifier
+    )
 }
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun AppListScreen(
-    appInfo: List<AppInfo>,
+    uiState: AppListUiState,
     navigateToAppDetail: (String) -> Unit,
     isRefreshing: Boolean,
     onRefresh: () -> Unit,
     modifier: Modifier
 ) {
-    val listContent = remember { appInfo }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        when (uiState) {
+            AppListUiState.Loading -> {
+                Column(
+                    modifier = modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    BlockerLoadingWheel(
+                        modifier = modifier,
+                        contentDesc = stringResource(id = string.loading),
+                    )
+                }
+            }
+
+            is AppListUiState.Success -> {
+                AppListContent(
+                    appList = uiState.appList,
+                    navigateToAppDetail = navigateToAppDetail,
+                    isRefreshing = isRefreshing,
+                    onRefresh = onRefresh,
+                    modifier = modifier
+                )
+            }
+
+            is AppListUiState.Error -> ErrorAppListScreen(uiState.errorMessage)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AppListContent(
+    appList: SnapshotStateList<AppInfo>,
+    navigateToAppDetail: (String) -> Unit,
+    isRefreshing: Boolean,
+    onRefresh: () -> Unit,
+    modifier: Modifier
+) {
+    val listContent = remember { appList }
     val listState = rememberLazyListState()
     val refreshing by remember { mutableStateOf(isRefreshing) }
     val refreshingState = rememberPullRefreshState(refreshing, onRefresh)
-    LazyColumn(
-        modifier = modifier,
-        state = listState
-    ) {
-        items(listContent, key = { it.packageName }) {
-            AppListItem(
-                appIconUrl = it.appIconUrl,
-                packageName = it.packageName,
-                versionName = it.versionName,
-                serviceStatus = stringResource(
-                    id = R.string.service_status_template,
-                    it.appStatus.running,
-                    it.appStatus.blocked,
-                    it.appStatus.total
-                ),
-                onClick = navigateToAppDetail
-            )
+    Box(modifier.pullRefresh(refreshingState)) {
+        LazyColumn(
+            modifier = modifier,
+            state = listState
+        ) {
+            items(listContent, key = { it.packageName }) {
+                AppListItem(
+                    appIconUrl = it.appIconUrl,
+                    packageName = it.packageName,
+                    versionName = it.versionName,
+                    serviceStatus = stringResource(
+                        id = string.service_status_template,
+                        it.appStatus.running,
+                        it.appStatus.blocked,
+                        it.appStatus.total
+                    ),
+                    onClick = navigateToAppDetail
+                )
+            }
         }
+        PullRefreshIndicator(
+            refreshing = refreshing,
+            state = refreshingState,
+            modifier = Modifier.align(Alignment.TopCenter),
+            scale = true
+        )
     }
+}
+
+@Composable
+fun ErrorAppListScreen(message: String) {
+    Text(text = message)
 }
