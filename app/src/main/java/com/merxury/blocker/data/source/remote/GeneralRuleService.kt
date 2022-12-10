63d5d7ff75aeeb88fec9861053bc2cdd6a9b039a
lihenggui <lihenggui@users.noreply.github.com>
2022-12-10 11:04:48 -0800

    Move local general rule to database module
    Change-Id: I46ec0e0862f749ca0bbf3625fa534727cd06ff3f

--- app/src/main/java/com/merxury/blocker/data/source/remote/GeneralRuleService.kt
+++ app/src/main/java/com/merxury/blocker/data/source/remote/GeneralRuleService.kt
@@ -16,11 +16,11 @@
 
 package com.merxury.blocker.data.source.remote
 
-import com.merxury.blocker.data.source.GeneralRule
+import com.merxury.blocker.core.database.generalrule.GeneralRuleEntity
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface GeneralRuleService {
     @GET("zh-cn/general.json")
-    suspend fun getOnlineRules(): Response<List<GeneralRule>>
+    suspend fun getOnlineRules(): Response<List<GeneralRuleEntity>>
 }
---
+++ core/model/src/main/java/com/merxury/blocker/core/model/data/GeneralRule.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022 Blocker
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.core.model.data
+
+data class GeneralRule(
+    val id: Int,
+    val name: String? = null,
+    val iconUrl: String? = null,
+    val company: String? = null,
+    val searchKeyword: List<String> = listOf(),
+    val useRegexSearch: Boolean? = null,
+    val description: String? = null,
+    val safeToBlock: Boolean? = null,
+    val sideEffect: String? = null,
+    val contributors: List<String> = listOf()
+)
