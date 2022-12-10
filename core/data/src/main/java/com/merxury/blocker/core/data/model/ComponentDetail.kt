52edae1b4edb4b242dc70dab009a6e3db1ccbb4b
lihenggui <lihenggui@users.noreply.github.com>
2022-12-10 12:41:07 -0800

    Add asEntity to GeneralRule
    Change-Id: I0464228e40f3dc475991f4a2c47103af40227282

--- core/data/src/main/java/com/merxury/blocker/core/data/model/ComponentDetail.kt
+++ core/data/src/main/java/com/merxury/blocker/core/data/model/ComponentDetail.kt
@@ -1,9 +1,9 @@
 package com.merxury.blocker.core.data.model
 
-import com.merxury.blocker.core.model.data.ComponentDetail
+import com.merxury.blocker.core.database.cmpdetail.ComponentDetailEntity
 import com.merxury.blocker.core.network.model.NetworkComponentDetail
 
-fun NetworkComponentDetail.asEntity() = ComponentDetail(
+fun NetworkComponentDetail.asEntity() = ComponentDetailEntity(
     name = name,
     icon = icon,
     sdkName = sdkName,
