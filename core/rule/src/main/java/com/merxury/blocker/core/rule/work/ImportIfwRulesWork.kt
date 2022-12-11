ed5d5d954a38f3e725820f187764528d31f53d47
lihenggui <lihenggui@users.noreply.github.com>
2022-12-11 15:12:29 -0800

    WIP: Move works to rule module
    Change-Id: I4ff8020b7f2b392507a61698a02d34c92f59d7f4

--- core/rule/src/main/java/com/merxury/blocker/core/rule/work/ImportIfwRulesWork.kt
+++ core/rule/src/main/java/com/merxury/blocker/core/rule/work/ImportIfwRulesWork.kt
@@ -14,7 +14,7 @@
  *   limitations under the License.
  */
 
-package com.merxury.blocker.work
+package com.merxury.blocker.core.rule.work
 
 import android.content.Context
 import android.content.pm.ComponentInfo
