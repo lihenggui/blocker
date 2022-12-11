4bd7026575b1e028c97fb44df4a41c11c43621c2
lihenggui <lihenggui@users.noreply.github.com>
2022-12-11 15:12:29 -0800

    WIP: Move works to rule module
    Change-Id: I4ff8020b7f2b392507a61698a02d34c92f59d7f4

--- core/rule/src/main/java/com/merxury/blocker/core/rule/util/NotificationUtil.kt
+++ core/rule/src/main/java/com/merxury/blocker/core/rule/util/NotificationUtil.kt
@@ -1,27 +1,27 @@
 /*
  * Copyright 2022 Blocker
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       https://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package com.merxury.blocker.util
+package com.merxury.blocker.core.rule.util
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
-import com.merxury.blocker.R
+import com.merxury.blocker.core.rule.R
 
 object NotificationUtil {
     const val PROCESSING_INDICATOR_CHANNEL_ID = "processing_progress_indicator"
