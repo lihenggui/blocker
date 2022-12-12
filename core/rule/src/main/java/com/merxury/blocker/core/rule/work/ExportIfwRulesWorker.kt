187d5a0b0e0621a5ea3c7e42b1e5838c87ce4c73
lihenggui <lihenggui@users.noreply.github.com>
2022-12-11 21:13:58 -0800

    Rename ExportIfwRulesWork
    Change-Id: Icfdb275d3a727fa95641c4364f67abd6f300dcb7

--- core/rule/src/main/java/com/merxury/blocker/core/rule/work/ExportIfwRulesWorker.kt
+++ core/rule/src/main/java/com/merxury/blocker/core/rule/work/ExportIfwRulesWorker.kt
@@ -19,8 +19,11 @@ package com.merxury.blocker.core.rule.work
 import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import com.merxury.blocker.core.rule.R
@@ -28,20 +31,25 @@ import com.merxury.blocker.core.rule.util.NotificationUtil
 import com.merxury.blocker.core.rule.util.StorageUtil
 import com.merxury.blocker.core.utils.FileUtils
 import com.merxury.ifw.util.StorageUtils
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import java.io.File
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 
-class ExportIfwRulesWork(context: Context, params: WorkerParameters) :
-    CoroutineWorker(context, params) {
+@HiltWorker
+class ExportIfwRulesWorker @AssistedInject constructor(
+    @Assisted private val context: Context,
+    @Assisted params: WorkerParameters
+) : CoroutineWorker(context, params) {
 
     override suspend fun getForegroundInfo(): ForegroundInfo {
         return updateNotification("", 0, 0)
     }
 
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
-        if (!StorageUtil.isSavedFolderReadable(applicationContext)) {
+        if (!StorageUtil.isSavedFolderReadable(context)) {
 //            ToastUtil.showToast(R.string.export_ifw_failed_message, Toast.LENGTH_LONG)
             return@withContext Result.failure()
         }
@@ -56,7 +64,7 @@ class ExportIfwRulesWork(context: Context, params: WorkerParameters) :
                 val filename = it.split(File.separator).last()
                 setForeground(updateNotification(filename, current, total))
                 val content = FileUtils.read(ifwFolder + it)
-                StorageUtil.saveIfwToStorage(applicationContext, filename, content)
+                StorageUtil.saveIfwToStorage(context, filename, content)
                 current++
             }
         } catch (e: Exception) {
@@ -65,23 +73,23 @@ class ExportIfwRulesWork(context: Context, params: WorkerParameters) :
             return@withContext Result.failure()
         }
         Timber.i("Export IFW rules finished, success count = $current.")
-        val message = applicationContext.getString(R.string.export_ifw_successful_message, current)
+        val message = context.getString(R.string.export_ifw_successful_message, current)
 //        ToastUtil.showToast(message, Toast.LENGTH_LONG)
         return@withContext Result.success()
     }
 
     private fun updateNotification(name: String, current: Int, total: Int): ForegroundInfo {
         val id = NotificationUtil.PROCESSING_INDICATOR_CHANNEL_ID
-        val title = applicationContext.getString(R.string.backing_up_ifw_please_wait)
-        val cancel = applicationContext.getString(R.string.cancel)
+        val title = context.getString(R.string.backing_up_ifw_please_wait)
+        val cancel = context.getString(R.string.cancel)
         // This PendingIntent can be used to cancel the worker
-        val intent = WorkManager.getInstance(applicationContext)
+        val intent = WorkManager.getInstance(context)
             .createCancelPendingIntent(getId())
         // Create a Notification channel if necessary
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationUtil.createProgressingNotificationChannel(applicationContext)
+            NotificationUtil.createProgressingNotificationChannel(context)
         }
-        val notification = NotificationCompat.Builder(applicationContext, id)
+        val notification = NotificationCompat.Builder(context, id)
             .setContentTitle(title)
             .setTicker(title)
             .setSubText(name)
@@ -91,5 +99,11 @@ class ExportIfwRulesWork(context: Context, params: WorkerParameters) :
             .addAction(android.R.drawable.ic_delete, cancel, intent)
             .build()
         return ForegroundInfo(NotificationUtil.PROCESSING_NOTIFICATION_ID, notification)
+    }
+
+    companion object {
+        fun exportWork() = OneTimeWorkRequestBuilder<ExportIfwRulesWorker>()
+            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+            .build()
     }
 }
