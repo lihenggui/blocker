3ff780117ac978332d88d91d142b67e18b9a2bb2
lihenggui <lihenggui@users.noreply.github.com>
2022-12-12 21:37:17 -0800

    Add user settings and data classes
    Change-Id: Ia4d7dc7ed3feab174b5213f334dae61ed3c574b8

---
+++ core/datastore/src/main/java/com/merxury/blocker/core/datastore/BlockerPreferencesDataSource.kt
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2022 Blocker
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.core.datastore
+
+import androidx.datastore.core.DataStore
+import com.merxury.blocker.core.model.data.ControllerType
+import com.merxury.blocker.core.model.preference.AppSorting
+import com.merxury.blocker.core.model.preference.ComponentShowPriority
+import com.merxury.blocker.core.model.preference.ComponentSorting
+import com.merxury.blocker.core.model.preference.DarkThemeConfig
+import com.merxury.blocker.core.model.preference.RuleServerProvider
+import com.merxury.blocker.core.model.preference.ThemeBrand
+import com.merxury.blocker.core.model.preference.UserPreferenceData
+import javax.inject.Inject
+import kotlinx.coroutines.flow.map
+
+class BlockerPreferencesDataSource @Inject constructor(
+    private val userPreferences: DataStore<UserPreferences>
+) {
+
+    val userData = userPreferences.data.map {
+        UserPreferenceData(
+            themeBrand = when (it.themeBrand) {
+                null,
+                ThemeBrandProto.THEME_BRAND_UNSPECIFIED,
+                ThemeBrandProto.UNRECOGNIZED,
+                ThemeBrandProto.THEME_BRAND_DEFAULT -> ThemeBrand.DEFAULT
+                ThemeBrandProto.THEME_BRAND_ANDROID -> ThemeBrand.ANDROID
+            },
+            darkThemeConfig = when (it.darkThemeConfig) {
+                null,
+                DarkThemeConfigProto.DARK_THEME_CONFIG_UNSPECIFIED,
+                DarkThemeConfigProto.UNRECOGNIZED,
+                DarkThemeConfigProto.DARK_THEME_CONFIG_FOLLOW_SYSTEM ->
+                    DarkThemeConfig.FOLLOW_SYSTEM
+                DarkThemeConfigProto.DARK_THEME_CONFIG_LIGHT -> DarkThemeConfig.LIGHT
+                DarkThemeConfigProto.DARK_THEME_CONFIG_DARK -> DarkThemeConfig.DARK
+            },
+            controllerType = when (it.controllerType) {
+                null,
+                ControllerTypeProto.UNRECOGNIZED,
+                ControllerTypeProto.IFW -> ControllerType.IFW
+                ControllerTypeProto.PM -> ControllerType.PM
+                ControllerTypeProto.SHIZUKU -> ControllerType.SHIZUKU
+            },
+            ruleServerProvider = when (it.ruleServerProvider) {
+                null,
+                RuleServerProviderProto.UNRECOGNIZED,
+                RuleServerProviderProto.GITLAB -> RuleServerProvider.GITLAB
+                RuleServerProviderProto.GITHUB -> RuleServerProvider.GITHUB
+            },
+            ruleBackupFolder = it.ruleBackupFolder,
+            backupSystemApp = it.backupSystemApp,
+            restoreSystemApp = it.restoreSystemApp,
+            showSystemApps = it.showSystemApps,
+            showServiceInfo = it.showServiceInfo,
+            appSorting = when (it.appSorting) {
+                null,
+                AppSortingProto.UNRECOGNIZED,
+                AppSortingProto.APP_NAME_ASCENDING ->
+                    AppSorting.NAME_ASCENDING
+                AppSortingProto.APP_NAME_DESCENDING ->
+                    AppSorting.NAME_DESCENDING
+                AppSortingProto.FIRST_INSTALL_TIME_ASCENDING ->
+                    AppSorting.FIRST_INSTALL_TIME_ASCENDING
+                AppSortingProto.FIRST_INSTALL_TIME_DESCENDING ->
+                    AppSorting.FIRST_INSTALL_TIME_DESCENDING
+                AppSortingProto.LAST_UPDATE_TIME_ASCENDING ->
+                    AppSorting.LAST_UPDATE_TIME_ASCENDING
+                AppSortingProto.LAST_UPDATE_TIME_DESCENDING ->
+                    AppSorting.LAST_UPDATE_TIME_DESCENDING
+            },
+            componentSorting = when (it.componentSorting) {
+                null,
+                ComponentSortingProto.UNRECOGNIZED,
+                ComponentSortingProto.COMPONENT_NAME_ASCENDING ->
+                    ComponentSorting.NAME_ASCENDING
+                ComponentSortingProto.COMPONENT_NAME_DESCENDING ->
+                    ComponentSorting.NAME_DESCENDING
+            },
+            componentShowPriority = when (it.componentShowPriority) {
+                null,
+                ComponentShowPriorityProto.UNRECOGNIZED,
+                ComponentShowPriorityProto.ENABLED_COMPONENTS_FIRST ->
+                    ComponentShowPriority.ENABLED_COMPONENTS_FIRST
+                ComponentShowPriorityProto.DISABLED_COMPONENTS_FIRST ->
+                    ComponentShowPriority.DISABLED_COMPONENTS_FIRST
+            }
+        )
+    }
+
+    suspend fun setThemeBrand(themeBrand: ThemeBrand) {
+        userPreferences.updateData {
+            it.copy {
+                this.themeBrand = when (themeBrand) {
+                    ThemeBrand.DEFAULT -> ThemeBrandProto.THEME_BRAND_DEFAULT
+                    ThemeBrand.ANDROID -> ThemeBrandProto.THEME_BRAND_ANDROID
+                }
+            }
+        }
+    }
+
+    suspend fun setDarkThemeConfig(darkThemeConfig: DarkThemeConfig) {
+        userPreferences.updateData {
+            it.copy {
+                this.darkThemeConfig = when (darkThemeConfig) {
+                    DarkThemeConfig.FOLLOW_SYSTEM ->
+                        DarkThemeConfigProto.DARK_THEME_CONFIG_FOLLOW_SYSTEM
+                    DarkThemeConfig.LIGHT -> DarkThemeConfigProto.DARK_THEME_CONFIG_LIGHT
+                    DarkThemeConfig.DARK -> DarkThemeConfigProto.DARK_THEME_CONFIG_DARK
+                }
+            }
+        }
+    }
+}
