fc41887a654c7083025259070acebe7376dd4f14
lihenggui <lihenggui@users.noreply.github.com>
2022-12-14 12:39:33 -0800

    Move AppIconCache to common
    Change-Id: I9bc0b1870e638a26b3953f7fca990c30e0f45950

--- core/common/src/main/java/com/merxury/blocker/core/utils/AppIconCache.kt
+++ core/common/src/main/java/com/merxury/blocker/core/utils/AppIconCache.kt
@@ -1,20 +1,4 @@
-/*
- * Copyright 2022 Blocker
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       https://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-
-package com.merxury.blocker.util
+package com.merxury.blocker.core.utils
 
 import android.annotation.SuppressLint
 import android.content.Context
@@ -23,9 +7,8 @@ import android.graphics.Bitmap
 import android.graphics.drawable.AdaptiveIconDrawable
 import android.os.Build
 import android.widget.ImageView
-import androidx.appcompat.view.ContextThemeWrapper
 import androidx.collection.LruCache
-import com.merxury.blocker.R
+import com.merxury.blocker.core.common.R
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import kotlin.coroutines.CoroutineContext
@@ -140,7 +123,7 @@ object AppIconCache : CoroutineScope {
             val bitmap = try {
                 withContext(dispatcher) {
                     getOrLoadBitmap(
-                        ContextThemeWrapper(context.applicationContext, R.style.AppTheme),
+                        context,
                         info,
                         userId,
                         size
