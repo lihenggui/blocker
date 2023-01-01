6d6719ac6623a70f433aac4bc74118925affce7a
juliewww <julie.www333@gmail.com>
2023-01-01 16:21:30 +0800

    Replace Any to T of menu type

--- core/designsystem/src/main/java/com/merxury/blocker/core/designsystem/component/TopAppBar.kt
+++ core/designsystem/src/main/java/com/merxury/blocker/core/designsystem/component/TopAppBar.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.merxury.blocker.core.designsystem.icon.BlockerIcons
+import com.merxury.blocker.core.designsystem.theme.BlockerTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -192,8 +193,10 @@ fun BlockerTopAppBarWithoutNavPreview() {
 @Preview("Top App Bar only with navigation icon")
 @Composable
 fun BlockerTopAppBarWithNavPreview() {
-    BlockerTopAppBar(
-        titleRes = string.untitled,
-        onNavigationClick = {}
-    )
+    BlockerTheme {
+        BlockerTopAppBar(
+            titleRes = string.untitled,
+            onNavigationClick = {}
+        )
+    }
 }
