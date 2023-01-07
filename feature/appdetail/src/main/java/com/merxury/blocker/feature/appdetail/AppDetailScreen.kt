6f0aedcc06f2021cb3d1d4e9bce3df8bc17c76c5
juliewww <julie.www333@gmail.com>
2023-01-07 18:54:51 +0800

    ScrollableTabRow instead of TabRow

--- feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/AppDetailScreen.kt
+++ feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/AppDetailScreen.kt
@@ -26,6 +26,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
@@ -38,13 +40,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.merxury.blocker.core.designsystem.component.BlockerLoadingWheel
-import com.merxury.blocker.core.designsystem.component.BlockerTab
-import com.merxury.blocker.core.designsystem.component.BlockerTabRow
 import com.merxury.blocker.core.designsystem.icon.BlockerIcons
 import com.merxury.blocker.core.designsystem.theme.BlockerTheme
 import com.merxury.blocker.core.model.Application
@@ -58,7 +59,7 @@ import com.merxury.blocker.feature.appdetail.model.AppInfoUiState.Success
 import com.merxury.blocker.feature.appdetail.model.AppInfoViewModel
 import kotlinx.datetime.Clock.System
 
-@OptIn(ExperimentalLifecycleComposeApi::class)
+@OptIn(ExperimentalLifecycleComposeApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun AppDetailRoute(
     viewModel: AppInfoViewModel = hiltViewModel(),
@@ -66,6 +67,9 @@ fun AppDetailRoute(
 ) {
     val tabState by viewModel.tabState.collectAsStateWithLifecycle()
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val scrollBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    val isCollapsed by remember { derivedStateOf { scrollBehavior.state.collapsedFraction > 0.5 } }
     AppDetailScreen(
         uiState = uiState,
         tabState = tabState,
@@ -77,7 +81,8 @@ fun AppDetailRoute(
         onFindInPage = viewModel::onFindInPage,
         onEnableApp = viewModel::onEnableApp,
         onEnableAll = viewModel::onEnableAll,
-        onBlockAll = viewModel::onBlockAll
+        onBlockAll = viewModel::onBlockAll,
+        isCollapsed = isCollapsed
     )
 }
 
@@ -94,6 +99,7 @@ fun AppDetailScreen(
     onEnableApp: () -> Unit,
     onEnableAll: () -> Unit,
     onBlockAll: () -> Unit,
+    isCollapsed: Boolean,
     modifier: Modifier = Modifier
 ) {
     Column(modifier) {
@@ -124,6 +130,7 @@ fun AppDetailScreen(
                     onEnableApp = onEnableApp,
                     onEnableAll = onEnableAll,
                     onBlockAll = onBlockAll,
+                    isCollapsed = isCollapsed,
                     modifier = modifier
                 )
             }
@@ -147,11 +154,11 @@ fun AppDetailContent(
     onEnableApp: () -> Unit,
     onEnableAll: () -> Unit,
     onBlockAll: () -> Unit,
+    isCollapsed: Boolean,
     modifier: Modifier = Modifier
 ) {
     val scrollBehavior =
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
-    val isCollapsed by remember { derivedStateOf { scrollBehavior.state.collapsedFraction > 0.5 } }
     val collapsed = 22
     val expanded = 28
     val topAppBarTextSize =
@@ -208,41 +215,43 @@ fun AppDetailContent(
         },
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
     ) { padding ->
-        BlockerTabRow(
-            modifier = Modifier.padding(padding),
-            selectedTabIndex = tabState.currentIndex
-        ) {
-            tabState.titles.forEachIndexed { index, titleRes ->
-                BlockerTab(
-                    selected = index == tabState.currentIndex,
-                    onClick = { switchTab(index) },
-                    text = { Text(text = stringResource(id = titleRes)) }
-                )
+        Column(modifier = Modifier.padding(padding)) {
+            ScrollableTabRow(
+                selectedTabIndex = tabState.currentIndex,
+                edgePadding = 16.dp
+            ) {
+                tabState.titles.forEachIndexed { index, titleRes ->
+                    Tab(
+                        selected = index == tabState.currentIndex,
+                        onClick = { switchTab(index) },
+                        text = { Text(text = stringResource(id = titleRes)) }
+                    )
+                }
             }
-        }
-        when (tabState.currentIndex) {
-            0 -> {
-                AppInfoTabContent(
-                    app = uiState.appInfo,
-                    isRefreshing = isRefreshing,
-                    onRefresh = onRefresh
-                )
-            }
+            when (tabState.currentIndex) {
+                0 -> {
+                    AppInfoTabContent(
+                        app = uiState.appInfo,
+                        isRefreshing = isRefreshing,
+                        onRefresh = onRefresh
+                    )
+                }
 
-            1 -> {
-                AppDetailCommonTabContentRoute()
-            }
+                1 -> {
+                    AppDetailCommonTabContentRoute()
+                }
 
-            2 -> {
-                AppDetailCommonTabContentRoute()
-            }
+                2 -> {
+                    AppDetailCommonTabContentRoute()
+                }
 
-            3 -> {
-                AppDetailCommonTabContentRoute()
-            }
+                3 -> {
+                    AppDetailCommonTabContentRoute()
+                }
 
-            4 -> {
-                AppDetailCommonTabContentRoute()
+                4 -> {
+                    AppDetailCommonTabContentRoute()
+                }
             }
         }
     }
@@ -287,7 +296,48 @@ fun AppDetailScreenPreview() {
             onFindInPage = {},
             onEnableApp = {},
             onEnableAll = {},
-            onBlockAll = {}
+            onBlockAll = {},
+            isCollapsed = false
+        )
+    }
+}
+
+@Composable
+@Preview
+fun AppDetailScreenCollapsedPreview() {
+    val app = Application(
+        label = "Blocker",
+        packageName = "com.mercury.blocker",
+        versionName = "1.2.69-alpha",
+        isEnabled = false,
+        firstInstallTime = System.now(),
+        lastUpdateTime = System.now(),
+        packageInfo = null,
+    )
+    val tabState = TabState(
+        titles = listOf(
+            string.app_info,
+            string.service,
+            string.service,
+            string.activity,
+            string.content_provider
+        ),
+        currentIndex = 0
+    )
+    BlockerTheme {
+        AppDetailScreen(
+            uiState = Success(appInfo = app),
+            tabState = tabState,
+            isRefreshing = false,
+            onRefresh = {},
+            switchTab = {},
+            onBackClick = {},
+            onShare = {},
+            onFindInPage = {},
+            onEnableApp = {},
+            onEnableAll = {},
+            onBlockAll = {},
+            isCollapsed = true
         )
     }
 }
