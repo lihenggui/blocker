a2cc3300627f4b5812dd87f86b98eb51e034458d
juliewww <julie.www333@gmail.com>
2023-01-08 17:39:05 +0800

    add verticalScroll n AppInfoTabContent

--- feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/component/AppInfoTabContent.kt
+++ feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/component/AppInfoTabContent.kt
@@ -22,6 +22,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -43,7 +45,13 @@ fun AppInfoTabContent(
     modifier: Modifier = Modifier
 ) {
     Box(modifier) {
-        Column(modifier = Modifier.fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(
+                    rememberScrollState()
+                )
+        ) {
             MoreInfo(
                 targetSdkVersion = app.packageInfo?.applicationInfo?.targetSdkVersion ?: 0,
                 // TODO add min SDK detection
