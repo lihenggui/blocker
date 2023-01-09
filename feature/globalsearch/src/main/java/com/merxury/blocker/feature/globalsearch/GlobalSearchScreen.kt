f4cdcd402831c1c71c46582fc8f8861786be50d9
juliewww <julie.www333@gmail.com>
2023-01-09 21:32:15 +0800

    Add buttons on GlobalSearchScreen

--- feature/globalsearch/src/main/java/com/merxury/blocker/feature/globalsearch/GlobalSearchScreen.kt
+++ feature/globalsearch/src/main/java/com/merxury/blocker/feature/globalsearch/GlobalSearchScreen.kt
@@ -1,13 +1,160 @@
 package com.merxury.blocker.feature.globalsearch
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.merxury.blocker.core.designsystem.component.BlockerTopAppBar
+import com.merxury.blocker.core.designsystem.icon.BlockerIcons
+import com.merxury.blocker.core.designsystem.theme.BlockerTheme
+import com.merxury.blocker.feature.globalsearch.model.LocalSearchViewModel
+import com.merxury.blocker.feature.globalsearch.model.SearchBoxUiState
 
+@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
-fun GlobalSearchRoute() {
-    Text(text = "Global search screen")
+fun GlobalSearchRoute(
+    viewModel: LocalSearchViewModel = hiltViewModel()
+) {
+    val searchBoxUiState by viewModel.searchBoxUiState.collectAsStateWithLifecycle()
+    GlobalSearchScreen(searchBoxUiState = searchBoxUiState)
 }
 
 @Composable
-fun GlobalSearchScreen() {
+fun GlobalSearchScreen(
+    modifier: Modifier = Modifier,
+    searchBoxUiState: SearchBoxUiState
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        SearchBar(
+            modifier = modifier,
+            uiState = searchBoxUiState
+        )
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            FilledTonalButton(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.application))
+            }
+            Spacer(modifier = modifier.width(8.dp))
+            FilledTonalButton(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.component))
+            }
+            Spacer(modifier = modifier.width(8.dp))
+            FilledTonalButton(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.online_rule))
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    uiState: SearchBoxUiState,
+    placeholderText: String = "",
+    onSearchTextChanged: (TextFieldValue) -> Unit = {},
+    onClearClick: () -> Unit = {},
+    onNavigateBack: () -> Unit = {}
+) {
+    var showClearButton by remember { mutableStateOf(false) }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusRequester = remember { FocusRequester() }
+    BlockerTopAppBar(
+        title = "",
+        navigationIcon = BlockerIcons.Back,
+        navigationIconContentDescription = null,
+        onNavigationClick = onNavigateBack,
+        actions = {
+            TextField(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp, end = 4.dp)
+                    .padding(vertical = 4.dp)
+                    .onFocusChanged { focusState ->
+                        showClearButton = (focusState.isFocused)
+                    }
+                    .focusRequester(focusRequester),
+                value = uiState.keyword,
+                onValueChange = onSearchTextChanged,
+                placeholder = {
+                    Text(text = placeholderText)
+                },
+                trailingIcon = {
+                    AnimatedVisibility(
+                        visible = showClearButton,
+                        enter = fadeIn(),
+                        exit = fadeOut()
+                    ) {
+                        IconButton(onClick = { onClearClick() }) {
+                            Icon(
+                                imageVector = BlockerIcons.Clear,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                },
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    keyboardController?.hide()
+                }),
+            )
+        }
+    )
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+}
+
+@Composable
+@Preview
+fun GlobalSearchScreenPreview() {
+    val searchBoxUiState = SearchBoxUiState()
+    BlockerTheme {
+        Surface {
+            GlobalSearchScreen(searchBoxUiState = searchBoxUiState)
+        }
+    }
 }
