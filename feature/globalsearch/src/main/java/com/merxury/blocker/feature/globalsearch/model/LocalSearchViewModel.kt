075e4daaf2d3da3ad29e5debc9e22870ac1fa3d7
Julie Wang <julie.wang@ringcentral.com>
2023-01-09 21:32:15 +0800

    Add buttons on GlobalSearchScreen

--- feature/globalsearch/src/main/java/com/merxury/blocker/feature/globalsearch/model/LocalSearchViewModel.kt
+++ feature/globalsearch/src/main/java/com/merxury/blocker/feature/globalsearch/model/LocalSearchViewModel.kt
@@ -1,15 +1,26 @@
 package com.merxury.blocker.feature.globalsearch.model
 
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
 import com.merxury.blocker.core.database.generalrule.GeneralRuleEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 @HiltViewModel
-class LocalSearchViewModel @Inject constructor() : ViewModel()
+class LocalSearchViewModel @Inject constructor() : ViewModel() {
+    private val _searchBoxUiState = MutableStateFlow(SearchBoxUiState())
+    val searchBoxUiState: StateFlow<SearchBoxUiState> = _searchBoxUiState.asStateFlow()
+}
 
 sealed interface LocalSearchUiState {
     object Loading : LocalSearchUiState
     class LocalSearchResult(val filter: List<GeneralRuleEntity>) : LocalSearchUiState
     class Error(val message: String) : LocalSearchUiState
 }
+
+data class SearchBoxUiState(
+    val keyword: TextFieldValue = TextFieldValue()
+)
