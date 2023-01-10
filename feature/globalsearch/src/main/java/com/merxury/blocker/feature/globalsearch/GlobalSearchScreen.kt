b4bdd311d3a2253b9f62aebbffbd46f9f21e92e6
juliewww <julie.www333@gmail.com>
2023-01-10 14:52:15 +0800

    1.GlobalSearchContent 2.FilterAppItem

--- feature/globalsearch/src/main/java/com/merxury/blocker/feature/globalsearch/GlobalSearchScreen.kt
+++ feature/globalsearch/src/main/java/com/merxury/blocker/feature/globalsearch/GlobalSearchScreen.kt
@@ -3,13 +3,16 @@ package com.merxury.blocker.feature.globalsearch
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -40,9 +43,12 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.merxury.blocker.core.designsystem.component.BlockerLoadingWheel
 import com.merxury.blocker.core.designsystem.component.BlockerTopAppBar
 import com.merxury.blocker.core.designsystem.icon.BlockerIcons
 import com.merxury.blocker.core.designsystem.theme.BlockerTheme
+import com.merxury.blocker.core.ui.data.ErrorMessage
+import com.merxury.blocker.feature.globalsearch.model.LocalSearchUiState
 import com.merxury.blocker.feature.globalsearch.model.LocalSearchViewModel
 import com.merxury.blocker.feature.globalsearch.model.SearchBoxUiState
 
@@ -52,37 +58,82 @@ fun GlobalSearchRoute(
     viewModel: LocalSearchViewModel = hiltViewModel()
 ) {
     val searchBoxUiState by viewModel.searchBoxUiState.collectAsStateWithLifecycle()
-    GlobalSearchScreen(searchBoxUiState = searchBoxUiState)
+    val localSearchUiState by viewModel.localSearchUiState.collectAsStateWithLifecycle()
+    GlobalSearchScreen(
+        searchBoxUiState = searchBoxUiState,
+        localSearchUiState = localSearchUiState
+    )
 }
 
 @Composable
 fun GlobalSearchScreen(
     modifier: Modifier = Modifier,
-    searchBoxUiState: SearchBoxUiState
+    searchBoxUiState: SearchBoxUiState,
+    localSearchUiState: LocalSearchUiState
 ) {
     Column(modifier = modifier.fillMaxWidth()) {
         SearchBar(
             modifier = modifier,
             uiState = searchBoxUiState
         )
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            FilledTonalButton(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(id = R.string.application))
+        when (localSearchUiState) {
+            LocalSearchUiState.Loading -> {
+                Column(
+                    modifier = modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    BlockerLoadingWheel(
+                        modifier = modifier,
+                        contentDesc = stringResource(id = R.string.searching),
+                    )
+                }
             }
-            Spacer(modifier = modifier.width(8.dp))
-            FilledTonalButton(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(id = R.string.component))
+
+            is LocalSearchUiState.LocalSearchResult -> {
+                GlobalSearchContent(
+                    modifier = modifier,
+                    localSearchUiState = localSearchUiState
+                )
             }
-            Spacer(modifier = modifier.width(8.dp))
-            FilledTonalButton(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(id = R.string.online_rule))
+
+            is LocalSearchUiState.Error -> {
+                ErrorScreen(localSearchUiState.message)
             }
+        }
+
+    }
+}
+
+@Composable
+fun GlobalSearchContent(
+    modifier: Modifier = Modifier,
+    localSearchUiState: LocalSearchUiState.LocalSearchResult
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .horizontalScroll(rememberScrollState()),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        FilledTonalButton(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(id = R.string.application, localSearchUiState.appCount))
+        }
+        Spacer(modifier = modifier.width(8.dp))
+        FilledTonalButton(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(id = R.string.component, localSearchUiState.componentCount))
+        }
+        Spacer(modifier = modifier.width(8.dp))
+        FilledTonalButton(onClick = { /*TODO*/ }) {
+            Text(
+                text = stringResource(
+                    id = R.string.online_rule,
+                    localSearchUiState.onlineRuleCount
+                )
+            )
         }
     }
 }
@@ -149,12 +200,26 @@ fun SearchBar(
 }
 
 @Composable
+fun ErrorScreen(message: ErrorMessage) {
+    Text(text = message.message)
+}
+
+@Composable
 @Preview
 fun GlobalSearchScreenPreview() {
     val searchBoxUiState = SearchBoxUiState()
+    val localSearchUiState = LocalSearchUiState.LocalSearchResult(
+        filter = listOf(),
+        appCount = 0,
+        componentCount = 99,
+        onlineRuleCount = 6
+    )
     BlockerTheme {
         Surface {
-            GlobalSearchScreen(searchBoxUiState = searchBoxUiState)
+            GlobalSearchScreen(
+                searchBoxUiState = searchBoxUiState,
+                localSearchUiState = localSearchUiState
+            )
         }
     }
 }
--- feature/globalsearch/src/main/java/com/merxury/blocker/feature/globalsearch/model/LocalSearchViewModel.kt
+++ feature/globalsearch/src/main/java/com/merxury/blocker/feature/globalsearch/model/LocalSearchViewModel.kt
@@ -1,8 +1,9 @@
 package com.merxury.blocker.feature.globalsearch.model
 
+import android.content.pm.PackageInfo
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
-import com.merxury.blocker.core.database.generalrule.GeneralRuleEntity
+import com.merxury.blocker.core.ui.data.ErrorMessage
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,14 +14,31 @@ import kotlinx.coroutines.flow.asStateFlow
 class LocalSearchViewModel @Inject constructor() : ViewModel() {
     private val _searchBoxUiState = MutableStateFlow(SearchBoxUiState())
     val searchBoxUiState: StateFlow<SearchBoxUiState> = _searchBoxUiState.asStateFlow()
+    private val _localSearchUiState = MutableStateFlow(LocalSearchUiState.Loading)
+    val localSearchUiState: StateFlow<LocalSearchUiState> = _localSearchUiState.asStateFlow()
 }
 
 sealed interface LocalSearchUiState {
     object Loading : LocalSearchUiState
-    class LocalSearchResult(val filter: List<GeneralRuleEntity>) : LocalSearchUiState
-    class Error(val message: String) : LocalSearchUiState
+    class LocalSearchResult(
+        val filter: List<FilterAppItem>,
+        val appCount: Int,
+        val componentCount: Int,
+        val onlineRuleCount: Int
+    ) : LocalSearchUiState
+
+    class Error(val message: ErrorMessage) : LocalSearchUiState
 }
 
 data class SearchBoxUiState(
     val keyword: TextFieldValue = TextFieldValue()
+)
+
+data class FilterAppItem(
+    val label: String = "",
+    val packageInfo: PackageInfo?,
+    val activityCount: Int = 0,
+    val broadcastCount: Int = 0,
+    val serviceCount: Int = 0,
+    val contentProviderCount: Int = 0,
 )
