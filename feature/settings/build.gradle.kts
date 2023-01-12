fdec517f1b9af4a704e40974eb1c23468aeae269
lihenggui <lihenggui@users.noreply.github.com>
2023-01-12 15:00:50 -0800

    Update copyrights for features
    Change-Id: Ic3c12200640926d989e4ee0ab8ae441e96185449

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -1,6 +1,5 @@
 /*
- * Copyright 2022 Blocker
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2023 Blocker
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
