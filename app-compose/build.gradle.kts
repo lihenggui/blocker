bf111a3fb2f31c69981c69238485aa3cac2e6419
lihenggui <lihenggui@users.noreply.github.com>
2023-01-17 15:43:42 -0800

    Android-CI workflow -> GMD refactor
    Change-Id: I21881be3425338dcd7c98b577241e9dbddb80452

--- app-compose/build.gradle.kts
+++ app-compose/build.gradle.kts
@@ -14,6 +14,7 @@
  *   limitations under the License.
  */
 
+import com.android.build.api.dsl.ManagedVirtualDevice
 import com.merxury.blocker.BlockerBuildType
 
 // TODO: Remove once https://youtrack.jetbrains.com/issue/KTIJ-19369 is fixed
@@ -83,7 +84,7 @@ android {
         // TODO: Convert it as a convention plugin once Flamingo goes out (https://github.com/android/nowinandroid/issues/523)
         managedDevices {
             devices {
-                maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixel4api30").apply {
+                maybeCreate<ManagedVirtualDevice>("pixel4api30").apply {
                     device = "Pixel 4"
                     apiLevel = 30
                     // ATDs currently support only API level 30.
