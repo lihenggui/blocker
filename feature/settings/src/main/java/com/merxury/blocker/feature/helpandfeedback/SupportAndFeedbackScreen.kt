affb23251f905c2ceae0bf9833c12e8e0ac8f7a2
juliewww <julie.www333@gmail.com>
2023-01-28 18:42:31 +0800

    Combine AppTopBar

--- feature/settings/src/main/java/com/merxury/blocker/feature/helpandfeedback/SupportAndFeedbackScreen.kt
+++ feature/settings/src/main/java/com/merxury/blocker/feature/helpandfeedback/SupportAndFeedbackScreen.kt
@@ -28,12 +28,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.merxury.blocker.core.designsystem.component.BlockerTopAppBar
@@ -54,8 +58,16 @@ fun SupportAndFeedbackRoute(
     Scaffold(
         topBar = {
             BlockerTopAppBar(
-                titleRes = string.support_and_feedback,
-                onNavigationClick = onNavigationClick,
+                title = stringResource(id = string.support_and_feedback),
+                navigationIcon = {
+                    IconButton(onClick = onNavigationClick) {
+                        Icon(
+                            imageVector = BlockerIcons.Back,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onSurface,
+                        )
+                    }
+                },
             )
         },
     ) { padding ->
