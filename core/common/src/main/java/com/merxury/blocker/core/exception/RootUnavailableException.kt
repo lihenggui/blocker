7509654e2eb3ad8715d8e975a8af1b225116d141
lihenggui <lihenggui@users.noreply.github.com>
2023-02-03 20:53:43 -0800

    Move RootUnavailableException to common
    Change-Id: I7795507068734f6411671aae7d7953c3f2cf5504

--- core/common/src/main/java/com/merxury/blocker/core/exception/RootUnavailableException.kt
+++ core/common/src/main/java/com/merxury/blocker/core/exception/RootUnavailableException.kt
@@ -14,6 +14,6 @@
  * limitations under the License.
  */
 
-package com.merxury.ifw.exception
+package com.merxury.blocker.core.exception
 
 class RootUnavailableException : RuntimeException("Root unavailable")
