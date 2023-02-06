411801a03876d91caf1c6d4e9fd0bee90fd22c61
lihenggui <lihenggui@users.noreply.github.com>
2023-02-06 12:12:18 -0800

    Rename module OnlineRules to GeneralRule
    Change-Id: Iaffcc8feeac6b30447859f4383d7be451e1f2466

--- app-compose/src/main/java/com/merxury/blocker/navigation/TopLevelDestination.kt
+++ app-compose/src/main/java/com/merxury/blocker/navigation/TopLevelDestination.kt
@@ -39,9 +39,9 @@ enum class TopLevelDestination(
         iconTextId = R.string.apps,
         titleTextId = R.string.app_name,
     ),
-    ONLINE_RULES(
-        selectedIcon = ImageVectorIcon(BlockerIcons.OnlineRules),
-        unselectedIcon = ImageVectorIcon(BlockerIcons.OnlineRules),
+    GENERAL_RULE(
+        selectedIcon = ImageVectorIcon(BlockerIcons.GeneralRule),
+        unselectedIcon = ImageVectorIcon(BlockerIcons.GeneralRule),
         iconTextId = R.string.rules,
         titleTextId = R.string.rules,
     ),
--- feature/generalrule/build.gradle.kts
+++ feature/generalrule/build.gradle.kts
@@ -1,5 +1,6 @@
 /*
  * Copyright 2023 Blocker
+ * Copyright 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,22 +15,14 @@
  * limitations under the License.
  */
 
-package com.merxury.blocker.feature.generalrules.navigation
-
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
-import androidx.navigation.compose.composable
-import com.merxury.blocker.feature.generalrules.GeneralRulesRoute
-
-const val generalRuleRoute = "online_rule_route"
-
-fun NavController.navigateToOnlineRules(navOptions: NavOptions? = null) {
-    this.navigate(generalRuleRoute, navOptions)
+plugins {
+    id("blocker.android.feature")
+    id("blocker.android.library.compose")
+    id("blocker.android.library.jacoco")
 }
-
-fun NavGraphBuilder.onlineRulesScreen() {
-    composable(route = generalRuleRoute) {
-        GeneralRulesRoute()
-    }
+android {
+    namespace = "com.merxury.blocker.feature.generalrule"
+}
+dependencies {
+    implementation(project(":core:database"))
 }
