5d5ed3db55da22fae56f742f9517d4c1d42356f6
lihenggui <lihenggui@users.noreply.github.com>
2023-02-10 11:12:39 -0800

    Add exception handler for the component list more menu
    Change-Id: I96bd8d92d45906bb21802dbb83cc14c07b21d3d5

--- feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/cmplist/ComponentListViewModel.kt
+++ feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/cmplist/ComponentListViewModel.kt
@@ -23,6 +23,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.merxury.blocker.core.data.respository.component.LocalComponentRepository
 import com.merxury.blocker.core.data.respository.userdata.UserDataRepository
+import com.merxury.blocker.core.dispatchers.BlockerDispatchers.IO
+import com.merxury.blocker.core.dispatchers.Dispatcher
 import com.merxury.blocker.core.extension.exec
 import com.merxury.blocker.core.model.ComponentType
 import com.merxury.blocker.core.model.data.ComponentInfo
@@ -37,6 +39,8 @@ import com.merxury.blocker.feature.appdetail.cmplist.ComponentListUiState.Succes
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -52,11 +56,16 @@ class ComponentListViewModel @AssistedInject constructor(
     private val componentRepository: LocalComponentRepository,
     @Assisted private val packageName: String,
     @Assisted private val type: ComponentType,
+    @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
 ) : ViewModel() {
     private val _uiState: MutableStateFlow<ComponentListUiState> = MutableStateFlow(Loading)
     val uiState: StateFlow<ComponentListUiState> = _uiState.asStateFlow()
     private val _errorState = MutableStateFlow<ErrorMessage?>(null)
     val errorState = _errorState.asStateFlow()
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Timber.e(throwable)
+        _errorState.tryEmit(throwable.toErrorMessage())
+    }
     private val stateList = mutableStateListOf<ComponentInfo>()
 
     init {
@@ -69,13 +78,13 @@ class ComponentListViewModel @AssistedInject constructor(
     }
 
     fun launchActivity(packageName: String, componentName: String) {
-        viewModelScope.launch {
+        viewModelScope.launch(ioDispatcher + exceptionHandler) {
             "am start -n $packageName/$componentName".exec()
         }
     }
 
     fun stopService(packageName: String, componentName: String) {
-        viewModelScope.launch {
+        viewModelScope.launch(ioDispatcher + exceptionHandler) {
             "am stopservice $packageName/$componentName".exec()
         }
     }
