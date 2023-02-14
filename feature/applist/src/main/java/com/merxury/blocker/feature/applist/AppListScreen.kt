33757c3e0a95eeb9ed9ae5e0f8d8b4e3bdd7ddcc
lihenggui <lihenggui@users.noreply.github.com>
2023-02-13 19:54:56 -0800

    Remove deprecations
    Change-Id: If53880140a4717a31953506a5002f90b1a82987a

--- feature/applist/src/main/java/com/merxury/blocker/feature/applist/AppListScreen.kt
+++ feature/applist/src/main/java/com/merxury/blocker/feature/applist/AppListScreen.kt
@@ -22,7 +22,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.consumedWindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
@@ -122,7 +122,7 @@ fun AppListScreen(
             modifier = modifier
                 .fillMaxSize()
                 .padding(top = padding.calculateTopPadding())
-                .consumedWindowInsets(padding)
+                .consumeWindowInsets(padding)
                 .windowInsetsPadding(
                     WindowInsets.safeDrawing.only(
                         WindowInsetsSides.Horizontal,
