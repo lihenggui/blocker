a03639935eada195413e13b0b00f70a84eaf562e
lihenggui <lihenggui@users.noreply.github.com>
2023-02-18 09:08:11 -0800

    Optimize labels in the InitializingScreen
    Change-Id: I0b425c75ff14a424bf8a8562f44a9c37a7a06da5

--- feature/search/src/main/java/com/merxury/blocker/feature/search/screen/InitializingScreen.kt
+++ feature/search/src/main/java/com/merxury/blocker/feature/search/screen/InitializingScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.merxury.blocker.core.designsystem.component.BlockerLoadingWheel
@@ -45,6 +46,8 @@ fun InitializingScreen(processingName: String) {
             text = processingName,
             color = MaterialTheme.colorScheme.outline,
             style = MaterialTheme.typography.bodyLarge,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
         )
     }
 }
