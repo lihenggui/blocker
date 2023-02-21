27b1220f5ee3dddd4f370f83399953cdb848201f
lihenggui <lihenggui@users.noreply.github.com>
2023-02-21 11:03:20 -0800

    Make Hilt provide Firebase.analytics
    Change-Id: I3e70a1aa2500077a49a5dd0e681c958b708deaa4

--- core/analytics/src/market/java/com/merxury/blocker/core/analytics/AnalyticsModule.kt
+++ core/analytics/src/market/java/com/merxury/blocker/core/analytics/AnalyticsModule.kt
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2023 Blocker
  * Copyright 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,14 +17,25 @@
 
 package com.merxury.blocker.core.analytics
 
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class AnalyticsModule {
     @Binds
     abstract fun bindsAnalyticsHelper(analyticsHelperImpl: FirebaseAnalyticsHelper): AnalyticsHelper
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideFirebaseAnalytics(): FirebaseAnalytics { return Firebase.analytics }
+    }
 }
