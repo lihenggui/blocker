779dbe8ea37c973807ab6a92257d335d1b516dab
juliewww <julie.www333@gmail.com>
2023-02-23 19:22:31 +0800

    Change text style on empty screen

--- feature/search/src/main/java/com/merxury/blocker/feature/search/screen/SearchingScreen.kt
+++ feature/search/src/main/java/com/merxury/blocker/feature/search/screen/SearchingScreen.kt
@@ -20,13 +20,14 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.merxury.blocker.core.designsystem.component.BlockerBodyMediumText
+import com.merxury.blocker.core.designsystem.component.BlockerBodyLargeText
 import com.merxury.blocker.core.designsystem.component.BlockerLoadingWheel
 import com.merxury.blocker.core.designsystem.theme.BlockerTheme
 import com.merxury.blocker.feature.search.R.string
@@ -39,8 +40,9 @@ fun SearchingScreen() {
         verticalArrangement = Arrangement.Center,
     ) {
         BlockerLoadingWheel(contentDesc = stringResource(id = string.searching))
-        BlockerBodyMediumText(
+        BlockerBodyLargeText(
             text = stringResource(id = string.searching),
+            color = MaterialTheme.colorScheme.outline,
         )
     }
 }
