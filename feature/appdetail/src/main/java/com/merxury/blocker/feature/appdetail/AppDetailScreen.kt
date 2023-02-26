333d9b6f02cb100f8a12d4dabeda862f517d9cfd
lihenggui <lihenggui@users.noreply.github.com>
2023-02-26 10:15:33 -0800

    Move error state to AppDetail
    Change-Id: Ib48bece17b5e4806e3282c917372ad9c479be21e

--- feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/AppDetailScreen.kt
+++ feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/AppDetailScreen.kt
@@ -53,6 +53,7 @@ import androidx.compose.ui.unit.Velocity
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.merxury.blocker.core.designsystem.component.BlockerCollapsingTopAppBar
+import com.merxury.blocker.core.designsystem.component.BlockerErrorAlertDialog
 import com.merxury.blocker.core.designsystem.component.BlockerScrollableTabRow
 import com.merxury.blocker.core.designsystem.component.BlockerTab
 import com.merxury.blocker.core.designsystem.component.MaxToolbarHeight
@@ -91,6 +92,7 @@ fun AppDetailRoute(
 ) {
     val tabState by viewModel.tabState.collectAsStateWithLifecycle()
     val uiState by viewModel.appInfoUiState.collectAsStateWithLifecycle()
+    val errorState by viewModel.errorState.collectAsStateWithLifecycle()
     val topAppBarUiState by viewModel.appBarUiState.collectAsStateWithLifecycle()
 
     AppDetailScreen(
@@ -110,6 +112,13 @@ fun AppDetailRoute(
         onImportIfw = viewModel::importIfw,
         onResetIfw = viewModel::resetIfw,
     )
+    if (errorState != null) {
+        BlockerErrorAlertDialog(
+            title = errorState?.message.orEmpty(),
+            text = errorState?.stackTrace.orEmpty(),
+            onDismissRequest = viewModel::dismissAlert,
+        )
+    }
 }
 
 @Composable
