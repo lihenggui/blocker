cb826122bc9b6c1139507d7dea9976643546da06
lihenggui <lihenggui@users.noreply.github.com>
2023-03-03 12:12:28 -0800

    Implement export & import rules for single application
    Change-Id: I9c43eccc1748731617d4d8093cd67e2d7f1478c9

--- core/rule/src/main/java/com/merxury/blocker/core/rule/work/ResetIfwWorker.kt
+++ core/rule/src/main/java/com/merxury/blocker/core/rule/work/ResetIfwWorker.kt
@@ -30,6 +30,7 @@ import androidx.work.workDataOf
 import com.merxury.blocker.core.dispatchers.BlockerDispatchers.IO
 import com.merxury.blocker.core.dispatchers.Dispatcher
 import com.merxury.blocker.core.rule.R
+import com.merxury.blocker.core.rule.Rule
 import com.merxury.blocker.core.rule.entity.RuleWorkResult.MISSING_ROOT_PERMISSION
 import com.merxury.blocker.core.rule.entity.RuleWorkResult.UNEXPECTED_EXCEPTION
 import com.merxury.blocker.core.rule.util.NotificationUtil
@@ -55,6 +56,10 @@ class ResetIfwWorker @AssistedInject constructor(
 
     override suspend fun doWork(): Result = withContext(ioDispatcher) {
         Timber.i("Clear IFW rules")
+        val packageName = inputData.getString(PARAM_RESET_PACKAGE_NAME)
+        if (!packageName.isNullOrEmpty()) {
+            return@withContext clearIfwRuleForPackage(packageName)
+        }
         var count = 0
         val total: Int
         try {
@@ -77,7 +82,7 @@ class ResetIfwWorker @AssistedInject constructor(
                 workDataOf(PARAM_WORK_RESULT to MISSING_ROOT_PERMISSION),
             )
         } catch (e: IOException) {
-            Timber.e(e, "Failed to clear IFW rules, IO exception occured")
+            Timber.e(e, "Failed to clear IFW rules, IO exception occurred")
             return@withContext Result.failure(
                 workDataOf(PARAM_WORK_RESULT to UNEXPECTED_EXCEPTION),
             )
@@ -86,6 +91,34 @@ class ResetIfwWorker @AssistedInject constructor(
         return@withContext Result.success(
             workDataOf(PARAM_CLEAR_COUNT to count),
         )
+    }
+
+    private suspend fun clearIfwRuleForPackage(packageName: String): Result {
+        try {
+            Timber.d("Start clearing IFW rules for package $packageName")
+            val ifwFolder = IfwStorageUtils.getIfwFolder()
+            val files = FileUtils.listFiles(ifwFolder)
+            if (files.contains(packageName + Rule.IFW_EXTENSION)) {
+                updateNotification(packageName, 1, 1)
+                Timber.d("Delete IFW rules for $packageName")
+                FileUtils.delete(
+                    path = ifwFolder + packageName,
+                    recursively = false,
+                    dispatcher = ioDispatcher,
+                )
+            }
+        } catch (e: RuntimeException) {
+            Timber.e(e, "Failed to clear IFW rules")
+            return Result.failure(
+                workDataOf(PARAM_WORK_RESULT to MISSING_ROOT_PERMISSION),
+            )
+        } catch (e: IOException) {
+            Timber.e(e, "Failed to clear IFW rules, IO exception occurred")
+            return Result.failure(
+                workDataOf(PARAM_WORK_RESULT to UNEXPECTED_EXCEPTION),
+            )
+        }
+        return Result.success(workDataOf(PARAM_CLEAR_COUNT to 1))
     }
 
     private fun updateNotification(name: String, current: Int, total: Int): ForegroundInfo {
@@ -114,8 +147,12 @@ class ResetIfwWorker @AssistedInject constructor(
     companion object {
         const val PARAM_CLEAR_COUNT = "param_clear_count"
         const val PARAM_WORK_RESULT = "param_work_result"
+        private const val PARAM_RESET_PACKAGE_NAME = "param_reset_package_name"
 
-        fun clearIfwWork() = OneTimeWorkRequestBuilder<ResetIfwWorker>()
+        fun clearIfwWork(packageName: String? = null) = OneTimeWorkRequestBuilder<ResetIfwWorker>()
+            .setInputData(
+                workDataOf(PARAM_RESET_PACKAGE_NAME to packageName),
+            )
             .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
             .build()
     }
