c16d3fa6b930f5efde3c57f79ef4a536342646c8
lihenggui <lihenggui@users.noreply.github.com>
2023-03-17 15:59:29 -0700

    Remove inclusive to avoid creating VM repeatedly
    Change-Id: I5954240bcc6f5ca63ab8936d170a323c14ab415b

--- app-compose/src/main/java/com/merxury/blocker/ui/BlockerAppState.kt
+++ app-compose/src/main/java/com/merxury/blocker/ui/BlockerAppState.kt
@@ -115,7 +115,6 @@ class BlockerAppState(
                 // on the back stack as users select items
                 popUpTo(navController.graph.findStartDestination().id) {
                     saveState = true
-                    inclusive = true
                 }
                 // Avoid multiple copies of the same destination when
                 // reselecting the same item
