55a64f7bcb4336717e3a744bd23c4eeaab4d7c61
lihenggui <lihenggui@users.noreply.github.com>
2023-03-18 08:58:57 -0700

    Adjust style for alert dialog
    Change-Id: I1c87a42ed816d25c346423940de79b3496bed667

--- core/designsystem/src/main/java/com/merxury/blocker/core/designsystem/component/AlertDialog.kt
+++ core/designsystem/src/main/java/com/merxury/blocker/core/designsystem/component/AlertDialog.kt
@@ -16,16 +16,14 @@
 
 package com.merxury.blocker.core.designsystem.component
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun BlockerErrorAlertDialog(
@@ -37,7 +35,9 @@ fun BlockerErrorAlertDialog(
     val scrollState = rememberScrollState(0)
     AlertDialog(
         modifier = modifier,
-        title = { Text(text = title) },
+        title = {
+            Text(text = title)
+        },
         text = {
             Text(
                 modifier = Modifier.verticalScroll(scrollState),
@@ -46,12 +46,13 @@ fun BlockerErrorAlertDialog(
         },
         onDismissRequest = onDismissRequest,
         confirmButton = {
-            Text(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .clickable { onDismissRequest() },
-                text = stringResource(id = android.R.string.ok),
-            )
+            TextButton(
+                onClick = onDismissRequest,
+            ) {
+                Text(
+                    text = stringResource(id = android.R.string.ok),
+                )
+            }
         },
     )
 }
@@ -69,7 +70,7 @@ fun BlockerWarningAlertDialog(
         modifier = modifier,
         title = {
             Text(
-                modifier = Modifier.padding(vertical = 16.dp),
+                modifier = Modifier,
                 text = title,
             )
         },
@@ -81,23 +82,23 @@ fun BlockerWarningAlertDialog(
         },
         onDismissRequest = onDismissRequest,
         confirmButton = {
-            Text(
-                modifier = Modifier
-                    .clickable {
-                        onConfirmRequest()
-                        onDismissRequest()
-                    }
-                    .padding(16.dp),
-                text = stringResource(id = android.R.string.ok),
-            )
+            TextButton(
+                onClick = {
+                    onConfirmRequest()
+                    onDismissRequest()
+                },
+            ) {
+                Text(
+                    text = stringResource(id = android.R.string.ok),
+                )
+            }
         },
         dismissButton = {
-            Text(
-                modifier = Modifier
-                    .clickable { onDismissRequest() }
-                    .padding(16.dp),
-                text = stringResource(id = android.R.string.cancel),
-            )
+            TextButton(onClick = onDismissRequest) {
+                Text(
+                    text = stringResource(id = android.R.string.cancel),
+                )
+            }
         },
     )
 }
