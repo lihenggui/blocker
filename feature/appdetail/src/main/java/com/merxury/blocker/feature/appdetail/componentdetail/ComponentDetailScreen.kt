5f75bfccf5e954dfec6155502c9d48083b28b704
lihenggui <lihenggui@users.noreply.github.com>
2023-04-14 15:01:05 -0700

    WIP: Remember values in the component dialog
    Change-Id: I40245433838e45b8940231c3d22792b8bcb2f427

--- feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/componentdetail/ComponentDetailScreen.kt
+++ feature/appdetail/src/main/java/com/merxury/blocker/feature/appdetail/componentdetail/ComponentDetailScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.merxury.blocker.core.model.data.ComponentDetail
 import com.merxury.blocker.core.ui.screen.ErrorScreen
 import com.merxury.blocker.core.ui.screen.LoadingScreen
 import com.merxury.blocker.feature.appdetail.componentdetail.ComponentDetailUiState.Success
@@ -45,14 +46,20 @@ fun ComponentDetailScreen(
     uiState: ComponentDetailUiState,
     modifier: Modifier = Modifier,
     dismissHandler: () -> Unit = {},
-    onSaveDetailClick: (UserEditableComponentDetail) -> Unit = {},
+    onSaveDetailClick: (ComponentDetail) -> Unit = {},
 ) {
     when (uiState) {
         is ComponentDetailUiState.Loading -> LoadingScreen()
         is ComponentDetailUiState.Error -> ErrorScreen(error = uiState.message)
         is Success -> ComponentDetailContent(
             name = uiState.detail.name,
-            detail = uiState.detail,
+            belongToSdk = uiState.detail.sdkName.isNullOrEmpty().not(),
+            sdkName = uiState.detail.sdkName,
+            description = uiState.detail.description,
+            disableEffect = uiState.detail.disableEffect,
+            contributor = uiState.detail.contributor,
+            addedVersion = uiState.detail.addedVersion,
+            recommendToBlock = uiState.detail.recommendToBlock,
             modifier = modifier,
             dismissHandler = dismissHandler,
             onSaveDetailClick = onSaveDetailClick,
@@ -63,15 +70,27 @@ fun ComponentDetailScreen(
 @Composable
 fun ComponentDetailContent(
     name: String,
-    detail: UserEditableComponentDetail,
     modifier: Modifier = Modifier,
+    belongToSdk: Boolean = false,
+    sdkName: String? = null,
+    description: String? = null,
+    disableEffect: String? = null,
+    contributor: String? = null,
+    addedVersion: String? = null,
+    recommendToBlock: Boolean = false,
     dismissHandler: () -> Unit = {},
-    onSaveDetailClick: (UserEditableComponentDetail) -> Unit = {},
+    onSaveDetailClick: (ComponentDetail) -> Unit = {},
 ) {
     ComponentDetailDialog(
-        name = name,
-        detail = detail,
         modifier = modifier,
+        name = name,
+        belongToSdk = belongToSdk,
+        sdkName = sdkName,
+        description = description,
+        disableEffect = disableEffect,
+        contributor = contributor,
+        addedVersion = addedVersion,
+        recommendToBlock = recommendToBlock,
         onDismiss = dismissHandler,
         onSaveDetailClick = onSaveDetailClick,
     )
