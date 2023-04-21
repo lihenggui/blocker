b6c9ed4977a510872b64559c3066ef14fa94d58e
lihenggui <350699171@qq.com>
2023-04-21 10:15:24 -0700

    Downgrade AGP to 8.0.0

--- build-logic/convention/src/main/kotlin/com/merxury/blocker/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/merxury/blocker/KotlinAndroid.kt
@@ -17,6 +17,7 @@
 package com.merxury.blocker
 
 import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
@@ -31,7 +32,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
  * Configure base Kotlin with Android options
  */
 internal fun Project.configureKotlinAndroid(
-    commonExtension: CommonExtension<*, *, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *>,
 ) {
     commonExtension.apply {
         compileSdk = 33
@@ -41,12 +42,16 @@ internal fun Project.configureKotlinAndroid(
         }
 
         compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
             isCoreLibraryDesugaringEnabled = true
         }
     }
     // Use withType to workaround https://youtrack.jetbrains.com/issue/KT-55947
     tasks.withType<KotlinCompile>().configureEach {
         kotlinOptions {
+            // Set JVM target to 17
+            jvmTarget = JavaVersion.VERSION_17.toString()
             // Treat all Kotlin warnings as errors (disabled by default)
             // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
             val warningsAsErrors: String? by project
