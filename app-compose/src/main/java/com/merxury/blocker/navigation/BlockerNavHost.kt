7942e40e397be19d9d5fb96c80aaa028b84f6b3c
lihenggui <350699171@qq.com>
2023-04-30 09:02:33 -0700

    Rename project componensort -> sort

--- app-compose/src/main/java/com/merxury/blocker/navigation/BlockerNavHost.kt
+++ app-compose/src/main/java/com/merxury/blocker/navigation/BlockerNavHost.kt
@@ -28,8 +28,8 @@ import com.merxury.blocker.feature.appdetail.navigation.navigateToAppDetail
 import com.merxury.blocker.feature.appdetail.navigation.navigateToComponentDetail
 import com.merxury.blocker.feature.applist.navigation.appListRoute
 import com.merxury.blocker.feature.applist.navigation.appListScreen
-import com.merxury.blocker.feature.componentsort.navigation.componentSortScreen
-import com.merxury.blocker.feature.componentsort.navigation.navigateToComponentSortScreen
+import com.merxury.blocker.feature.sort.navigation.componentSortScreen
+import com.merxury.blocker.feature.sort.navigation.navigateToComponentSortScreen
 import com.merxury.blocker.feature.generalrules.navigation.generalRuleScreen
 import com.merxury.blocker.feature.helpandfeedback.navigation.navigateToSupportAndFeedback
 import com.merxury.blocker.feature.helpandfeedback.navigation.supportAndFeedbackScreen
