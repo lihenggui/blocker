7e66b3fb6705f8d49b58a9f1cb874dcc7acd94d4
lihenggui <350699171@qq.com>
2023-06-15 20:59:49 -0700

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,6 +32,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+        maven("https://androidx.dev/storage/compose-compiler/repository/")
     }
 }
 rootProject.name = "Blocker"
