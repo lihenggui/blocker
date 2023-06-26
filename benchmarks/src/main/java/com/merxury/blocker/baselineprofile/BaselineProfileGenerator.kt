8edb43d8b2631003edda0a9bcdf64d38a7b83d77
lihenggui <lihenggui@users.noreply.github.com>
2023-06-26 12:14:51 -0700

    Baseline Profiles API change
    Change-Id: I5ffb6a41c88a973c07907432f249447a07890cca

--- benchmarks/src/main/java/com/merxury/blocker/baselineprofile/BaselineProfileGenerator.kt
+++ benchmarks/src/main/java/com/merxury/blocker/baselineprofile/BaselineProfileGenerator.kt
@@ -16,7 +16,6 @@
 
 package com.merxury.blocker.baselineprofile
 
-import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import com.merxury.blocker.PACKAGE_NAME
 import com.merxury.blocker.applist.appListScrollDownUp
@@ -30,13 +29,12 @@ import org.junit.Test
 /**
  * Generates a baseline profile which can be copied to `app/src/main/baseline-prof.txt`.
  */
-@ExperimentalBaselineProfilesApi
 class BaselineProfileGenerator {
     @get:Rule val baselineProfileRule = BaselineProfileRule()
 
     @Test
     fun generate() =
-        baselineProfileRule.collectBaselineProfile(PACKAGE_NAME) {
+        baselineProfileRule.collect(PACKAGE_NAME) {
             // This block defines the app's critical user journey. Here we are interested in
             // optimizing for app startup. But you can also navigate and scroll
             // through your most important UI.
