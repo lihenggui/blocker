43a59102e2c0a1a14259be7fbc24f1ea62f06526
lihenggui <lihenggui@users.noreply.github.com>
2023-06-26 11:07:09 -0700

    Android Lint improvements
    Change-Id: Iacbb8a3b90688e9a3b84dbf4c34f5b819a5267d0

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -32,6 +32,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.android.application")
                 apply("org.jetbrains.kotlin.android")
+                apply("blocker.android.lint")
             }
             extensions.configure<ApplicationExtension> {
                 configureKotlinAndroid(this)
