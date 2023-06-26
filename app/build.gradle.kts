944fe91981f1ed5b7b61f03799370db9a5b70f58
lihenggui <lihenggui@users.noreply.github.com>
2023-06-26 10:48:58 -0700

    Remove unnecessary resolutionStrategy
    Change-Id: I790e6022659b009f0bc2810e182f69f466987353

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -160,12 +160,3 @@ dependencies {
     implementation(libs.shizuku.api)
     implementation(libs.shizuku.provider)
 }
-
-// androidx.test is forcing JUnit, 4.12. This forces it to use 4.13
-configurations.configureEach {
-    resolutionStrategy {
-        force(libs.junit4)
-        // Temporary workaround for https://issuetracker.google.com/174733673
-        force("org.objenesis:objenesis:2.6")
-    }
-}
