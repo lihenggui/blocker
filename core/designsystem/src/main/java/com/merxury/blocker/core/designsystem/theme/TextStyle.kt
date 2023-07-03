a95126d32f2486df4075ada4d238a4562e138bc3
lihenggui <350699171@qq.com>
2023-07-02 21:51:37 -0700

    Use Roboto condensed font to show component descriptions

---
+++ core/designsystem/src/main/java/com/merxury/blocker/core/designsystem/theme/TextStyle.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.core.designsystem.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.merxury.blocker.core.designsystem.R
+
+@Composable
+fun TextStyle.condensedRegular() = this.copy(
+    fontFamily = FontFamily(Font(R.font.roboto_condensed_regular)),
+)
