b1ba21bc003f769c181dc83b2e5b8c7d1413389d
lihenggui <350699171@qq.com>
2023-07-14 21:58:41 -0700

    Add correct annotation for entities, and add tests

---
+++ core/ifw-api/src/test/java/com/merxury/core/ifw/IntentFirewallUnitTest.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.merxury.core.ifw
+
+import com.merxury.core.ifw.Component.Activity
+import com.merxury.core.ifw.Component.Broadcast
+import com.merxury.core.ifw.Component.Service
+import kotlinx.serialization.decodeFromString
+import nl.adaptivity.xmlutil.ExperimentalXmlUtilApi
+import nl.adaptivity.xmlutil.serialization.XML
+import org.junit.Test
+class IntentFirewallUnitTest {
+
+    @OptIn(ExperimentalXmlUtilApi::class)
+    private val xml = XML {
+        policy = JacksonPolicy
+    }
+
+    @Test
+    fun verifyXmlDeserializer() {
+        val fileContent = """
+            <rules>
+               <activity block="true" log="false">
+                  <component-filter name="com.example/com.example.MainActivity"/>
+               </activity>
+               <broadcast block="true" log="false">
+                  <component-filter name="com.example/com.example.AlarmReceiver"/>
+                  <component-filter name="com.example/com.example.BootReceiver"/>
+                  <component-filter name="com.example/com.example.NotifierReceiver"/>
+                  <component-filter name="com.example/com.example.TimeZoneReceiver"/>
+               </broadcast>
+               <service block="true" log="false">
+                  <component-filter name="com.example/com.example.DaemonService"/>
+                  <component-filter name="com.example/com.example.ForwardService"/>
+                  <component-filter name="com.example/com.example.VpnService"/>
+               </service>
+            </rules>
+        """.trimIndent()
+        val deserializedRule = xml.decodeFromString<Rules>(fileContent)
+        val targetRule = Rules(
+            activity = Activity(
+                componentFilter = listOf(
+                    ComponentFilter("com.example/com.example.MainActivity"),
+                ),
+            ),
+            service = Service(
+                componentFilter = listOf(
+                    ComponentFilter("com.example/com.example.DaemonService"),
+                    ComponentFilter("com.example/com.example.ForwardService"),
+                    ComponentFilter("com.example/com.example.VpnService"),
+                ),
+            ),
+            broadcast = Broadcast(
+                componentFilter = listOf(
+                    ComponentFilter("com.example/com.example.AlarmReceiver"),
+                    ComponentFilter("com.example/com.example.BootReceiver"),
+                    ComponentFilter("com.example/com.example.NotifierReceiver"),
+                    ComponentFilter("com.example/com.example.TimeZoneReceiver"),
+                ),
+            ),
+        )
+        assert(deserializedRule == targetRule)
+    }
+}
