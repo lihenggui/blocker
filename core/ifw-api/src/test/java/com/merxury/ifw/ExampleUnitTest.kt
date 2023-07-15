293a47e07276f15dbab6c7463a7d4633909db25e
lihenggui <350699171@qq.com>
2023-07-14 18:06:15 -0700

    Convert UnitTest to Kotlin

--- core/ifw-api/src/test/java/com/merxury/ifw/ExampleUnitTest.kt
+++ core/ifw-api/src/test/java/com/merxury/ifw/ExampleUnitTest.kt
@@ -13,21 +13,19 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+package com.merxury.ifw
 
-package com.merxury.ifw;
-
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-public class ExampleUnitTest {
+class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, (2 + 2).toLong())
     }
 }
