4004f8bacee8fd3a8b0ff6d030deed89637eb55e
lihenggui <350699171@qq.com>
2023-07-14 18:07:00 -0700

    Move tests to correct package

--- core/ifw-api/src/test/java/com/merxury/core/ifw/ExampleUnitTest.kt
+++ core/ifw-api/src/test/java/com/merxury/core/ifw/ExampleUnitTest.kt
@@ -13,7 +13,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-package com.merxury.ifw
+package com.merxury.core.ifw
 
 import org.junit.Assert
 import org.junit.Test
