afface03b601c5d1f615eba3edb5a6732b8cd11a
lihenggui <lihenggui@users.noreply.github.com>
2023-08-11 11:11:23 -0700

    Remove google occurrence in fdroid branch
    Change-Id: I64e10d9a0a526d2e67bf90f1d338a54fc44bf42d

--- core/analytics/src/market/java/com/merxury/blocker/core/analytics/AnalyticsModule.kt
+++ core/analytics/src/market/java/com/merxury/blocker/core/analytics/AnalyticsModule.kt
@@ -17,9 +17,6 @@
 
 package com.merxury.blocker.core.analytics
 
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -32,10 +29,4 @@ import javax.inject.Singleton
 abstract class AnalyticsModule {
     @Binds
     abstract fun bindsAnalyticsHelper(analyticsHelperImpl: FirebaseAnalyticsHelper): AnalyticsHelper
-
-    companion object {
-        @Provides
-        @Singleton
-        fun provideFirebaseAnalytics(): FirebaseAnalytics { return Firebase.analytics }
-    }
 }
--- core/analytics/src/market/java/com/merxury/blocker/core/analytics/FirebaseAnalyticsHelper.kt
+++ core/analytics/src/market/java/com/merxury/blocker/core/analytics/FirebaseAnalyticsHelper.kt
@@ -16,26 +16,14 @@
 
 package com.merxury.blocker.core.analytics
 
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.logEvent
 import javax.inject.Inject
 
 /**
  * Implementation of `AnalyticsHelper` which logs events to a Firebase backend.
  */
-class FirebaseAnalyticsHelper @Inject constructor(
-    private val firebaseAnalytics: FirebaseAnalytics,
-) : AnalyticsHelper {
+class FirebaseAnalyticsHelper @Inject constructor() : AnalyticsHelper {
 
     override fun logEvent(event: AnalyticsEvent) {
-        firebaseAnalytics.logEvent(event.type) {
-            for (extra in event.extras) {
-                // Truncate parameter keys and values according to firebase maximum length values.
-                param(
-                    key = extra.key.take(40),
-                    value = extra.value.take(100),
-                )
-            }
-        }
+
     }
 }
