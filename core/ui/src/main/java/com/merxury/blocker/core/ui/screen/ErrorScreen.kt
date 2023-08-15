771a97763ce15180ea20b64ba729c648febc51e6
julie.wang <julie.www333@gmail.com>
2023-08-15 15:54:19 +0800

    AppDetailScreenTest: circularProgressIndicator_whenScreenIsLoading_exists & errorIndicator_whenFailToLoadInfo_exists

--- core/ui/src/main/java/com/merxury/blocker/core/ui/screen/ErrorScreen.kt
+++ core/ui/src/main/java/com/merxury/blocker/core/ui/screen/ErrorScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.merxury.blocker.core.designsystem.bottomsheet.getString
 import com.merxury.blocker.core.designsystem.component.BlockerBodyLargeText
 import com.merxury.blocker.core.designsystem.icon.BlockerDisplayIcon
 import com.merxury.blocker.core.designsystem.icon.BlockerIcons
