d8a00552819b7fbf32efa8a6f595faa386b7c3ac
julie.wang <julie.www333@gmail.com>
2023-08-16 15:57:37 +0800

    AppDetailScreenTest: showComponentIno

---
+++ core/testing/src/main/java/com/merxury/blocker/core/testing/testing/data/ComponentInfoTestData.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2023 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.core.testing.testing.data
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import com.merxury.blocker.core.model.ComponentType.RECEIVER
+import com.merxury.blocker.core.ui.component.ComponentItem
+
+val componentInfoTestData: SnapshotStateList<ComponentItem> = mutableStateListOf(
+    ComponentItem(
+        name = "AlarmManagerSchedulerBroadcast",
+        simpleName = "AlarmManagerSchedulerBroadcast",
+        packageName = "com.merxury.blocker",
+        pmBlocked = false,
+        type = RECEIVER,
+    ),
+)
