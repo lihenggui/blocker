66eb53ddeec40ba14119c7cbae87623481a745dc
lihenggui <350699171@qq.com>
2023-08-15 20:55:25 -0700

    Use correct package name

--- feature/appdetail/src/androidTest/java/com.merxury.blocker.feature.appdetail/AppDetailScreenTest.kt
+++ feature/appdetail/src/androidTest/java/com.merxury.blocker.feature.appdetail/AppDetailScreenTest.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2023 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.feature.appdetail
+
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.runtime.mutableStateListOf
@@ -17,28 +35,9 @@ import com.merxury.blocker.core.ui.bottomsheet.ComponentSortInfoUiState
 import com.merxury.blocker.core.ui.component.ComponentItem
 import com.merxury.blocker.core.ui.data.UiMessage
 import com.merxury.blocker.core.ui.state.toolbar.AppBarUiState
-import com.merxury.blocker.feature.appdetail.AppDetailScreen
-import com.merxury.blocker.feature.appdetail.AppInfoUiState
-import com.merxury.blocker.feature.appdetail.ComponentListUiState
 import org.junit.Rule
 import org.junit.Test
 import com.merxury.blocker.core.ui.R as uiR
-
-/*
- * Copyright 2022 Blocker Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 class AppDetailScreenTest {
     @get:Rule(order = 0)
