9d30644a92f4327f3a8de77baccac0692482d4ed
lihenggui <lihenggui@users.noreply.github.com>
2023-08-22 14:56:06 -0700

    Temporary remove the component detail function
    Change-Id: I92a000ca50bd9fd712991cfc1597e609ecebfd8e

--- app-compose/src/main/java/com/merxury/blocker/navigation/BlockerNavHost.kt
+++ app-compose/src/main/java/com/merxury/blocker/navigation/BlockerNavHost.kt
@@ -32,7 +32,6 @@ import com.merxury.blocker.core.model.data.IconBasedThemingState
 import com.merxury.blocker.feature.appdetail.navigation.componentDetailScreen
 import com.merxury.blocker.feature.appdetail.navigation.detailScreen
 import com.merxury.blocker.feature.appdetail.navigation.navigateToAppDetail
-import com.merxury.blocker.feature.appdetail.navigation.navigateToComponentDetail
 import com.merxury.blocker.feature.applist.navigation.appListRoute
 import com.merxury.blocker.feature.applist.navigation.appListScreen
 import com.merxury.blocker.feature.generalrules.navigation.generalRuleScreen
@@ -79,7 +78,9 @@ fun BlockerNavHost(
             detailScreen(
                 onBackClick = onBackClick,
                 snackbarHostState = snackbarHostState,
-                navigateToComponentDetail = navController::navigateToComponentDetail,
+                // TODO Temporary disable showing component detail function
+                navigateToComponentDetail = { },
+//                navigateToComponentDetail = navController::navigateToComponentDetail,
                 updateIconBasedThemingState = updateIconBasedThemingState,
             )
             generalRuleScreen(
