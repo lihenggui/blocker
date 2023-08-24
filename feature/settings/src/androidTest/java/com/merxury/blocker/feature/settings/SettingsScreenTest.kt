2867374c19f25fd219e7da64c7e35defc2635c82
julie.wang <julie.www333@gmail.com>
2023-08-24 14:58:02 +0800

    fix SettingsScreenTest composeTestRule issue

--- feature/settings/src/androidTest/java/com/merxury/blocker/feature/settings/SettingsScreenTest.kt
+++ feature/settings/src/androidTest/java/com/merxury/blocker/feature/settings/SettingsScreenTest.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import com.merxury.blocker.core.testing.testing.data.userEditableSettingsTestData
 import com.merxury.blocker.feature.settings.R.string
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import com.merxury.blocker.core.ui.R as uiR
@@ -32,6 +33,58 @@ import com.merxury.blocker.core.ui.R as uiR
 class SettingsScreenTest {
     @get:Rule(order = 0)
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var loadingDescription: String
+    private lateinit var controllerType: String
+    private lateinit var shizuku: String
+    private lateinit var onlineRuleSource: String
+    private lateinit var optionsGitlab: String
+    private lateinit var themeString: String
+    private lateinit var dynamicColor: String
+    private lateinit var darkMode: String
+    private lateinit var dark: String
+    private lateinit var applicationList: String
+    private lateinit var showSystemApps: String
+    private lateinit var showServiceInfo: String
+    private lateinit var backup: String
+    private lateinit var folderToSave: String
+    private lateinit var backupSystemApps: String
+    private lateinit var restoreSystemApps: String
+    private lateinit var blockerRules: String
+    private lateinit var exportRules: String
+    private lateinit var importRules: String
+    private lateinit var ifwRules: String
+    private lateinit var exportIfwRules: String
+    private lateinit var importIfwRules: String
+
+    @Before
+    fun setup() {
+        composeTestRule.activity.apply {
+            loadingDescription = getString(uiR.string.core_ui_loading)
+            controllerType = getString(string.feature_settings_controller_type)
+            shizuku = getString(string.feature_settings_shizuku)
+            onlineRuleSource = getString(string.feature_settings_online_rule_source)
+            optionsGitlab = getString(string.feature_settings_options_gitlab)
+            themeString = getString(string.feature_settings_theme)
+            dynamicColor = getString(string.feature_settings_dynamic_color)
+            darkMode = getString(string.feature_settings_dark_mode)
+            dark = getString(string.feature_settings_dark)
+            applicationList = getString(string.feature_settings_application_list)
+            showSystemApps = getString(string.feature_settings_show_system_apps)
+            showServiceInfo = getString(string.feature_settings_show_service_info)
+            backup = getString(string.feature_settings_backup)
+            folderToSave = getString(string.feature_settings_folder_to_save)
+            backupSystemApps = getString(string.feature_settings_backup_system_apps)
+            restoreSystemApps = getString(string.feature_settings_restore_system_apps)
+            blockerRules = getString(string.feature_settings_blocker_rules)
+            exportRules = getString(string.feature_settings_export_rules)
+            importRules = getString(string.feature_settings_import_rules)
+            ifwRules = getString(string.feature_settings_ifw_rules)
+            exportIfwRules = getString(string.feature_settings_export_ifw_rules)
+            importIfwRules = getString(string.feature_settings_import_ifw_rules)
+
+        }
+    }
 
     @Test
     fun circularProgressIndicator_whenScreenIsLoading_exists() {
@@ -44,9 +97,7 @@ class SettingsScreenTest {
             }
         }
 
-        composeTestRule.onNodeWithContentDescription(
-            composeTestRule.activity.resources.getString(uiR.string.core_ui_loading),
-        ).assertExists()
+        composeTestRule.onNodeWithContentDescription(loadingDescription).assertExists()
     }
 
     @Test
@@ -63,73 +114,31 @@ class SettingsScreenTest {
         }
 
         composeTestRule.onNodeWithTag("settings:content").assertExists()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_controller_type),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_shizuku),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_online_rule_source),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_options_gitlab),
-        ).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(controllerType).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(shizuku).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(onlineRuleSource).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(optionsGitlab).assertExists().assertHasClickAction()
         // Theme settings
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_theme),
-        ).assertExists()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_dynamic_color),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_dark_mode),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_dark),
-        ).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(themeString).assertExists()
+        composeTestRule.onNodeWithText(dynamicColor).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(darkMode).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(dark).assertExists().assertHasClickAction()
         // Application list settings
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_application_list),
-        ).assertExists()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_show_system_apps),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_show_service_info),
-        ).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(applicationList).assertExists()
+        composeTestRule.onNodeWithText(showSystemApps).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(showServiceInfo).assertExists().assertHasClickAction()
         // Backup settings
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_backup),
-        ).assertExists()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_folder_to_save),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_backup_system_apps),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_restore_system_apps),
-        ).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(backup).assertExists()
+        composeTestRule.onNodeWithText(folderToSave).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(backupSystemApps).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(restoreSystemApps).assertExists().assertHasClickAction()
         // Blocker rules settings
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_blocker_rules),
-        ).assertExists()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_export_rules),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_import_rules),
-        ).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(blockerRules).assertExists()
+        composeTestRule.onNodeWithText(exportRules).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(importRules).assertExists().assertHasClickAction()
         // IFW rules settings
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_ifw_rules),
-        ).assertExists()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_export_ifw_rules),
-        ).assertExists().assertHasClickAction()
-        composeTestRule.onNodeWithText(
-            composeTestRule.activity.resources.getString(string.feature_settings_import_ifw_rules),
-        ).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(ifwRules).assertExists()
+        composeTestRule.onNodeWithText(exportIfwRules).assertExists().assertHasClickAction()
+        composeTestRule.onNodeWithText(importIfwRules).assertExists().assertHasClickAction()
     }
 }
