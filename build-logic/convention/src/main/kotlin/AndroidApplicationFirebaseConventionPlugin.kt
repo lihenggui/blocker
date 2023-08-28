74aeef670c282195bdaaa18f4776cdb2d97cc55c
lihenggui <lihenggui@users.noreply.github.com>
2023-08-28 14:01:03 -0700

    Enable mappingFileUploadEnabled property for release version
    Change-Id: I424f8d30effe6523184c3a650e5e3389c031df7a

--- build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
@@ -39,11 +39,8 @@ class AndroidApplicationFirebaseConventionPlugin : Plugin<Project> {
             }
             extensions.configure<ApplicationExtension> {
                 buildTypes.configureEach {
-                    // Disable the Crashlytics mapping file upload. This feature should only be
-                    // enabled if a Firebase backend is available and configured in
-                    // google-services.json.
                     configure<CrashlyticsExtension> {
-                        mappingFileUploadEnabled = false
+                        mappingFileUploadEnabled = this@configureEach.name.contains("release")
                     }
                 }
                 tasks.configureEach {
