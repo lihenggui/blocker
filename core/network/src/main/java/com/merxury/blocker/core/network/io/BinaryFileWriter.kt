815180766472dd32a8ab5d27b863b5286bf71486
lihenggui <350699171@qq.com>
2023-08-28 17:16:46 -0700

    Move BinaryFileWriter to io package

--- core/network/src/main/java/com/merxury/blocker/core/network/io/BinaryFileWriter.kt
+++ core/network/src/main/java/com/merxury/blocker/core/network/io/BinaryFileWriter.kt
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.merxury.blocker.core.network.model
+
+package com.merxury.blocker.core.network.io
 
 import java.io.BufferedInputStream
 import java.io.IOException
