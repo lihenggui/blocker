8b1b261878cef986f4a54307cba4f1ab904c7ba9
julie.wang <julie.www333@gmail.com>
2023-08-30 16:52:03 +0800

    Remove un-use code

--- feature/search/src/main/java/com/merxury/blocker/feature/search/component/SearchTopBar.kt
+++ feature/search/src/main/java/com/merxury/blocker/feature/search/component/SearchTopBar.kt
@@ -18,7 +18,6 @@ package com.merxury.blocker.feature.search.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -120,7 +119,6 @@ fun SelectedAppTopBar(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier,
