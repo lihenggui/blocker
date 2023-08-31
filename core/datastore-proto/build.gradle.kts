cda4a6670424b55bc1791e6fb4b215e69f1e51e0
lihenggui <350699171@qq.com>
2023-08-30 21:36:18 -0700

    Remove workarounds for supporting protobuf

--- core/datastore-proto/build.gradle.kts
+++ core/datastore-proto/build.gradle.kts
@@ -43,14 +43,6 @@ protobuf {
     }
 }
 
-androidComponents.beforeVariants {
-    android.sourceSets.register(it.name) {
-        val buildDir = layout.buildDirectory.get().asFile
-        java.srcDir(buildDir.resolve("generated/source/proto/${it.name}/java"))
-        kotlin.srcDir(buildDir.resolve("generated/source/proto/${it.name}/kotlin"))
-    }
-}
-
 dependencies {
     implementation(libs.protobuf.kotlin.lite)
 }
