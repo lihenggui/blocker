af92317e145d16ad8bc67a56a3bbe51f64337be1
julie.wang <julie.www333@gmail.com>
2023-09-11 10:15:58 +0800

    Add dependency

--- core/testing/build.gradle.kts
+++ core/testing/build.gradle.kts
@@ -32,16 +32,19 @@ dependencies {
     implementation(projects.core.ui)
     implementation(libs.kotlinx.datetime)
 
-    api(libs.junit4)
+    api(libs.accompanist.testharness)
+    api(libs.androidx.activity.compose)
+    api(libs.androidx.compose.ui.test)
     api(libs.androidx.test.core)
-    api(libs.kotlinx.coroutines.test)
-    api(libs.turbine)
-
     api(libs.androidx.test.espresso.core)
     api(libs.androidx.test.runner)
     api(libs.androidx.test.rules)
-    api(libs.androidx.compose.ui.test)
     api(libs.hilt.android.testing)
+    api(libs.junit4)
+    api(libs.kotlinx.coroutines.test)
+    api(libs.roborazzi)
+    api(libs.robolectric.shadows)
+    api(libs.turbine)
 
     debugApi(libs.androidx.compose.ui.testManifest)
 }
