a4e5e3f6620409c851a3ffc1f928c0aa5cad6899
julie.wang <julie.www333@gmail.com>
2023-09-15 15:42:19 +0800

    Revert "Move NoSearchResultScreen to core.ui"
    This reverts commit af599816f864c046807e54ad59673e31962cd16c.

--- core/ui/src/main/java/com/merxury/blocker/core/ui/screen/EmptyScreen.kt
+++ core/ui/src/main/java/com/merxury/blocker/core/ui/screen/EmptyScreen.kt
@@ -19,6 +19,7 @@ package com.merxury.blocker.core.ui.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -26,37 +27,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.merxury.blocker.core.designsystem.component.BlockerBodyLargeText
-import com.merxury.blocker.core.designsystem.icon.BlockerDisplayIcon
-import com.merxury.blocker.core.designsystem.icon.BlockerIcons
 import com.merxury.blocker.core.designsystem.theme.BlockerTheme
 import com.merxury.blocker.core.ui.R
 
 @Composable
-fun NoSearchResultScreen() {
+fun EmptyScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        BlockerDisplayIcon(
-            imageVector = BlockerIcons.Inbox,
-            contentDescription = null,
-        )
         BlockerBodyLargeText(
-            text = stringResource(id = R.string.core_ui_no_search_result),
+            modifier = Modifier.padding(16.dp),
+            text = stringResource(id = R.string.core_ui_not_found_app),
             color = MaterialTheme.colorScheme.outline,
         )
     }
 }
 
-@Composable
 @Preview
-@Preview(uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES)
-private fun NoSearchResultScreenPreview() {
+@Composable
+fun EmptyScreenPreview() {
     BlockerTheme {
         Surface {
-            NoSearchResultScreen()
+            EmptyScreen()
         }
     }
 }
---
+++ feature/search/src/main/java/com/merxury/blocker/feature/search/screen/NoSearchResultScreen.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.feature.search.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.merxury.blocker.core.designsystem.component.BlockerBodyLargeText
+import com.merxury.blocker.core.designsystem.icon.BlockerDisplayIcon
+import com.merxury.blocker.core.designsystem.icon.BlockerIcons
+import com.merxury.blocker.core.designsystem.theme.BlockerTheme
+import com.merxury.blocker.feature.search.R.string
+
+@Composable
+fun NoSearchResultScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        BlockerDisplayIcon(
+            imageVector = BlockerIcons.Inbox,
+            contentDescription = null,
+        )
+        BlockerBodyLargeText(
+            text = stringResource(id = string.feature_search_no_search_result),
+            color = MaterialTheme.colorScheme.outline,
+        )
+    }
+}
+
+@Composable
+@Preview
+@Preview(uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES)
+private fun NoSearchResultScreenPreview() {
+    BlockerTheme {
+        Surface {
+            NoSearchResultScreen()
+        }
+    }
+}
