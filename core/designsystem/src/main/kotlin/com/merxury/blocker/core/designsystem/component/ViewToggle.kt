0a4d7ca60f93d61e6e9321000382679689e612be
Julie Wang <julie.wang@ringcentral.com>
2023-09-24 22:25:01 +0800

    ViewToggleScreenshotTests

--- core/designsystem/src/main/kotlin/com/merxury/blocker/core/designsystem/component/ViewToggle.kt
+++ core/designsystem/src/main/kotlin/com/merxury/blocker/core/designsystem/component/ViewToggle.kt
@@ -25,11 +25,15 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.merxury.blocker.core.designsystem.icon.BlockerIcons
+import com.merxury.blocker.core.designsystem.theme.BlockerTheme
 
 /**
  * Blocker view toggle button with included trailing icon as well as compact and expanded
@@ -102,6 +106,36 @@ private fun BlockerViewToggleButtonContent(
     if (trailingIcon != null) {
         Box(Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize)) {
             trailingIcon()
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+fun ViewTogglePreviewExpanded() {
+    BlockerTheme {
+        Surface {
+            BlockerViewToggleButton(
+                expanded = true,
+                onExpandedChange = { },
+                compactText = { Text(text = "Compact view") },
+                expandedText = { Text(text = "Expanded view") },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ViewTogglePreviewCompact() {
+    BlockerTheme {
+        Surface {
+            BlockerViewToggleButton(
+                expanded = false,
+                onExpandedChange = { },
+                compactText = { Text(text = "Compact view") },
+                expandedText = { Text(text = "Expanded view") },
+            )
         }
     }
 }
