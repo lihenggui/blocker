2c2a3aa5aa0875c19f9bb292da50b0272884248d
juliewww <julie.www333@gmail.com>
2023-09-26 21:20:14 +0800

    LoadingScreenScreenshotTests InitializingScreenScreenshotTests

---
+++ core/ui/src/test/kotlin/com/merxury/blocker/core/ui/LoadingScreenScreenshotTests.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2023 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.core.ui
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onRoot
+import com.github.takahirom.roborazzi.captureRoboImage
+import com.google.accompanist.testharness.TestHarness
+import com.merxury.blocker.core.designsystem.theme.BlockerTheme
+import com.merxury.blocker.core.testing.util.DefaultRoborazziOptions
+import com.merxury.blocker.core.testing.util.captureMultiTheme
+import com.merxury.blocker.core.ui.screen.LoadingScreen
+import dagger.hilt.android.testing.HiltTestApplication
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+import org.robolectric.annotation.LooperMode
+import kotlin.test.Test
+
+@RunWith(RobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(application = HiltTestApplication::class, sdk = [33], qualifiers = "480dpi")
+@LooperMode(LooperMode.Mode.PAUSED)
+class LoadingScreenScreenshotTests {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun loadingScreen_multipleThemes() {
+        composeTestRule.captureMultiTheme("LoadingScreen") {
+            Surface {
+                LoadingScreenExample()
+            }
+        }
+    }
+
+    @Test
+    fun loadingScreen_hugeFont() {
+        composeTestRule.setContent {
+            CompositionLocalProvider(
+                LocalInspectionMode provides true,
+            ) {
+                TestHarness(fontScale = 2f) {
+                    BlockerTheme {
+                        LoadingScreenExample()
+                    }
+                }
+            }
+        }
+        composeTestRule.onRoot()
+            .captureRoboImage(
+                "src/test/screenshots/LoadingScreen" +
+                    "/LoadingScreen_fontScale2.png",
+                roborazziOptions = DefaultRoborazziOptions,
+            )
+    }
+
+    @Composable
+    private fun LoadingScreenExample() {
+        LoadingScreen()
+    }
+}
