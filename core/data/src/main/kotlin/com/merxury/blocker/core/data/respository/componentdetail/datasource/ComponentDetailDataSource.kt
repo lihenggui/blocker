2b3a1019fd7ad350bf834333f723a1186023ac14
lihenggui <350699171@qq.com>
2023-10-11 21:37:17 -0700

    Use correct path to add customized rules

--- core/data/src/main/kotlin/com/merxury/blocker/core/data/respository/componentdetail/datasource/ComponentDetailDataSource.kt
+++ core/data/src/main/kotlin/com/merxury/blocker/core/data/respository/componentdetail/datasource/ComponentDetailDataSource.kt
@@ -18,12 +18,6 @@ package com.merxury.blocker.core.data.respository.componentdetail.datasource
 
 import com.merxury.blocker.core.model.data.ComponentDetail
 import kotlinx.coroutines.flow.Flow
-import java.util.Locale
-
-val LIB_SUPPORTED_LANGUAGE = listOf(
-    Locale.ENGLISH.toLanguageTag(),
-    Locale.SIMPLIFIED_CHINESE.toLanguageTag(),
-)
 
 interface ComponentDetailDataSource {
 
@@ -32,31 +26,4 @@ interface ComponentDetailDataSource {
     fun getByComponentName(name: String): Flow<ComponentDetail?>
 
     fun saveComponentData(component: ComponentDetail): Flow<Boolean>
-
-    suspend fun getLibDisplayLanguage(displayLanguageInSettings: String): String {
-        if (displayLanguageInSettings.isNotBlank()) {
-            return displayLanguageInSettings
-        }
-        // Empty means follow the system language first
-        // If no matching found, fallback to English
-        val locale = Locale.getDefault()
-        val language = locale.language
-        val country = locale.country
-        val systemLanguage = if (country.isNotBlank()) {
-            "$language-$country"
-        } else {
-            language
-        }
-        var splittedLanguage = systemLanguage.split("-")
-        while (splittedLanguage.isNotEmpty()) {
-            val languageTag = splittedLanguage.joinToString("-")
-            val matchingLanguage = LIB_SUPPORTED_LANGUAGE.find { it == languageTag }
-            if (matchingLanguage != null) {
-                return matchingLanguage
-            } else {
-                splittedLanguage = splittedLanguage.dropLast(1)
-            }
-        }
-        return Locale.ENGLISH.toLanguageTag()
-    }
 }
