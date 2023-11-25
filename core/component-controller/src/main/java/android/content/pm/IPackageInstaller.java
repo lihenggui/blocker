225ded2e151ea6469ba25c49215bb5204c429160
lihenggui <350699171@qq.com>
2023-11-24 17:06:33 -0800

    Add function for uninstall an app

--- core/component-controller/src/main/java/android/content/pm/IPackageInstaller.java
+++ core/component-controller/src/main/java/android/content/pm/IPackageInstaller.java
@@ -22,14 +22,15 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.os.IInterface;
 import android.os.RemoteException;
-
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
 import java.util.List;
 
 public interface IPackageInstaller extends IInterface {
 
-    int createSession(PackageInstaller.SessionParams params, String installerPackageName, int userId)
+    int createSession(PackageInstaller.SessionParams params, String installerPackageName,
+            int userId)
             throws RemoteException;
 
     void updateSessionAppIcon(int sessionId, Bitmap appIcon)
@@ -50,7 +51,8 @@ public interface IPackageInstaller extends IInterface {
     ParceledListSlice<PackageInstaller.SessionInfo> getAllSessions(int userId)
             throws RemoteException;
 
-    ParceledListSlice<PackageInstaller.SessionInfo> getMySessions(String installerPackageName, int userId)
+    ParceledListSlice<PackageInstaller.SessionInfo> getMySessions(String installerPackageName,
+            int userId)
             throws RemoteException;
 
     @RequiresApi(29)
@@ -65,16 +67,30 @@ public interface IPackageInstaller extends IInterface {
 
     // removed from 26
     void uninstall(String packageName, String callerPackageName, int flags,
-                   IntentSender statusReceiver, int userId);
+            IntentSender statusReceiver, int userId);
 
     @RequiresApi(26)
     void uninstall(VersionedPackage versionedPackage, String callerPackageName, int flags,
-                   IntentSender statusReceiver, int userId)
+            IntentSender statusReceiver, int userId)
+            throws RemoteException;
+
+    /**
+     * Uninstall the given package, removing it completely from the device. This
+     * method is only available to the current "installer of record" for the
+     * package.
+     *
+     * @param packageName    The package to uninstall.
+     * @param flags          Flags for uninstall.
+     * @param statusReceiver Where to deliver the result of the operation indicated by the extra
+     *                       #EXTRA_STATUS. Refer to the individual status codes
+     *                       on how to handle them.
+     */
+    void uninstall(@NonNull String packageName, int flags, @NonNull IntentSender statusReceiver)
             throws RemoteException;
 
     @RequiresApi(29)
     void installExistingPackage(String packageName, int installFlags, int installReason,
-                                IntentSender statusReceiver, int userId, List<String> whiteListedPermissions)
+            IntentSender statusReceiver, int userId, List<String> whiteListedPermissions)
             throws RemoteException;
 
     void setPermissionsResult(int sessionId, boolean accepted)
