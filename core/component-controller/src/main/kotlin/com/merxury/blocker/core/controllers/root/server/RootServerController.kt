3475ea6bb4fc800ad6ed177a7008e75d1217ab86
lihenggui <lihenggui@users.noreply.github.com>
2023-11-30 14:38:58 -0800

    Add RootServerController and helper class
    Change-Id: I17fad31798c2db3781ce05854459234eb3c8671d

---
+++ core/component-controller/src/main/kotlin/com/merxury/blocker/core/controllers/root/server/RootServerController.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2023 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.core.controllers.root.server
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.content.pm.ComponentInfo
+import android.content.pm.PackageManager
+import android.os.IBinder
+import com.merxury.blocker.core.controller.root.service.IRootService
+import com.merxury.blocker.core.controllers.IController
+import com.topjohnwu.superuser.ipc.RootService
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+import javax.inject.Inject
+
+class RootServerController @Inject constructor(
+    @ApplicationContext private val context: Context,
+): IController {
+    private var rootConnection: RootConnection? = null
+    private var rootServer: IRootService? = null
+    inner class RootConnection : ServiceConnection {
+        private var isDaemon = false
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Timber.d("onServiceConnected")
+            rootConnection = this
+            rootServer = IRootService.Stub.asInterface(service)
+            Timber.d("uid: ${rootServer?.uid}")
+            Timber.d("pid: ${rootServer?.pid}")
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Timber.d("onServiceDisconnected")
+            rootServer = null
+        }
+    }
+
+    private suspend fun ensureInitialization() = withContext(Dispatchers.Main) {
+        if (rootConnection == null) {
+            val connection = RootConnection()
+            val intent = Intent(context, RootServer::class.java)
+            RootService.bind(intent, connection)
+            this@RootServerController.rootConnection = connection
+        }
+    }
+
+    override suspend fun switchComponent(
+        packageName: String,
+        componentName: String,
+        state: Int,
+    ): Boolean {
+        Timber.d("Switch component: $packageName/$componentName, state: $state")
+        ensureInitialization()
+        return false
+    }
+
+    override suspend fun enable(packageName: String, componentName: String): Boolean {
+        return switchComponent(packageName, componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED)
+    }
+
+    override suspend fun disable(packageName: String, componentName: String): Boolean {
+        return switchComponent(packageName, componentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED)
+    }
+
+    override suspend fun batchEnable(
+        componentList: List<ComponentInfo>,
+        action: suspend (info: ComponentInfo) -> Unit,
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun batchDisable(
+        componentList: List<ComponentInfo>,
+        action: suspend (info: ComponentInfo) -> Unit,
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun checkComponentEnableState(
+        packageName: String,
+        componentName: String,
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+}
---
+++ core/component-controller/src/main/kotlin/com/merxury/blocker/core/controllers/root/server/SystemServiceHelper.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.core.controllers.root.server;
+
+import android.annotation.SuppressLint;
+import android.os.IBinder;
+import androidx.annotation.NonNull;
+import timber.log.Timber;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Helper class for accessing system services.
+ * Copied from <a href="https://github.com/RikkaApps/Shizuku-API/blob/01e08879d58a5cb11a333535c6ddce9f7b7c88ff/api/src/main/java/rikka/shizuku/SystemServiceHelper.java">...</a>
+ */
+@SuppressLint("PrivateApi")
+public class SystemServiceHelper {
+
+    private static final Map<String, IBinder> SYSTEM_SERVICE_CACHE = new HashMap<>();
+
+    private static Method getService;
+
+    static {
+        try {
+            Class<?> sm = Class.forName("android.os.ServiceManager");
+            getService = sm.getMethod("getService", String.class);
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            Timber.w(e);
+        }
+    }
+
+    /**
+     * Returns a reference to a service with the given name.
+     *
+     * @param name the name of the service to get such as "package" for android.content.pm.IPackageManager
+     * @return a reference to the service, or <code>null</code> if the service doesn't exist
+     */
+    public static IBinder getSystemService(@NonNull String name) {
+        IBinder binder = SYSTEM_SERVICE_CACHE.get(name);
+        if (binder == null) {
+            try {
+                binder = (IBinder) getService.invoke(null, name);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                Timber.w(e);
+            }
+            SYSTEM_SERVICE_CACHE.put(name, binder);
+        }
+        return binder;
+    }
+}
