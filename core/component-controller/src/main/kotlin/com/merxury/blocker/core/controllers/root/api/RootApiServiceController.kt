642527a71bbc2f2711d89d72856e4afef47572ce
lihenggui <350699171@qq.com>
2023-12-05 19:13:54 -0800

    Request root permission before binding the server

--- core/component-controller/src/main/kotlin/com/merxury/blocker/core/controllers/root/api/RootApiServiceController.kt
+++ core/component-controller/src/main/kotlin/com/merxury/blocker/core/controllers/root/api/RootApiServiceController.kt
@@ -23,8 +23,11 @@ import android.content.ServiceConnection
 import android.os.IBinder
 import com.merxury.blocker.core.controller.root.service.IRootService
 import com.merxury.blocker.core.controllers.IServiceController
+import com.merxury.blocker.core.dispatchers.BlockerDispatchers.IO
 import com.merxury.blocker.core.dispatchers.BlockerDispatchers.MAIN
 import com.merxury.blocker.core.dispatchers.Dispatcher
+import com.merxury.blocker.core.exception.RootUnavailableException
+import com.merxury.blocker.core.utils.PermissionUtils
 import com.topjohnwu.superuser.ipc.RootService
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineDispatcher
@@ -38,11 +41,15 @@ import kotlin.coroutines.suspendCoroutine
 class RootApiServiceController @Inject constructor(
     @ApplicationContext private val context: Context,
     @Dispatcher(MAIN) private val mainDispatcher: CoroutineDispatcher,
+    @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
 ) : IServiceController {
     private var rootConnection: ServiceConnection? = null
     private var rootService: IRootService? = null
 
     override suspend fun init() = withContext(mainDispatcher) {
+        if (!PermissionUtils.isRootAvailable(ioDispatcher)) {
+            throw RootUnavailableException()
+        }
         Timber.d("Start initialize RootApiServiceController")
         val intent = Intent(context, RootServer::class.java)
         suspendCoroutine { cont ->
