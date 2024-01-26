8992545f45f476bafb4eddebfadd53bcfe737880
lihenggui <350699171@qq.com>
2024-01-25 17:36:08 -0800

    Add test cases to test SearchComponentsUseCase

--- core/domain/src/test/kotlin/com/merxury/blocker/core/domain/components/SearchComponentsUseCaseTest.kt
+++ core/domain/src/test/kotlin/com/merxury/blocker/core/domain/components/SearchComponentsUseCaseTest.kt
@@ -23,6 +23,9 @@ import com.merxury.blocker.core.model.ComponentType
 import com.merxury.blocker.core.model.data.ComponentInfo
 import com.merxury.blocker.core.model.data.InstalledApp
 import com.merxury.blocker.core.model.data.toAppItem
+import com.merxury.blocker.core.model.preference.ComponentShowPriority
+import com.merxury.blocker.core.model.preference.ComponentSorting
+import com.merxury.blocker.core.model.preference.SortingOrder
 import com.merxury.blocker.core.testing.controller.FakeServiceController
 import com.merxury.blocker.core.testing.repository.TestAppRepository
 import com.merxury.blocker.core.testing.repository.TestComponentDetailRepository
@@ -93,8 +96,73 @@ class SearchComponentsUseCaseTest {
     }
 
     @Test
-    fun givenAppAndComponentList_whenNoSearchKeyword_thenReturnUnfilteredResult() = runTest {
+    fun givenComponentList_whenSearchByKeyword_thenGetFilteredList() = runTest {
         userDataRepository.sendUserData(defaultUserData)
+        appRepository.sendAppList(listOf(app1, app2, app3))
+        componentRepository.sendComponentList(components1 + components2 + components3)
+        componentDetailRepository.sendComponentDetail(emptyList())
+        // There would be only one component with the name "15"
+        searchComponentsUseCase(app1PackageName, "15").test {
+            val expectedList = ComponentSearchResult(
+                app = app1.toAppItem(),
+                activity = listOf(),
+                service = listOf(),
+                receiver = listOf(),
+                provider = listOf(components1[15]),
+            )
+            assertEquals(expectedList, awaitItem())
+        }
+    }
+
+    @Test
+    fun givenComponentListWithRunningService_whenInDefault_thenShowRunningServiceOnTop() = runTest {
+        userDataRepository.sendUserData(
+            defaultUserData.copy(
+                componentSorting = ComponentSorting.COMPONENT_NAME,
+                componentSortingOrder = SortingOrder.ASCENDING,
+                componentShowPriority = ComponentShowPriority.NONE,
+            ),
+        )
+        appRepository.sendAppList(listOf(app1, app2, app3))
+        componentRepository.sendComponentList(components1 + components2 + components3)
+        componentDetailRepository.sendComponentDetail(emptyList())
+        val runningServiceName = "$app1PackageName.5"
+        serviceController.sendRunningServices(runningServiceName)
+
+        val listWithRunningService = components1.map {
+            if (it.name == runningServiceName) {
+                it.copy(isRunning = true)
+            } else {
+                it
+            }
+        }
+
+        searchComponentsUseCase(app1PackageName, "").test {
+            val expectedList = ComponentSearchResult(
+                app = app1.toAppItem(),
+                activity = components1.filter { it.type == ComponentType.ACTIVITY }
+                    .sortedBy { it.simpleName.lowercase() },
+                service = listWithRunningService.filter { it.type == ComponentType.SERVICE }
+                    .sortedBy { it.simpleName.lowercase() }
+                    .sortedByDescending { it.name == "5" },
+                receiver = components1.filter { it.type == ComponentType.RECEIVER }
+                    .sortedBy { it.simpleName.lowercase() },
+                provider = components1.filter { it.type == ComponentType.PROVIDER }
+                    .sortedBy { it.simpleName.lowercase() },
+            )
+            assertEquals(expectedList, awaitItem())
+        }
+    }
+
+    @Test
+    fun givenSortByNameAscending_whenNoSearchKeyword_thenReturnUnfilteredResult() = runTest {
+        userDataRepository.sendUserData(
+            defaultUserData.copy(
+                componentSorting = ComponentSorting.COMPONENT_NAME,
+                componentSortingOrder = SortingOrder.ASCENDING,
+                componentShowPriority = ComponentShowPriority.NONE,
+            ),
+        )
         appRepository.sendAppList(listOf(app1, app2, app3))
         componentRepository.sendComponentList(components1 + components2 + components3)
         componentDetailRepository.sendComponentDetail(emptyList())
@@ -110,6 +178,173 @@ class SearchComponentsUseCaseTest {
                     .sortedBy { it.simpleName.lowercase() },
                 provider = components1.filter { it.type == ComponentType.PROVIDER }
                     .sortedBy { it.simpleName.lowercase() },
+            )
+            assertEquals(expectedList, awaitItem())
+        }
+    }
+
+    @Test
+    fun givenSortByNameDescending_whenNoSearchKeyword_thenGetSortedList() = runTest {
+        userDataRepository.sendUserData(
+            defaultUserData.copy(
+                componentSorting = ComponentSorting.COMPONENT_NAME,
+                componentSortingOrder = SortingOrder.DESCENDING,
+                componentShowPriority = ComponentShowPriority.NONE,
+            ),
+        )
+        appRepository.sendAppList(listOf(app1, app2, app3))
+        componentRepository.sendComponentList(components1 + components2 + components3)
+        componentDetailRepository.sendComponentDetail(emptyList())
+        // Default preference is sort by COMPONENT_NAME, ascending
+        searchComponentsUseCase(app2PackageName, "").test {
+            val expectedList = ComponentSearchResult(
+                app = app2.toAppItem(),
+                activity = components2.filter { it.type == ComponentType.ACTIVITY }
+                    .sortedByDescending { it.simpleName.lowercase() },
+                service = components2.filter { it.type == ComponentType.SERVICE }
+                    .sortedByDescending { it.simpleName.lowercase() },
+                receiver = components2.filter { it.type == ComponentType.RECEIVER }
+                    .sortedByDescending { it.simpleName.lowercase() },
+                provider = components2.filter { it.type == ComponentType.PROVIDER }
+                    .sortedByDescending { it.simpleName.lowercase() },
+            )
+            assertEquals(expectedList, awaitItem())
+        }
+    }
+
+    @Test
+    fun givenSortByFullNameAscending_whenNoSearchKeyword_thenGetSortedList() = runTest {
+        userDataRepository.sendUserData(
+            defaultUserData.copy(
+                componentSorting = ComponentSorting.PACKAGE_NAME,
+                componentSortingOrder = SortingOrder.ASCENDING,
+                componentShowPriority = ComponentShowPriority.NONE,
+            ),
+        )
+        appRepository.sendAppList(listOf(app1, app2, app3))
+        componentRepository.sendComponentList(components1 + components2 + components3)
+        componentDetailRepository.sendComponentDetail(emptyList())
+        // Default preference is sort by COMPONENT_NAME, ascending
+        searchComponentsUseCase(app3PackageName, "").test {
+            val expectedList = ComponentSearchResult(
+                app = app3.toAppItem(),
+                activity = components3.filter { it.type == ComponentType.ACTIVITY }
+                    .sortedBy { it.name.lowercase() },
+                service = components3.filter { it.type == ComponentType.SERVICE }
+                    .sortedBy { it.name.lowercase() },
+                receiver = components3.filter { it.type == ComponentType.RECEIVER }
+                    .sortedBy { it.name.lowercase() },
+                provider = components3.filter { it.type == ComponentType.PROVIDER }
+                    .sortedBy { it.name.lowercase() },
+            )
+            assertEquals(expectedList, awaitItem())
+        }
+    }
+
+    @Test
+    fun givenSortByFullNameDescending_whenNoSearchKeyword_thenGetSortedList() = runTest {
+        userDataRepository.sendUserData(
+            defaultUserData.copy(
+                componentSorting = ComponentSorting.PACKAGE_NAME,
+                componentSortingOrder = SortingOrder.DESCENDING,
+                componentShowPriority = ComponentShowPriority.NONE,
+            ),
+        )
+        appRepository.sendAppList(listOf(app1, app2, app3))
+        componentRepository.sendComponentList(components1 + components2 + components3)
+        componentDetailRepository.sendComponentDetail(emptyList())
+        // Default preference is sort by COMPONENT_NAME, ascending
+        searchComponentsUseCase(app2PackageName, "").test {
+            val expectedList = ComponentSearchResult(
+                app = app2.toAppItem(),
+                activity = components2.filter { it.type == ComponentType.ACTIVITY }
+                    .sortedByDescending { it.name.lowercase() },
+                service = components2.filter { it.type == ComponentType.SERVICE }
+                    .sortedByDescending { it.name.lowercase() },
+                receiver = components2.filter { it.type == ComponentType.RECEIVER }
+                    .sortedByDescending { it.name.lowercase() },
+                provider = components2.filter { it.type == ComponentType.PROVIDER }
+                    .sortedByDescending { it.name.lowercase() },
+            )
+            assertEquals(expectedList, awaitItem())
+        }
+    }
+
+    @Test
+    fun givenComponentListWithDisabledItems_whenSetShowDisabledFirst_thenGetSortedList() = runTest {
+        userDataRepository.sendUserData(
+            defaultUserData.copy(
+                componentSorting = ComponentSorting.COMPONENT_NAME,
+                componentSortingOrder = SortingOrder.ASCENDING,
+                componentShowPriority = ComponentShowPriority.DISABLED_COMPONENTS_FIRST,
+            ),
+        )
+        appRepository.sendAppList(listOf(app1, app2, app3))
+        val componentWithDisabledItem = components1.mapIndexed { index, component ->
+            if (index % 2 == 0) {
+                component.copy(pmBlocked = true)
+            } else {
+                component
+            }
+        }
+        componentRepository.sendComponentList(componentWithDisabledItem)
+        componentDetailRepository.sendComponentDetail(emptyList())
+        // Default preference is sort by COMPONENT_NAME, ascending
+        searchComponentsUseCase(app1PackageName, "").test {
+            val expectedList = ComponentSearchResult(
+                app = app1.toAppItem(),
+                activity = componentWithDisabledItem.filter { it.type == ComponentType.ACTIVITY }
+                    .sortedBy { it.simpleName.lowercase() }
+                    .sortedBy { it.enabled() },
+                service = componentWithDisabledItem.filter { it.type == ComponentType.SERVICE }
+                    .sortedBy { it.simpleName.lowercase() }
+                    .sortedBy { it.enabled() },
+                receiver = componentWithDisabledItem.filter { it.type == ComponentType.RECEIVER }
+                    .sortedBy { it.simpleName.lowercase() }
+                    .sortedBy { it.enabled() },
+                provider = componentWithDisabledItem.filter { it.type == ComponentType.PROVIDER }
+                    .sortedBy { it.simpleName.lowercase() }
+                    .sortedBy { it.enabled() },
+            )
+            assertEquals(expectedList, awaitItem())
+        }
+    }
+
+    @Test
+    fun givenComponentListWithDisabledItems_whenSetShowEnabledFirst_thenGetSortedList() = runTest {
+        userDataRepository.sendUserData(
+            defaultUserData.copy(
+                componentSorting = ComponentSorting.COMPONENT_NAME,
+                componentSortingOrder = SortingOrder.ASCENDING,
+                componentShowPriority = ComponentShowPriority.ENABLED_COMPONENTS_FIRST,
+            ),
+        )
+        appRepository.sendAppList(listOf(app1, app2, app3))
+        val componentWithDisabledItem = components1.mapIndexed { index, component ->
+            if (index % 2 == 0) {
+                component.copy(pmBlocked = true)
+            } else {
+                component
+            }
+        }
+        componentRepository.sendComponentList(componentWithDisabledItem)
+        componentDetailRepository.sendComponentDetail(emptyList())
+        // Default preference is sort by COMPONENT_NAME, ascending
+        searchComponentsUseCase(app1PackageName, "").test {
+            val expectedList = ComponentSearchResult(
+                app = app1.toAppItem(),
+                activity = componentWithDisabledItem.filter { it.type == ComponentType.ACTIVITY }
+                    .sortedBy { it.simpleName.lowercase() }
+                    .sortedByDescending { it.enabled() },
+                service = componentWithDisabledItem.filter { it.type == ComponentType.SERVICE }
+                    .sortedBy { it.simpleName.lowercase() }
+                    .sortedByDescending { it.enabled() },
+                receiver = componentWithDisabledItem.filter { it.type == ComponentType.RECEIVER }
+                    .sortedBy { it.simpleName.lowercase() }
+                    .sortedByDescending { it.enabled() },
+                provider = componentWithDisabledItem.filter { it.type == ComponentType.PROVIDER }
+                    .sortedBy { it.simpleName.lowercase() }
+                    .sortedByDescending { it.enabled() },
             )
             assertEquals(expectedList, awaitItem())
         }
