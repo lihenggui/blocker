82c64e65253d751c724334020c2ba1ffaacd714a
juliewww <julie.www333@gmail.com>
2024-03-15 13:32:52 +0800

    Add Search2PaneViewModel

---
+++ app-compose/src/main/kotlin/com/merxury/blocker/ui/search2pane/Search2PaneViewModel.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.ui.search2pane
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.merxury.blocker.feature.generalrules.navigation.RULE_ID_ARG
+import com.merxury.blocker.feature.search.navigation.PACKAGE_NAME_ARG
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class Search2PaneViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    private val selectedPackageName: StateFlow<String?> = savedStateHandle.getStateFlow(
+        PACKAGE_NAME_ARG, null,
+    )
+    val selectedRuleId: StateFlow<String?> = savedStateHandle.getStateFlow(RULE_ID_ARG, null)
+
+    fun onRuleClick(ruleId: String?) {
+        savedStateHandle[RULE_ID_ARG] = ruleId
+    }
+
+    fun onAppClick(packageName: String?) {
+        savedStateHandle[PACKAGE_NAME_ARG] = packageName
+    }
+
+    fun onComponentClick(packageName: String?) {
+        savedStateHandle[PACKAGE_NAME_ARG] = packageName
+    }
+}
