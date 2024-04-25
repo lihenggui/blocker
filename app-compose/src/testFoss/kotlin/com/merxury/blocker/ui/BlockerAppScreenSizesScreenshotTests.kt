f6eba2014a523c6002d41cda021be1643ad80964
lihenggui <350699171@qq.com>
2024-04-24 17:37:49 -0700

    Fixes flakiness in app's screenshot tests

--- app-compose/src/testFoss/kotlin/com/merxury/blocker/ui/BlockerAppScreenSizesScreenshotTests.kt
+++ app-compose/src/testFoss/kotlin/com/merxury/blocker/ui/BlockerAppScreenSizesScreenshotTests.kt
@@ -17,7 +17,6 @@
 
 package com.merxury.blocker.ui
 
-import android.util.Log
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
@@ -28,10 +27,6 @@ import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.work.Configuration
-import androidx.work.testing.SynchronousExecutor
-import androidx.work.testing.WorkManagerTestInitHelper
 import com.github.takahirom.roborazzi.captureRoboImage
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.google.accompanist.testharness.TestHarness
@@ -100,17 +95,6 @@ class BlockerAppScreenSizesScreenshotTests {
 
     @Before
     fun setup() {
-        val config = Configuration.Builder()
-            .setMinimumLoggingLevel(Log.DEBUG)
-            .setExecutor(SynchronousExecutor())
-            .build()
-
-        // Initialize WorkManager for instrumentation tests.
-        WorkManagerTestInitHelper.initializeTestWorkManager(
-            InstrumentationRegistry.getInstrumentation().context,
-            config,
-        )
-
         hiltRule.inject()
     }
 
