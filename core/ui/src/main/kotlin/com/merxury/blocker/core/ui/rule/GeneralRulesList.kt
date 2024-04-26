09f971093830ca0d0a65a8847cea14eee195c33d
juliewww <julie.www333@gmail.com>
2024-04-26 17:26:59 +0800

    Update GeneralRuleScreenPreview

--- core/ui/src/main/kotlin/com/merxury/blocker/core/ui/rule/GeneralRulesList.kt
+++ core/ui/src/main/kotlin/com/merxury/blocker/core/ui/rule/GeneralRulesList.kt
@@ -28,10 +28,12 @@ import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.merxury.blocker.core.designsystem.component.ThemePreviews
 import com.merxury.blocker.core.designsystem.component.scrollbar.FastScrollbar
@@ -39,6 +41,7 @@ import com.merxury.blocker.core.designsystem.component.scrollbar.rememberDraggab
 import com.merxury.blocker.core.designsystem.component.scrollbar.scrollbarState
 import com.merxury.blocker.core.designsystem.theme.BlockerTheme
 import com.merxury.blocker.core.model.data.GeneralRule
+import com.merxury.blocker.core.ui.previewparameter.RuleListPreviewParameterProvider
 
 @Composable
 fun GeneralRulesList(
@@ -82,44 +85,13 @@ fun GeneralRulesList(
 
 @Composable
 @ThemePreviews
-fun GeneralRuleScreenPreview() {
-    val ruleList = listOf(
-        GeneralRule(
-            id = 1,
-            name = "AWS SDK for Kotlin (Developer Preview)",
-            iconUrl = null,
-            company = "Amazon",
-            description = "The AWS SDK for Kotlin simplifies the use of AWS services by " +
-                "providing a set of libraries that are consistent and familiar for " +
-                "Kotlin developers. All AWS SDKs support API lifecycle considerations " +
-                "such as credential management, retries, data marshaling, and serialization.",
-            sideEffect = "Unknown",
-            safeToBlock = true,
-            contributors = listOf("Online contributor"),
-            searchKeyword = listOf("androidx.google.example1"),
-        ),
-        GeneralRule(
-            id = 2,
-            name = "Android WorkerManager",
-            iconUrl = null,
-            company = "Google",
-            description = "WorkManager is the recommended solution for persistent work. " +
-                "Work is persistent when it remains scheduled through app restarts and " +
-                "system reboots. Because most background processing is best accomplished " +
-                "through persistent work, WorkManager is the primary recommended API for " +
-                "background processing.",
-            sideEffect = "Background works won't be able to execute",
-            safeToBlock = false,
-            contributors = listOf("Google"),
-            searchKeyword = listOf(
-                "androidx.google.example1",
-                "androidx.google.example2",
-                "androidx.google.example3",
-                "androidx.google.example4",
-            ),
-        ),
-    )
+fun GeneralRuleScreenPreview(
+    @PreviewParameter(RuleListPreviewParameterProvider::class)
+    ruleList: List<GeneralRule>,
+) {
     BlockerTheme {
-        GeneralRulesList(rules = ruleList)
+        Surface {
+            GeneralRulesList(rules = ruleList)
+        }
     }
 }
