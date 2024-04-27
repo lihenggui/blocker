5cd26965250af96d5d54735670cff941ab574545
lihenggui <lihenggui@users.noreply.github.com>
2024-04-27 12:31:21 -0700

    Update translations
    Change-Id: I8749e88779c819bcd5d3aa8a991573b37507f40a

--- core/ui/src/main/kotlin/com/merxury/blocker/core/ui/rule/GeneralRulesList.kt
+++ core/ui/src/main/kotlin/com/merxury/blocker/core/ui/rule/GeneralRulesList.kt
@@ -64,7 +64,7 @@ fun GeneralRulesList(
         ) {
             if (matchedRules.isNotEmpty()) {
                 item {
-                    RuleItemHeader(title = stringResource(id = R.string.core_ui_matching_app_found))
+                    RuleItemHeader(title = stringResource(id = R.string.core_ui_matched_rules))
                 }
                 items(matchedRules, key = { it.id }) {
                     RuleItem(
@@ -80,7 +80,7 @@ fun GeneralRulesList(
             }
             if (unmatchedRules.isNotEmpty()) {
                 item {
-                    RuleItemHeader(title = stringResource(id = R.string.core_ui_not_matching_app_found))
+                    RuleItemHeader(title = stringResource(id = R.string.core_ui_unmatched_rules))
                 }
                 items(unmatchedRules, key = { it.id }) {
                     RuleItem(
