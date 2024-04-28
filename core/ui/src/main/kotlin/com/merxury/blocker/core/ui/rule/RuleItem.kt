50bf36eb755fa983f24dcc7aa820b5595dd5c583
juliewww <julie.www333@gmail.com>
2024-04-28 10:27:16 +0800

    Rewrite MatchedAppIcon

--- core/ui/src/main/kotlin/com/merxury/blocker/core/ui/rule/RuleItem.kt
+++ core/ui/src/main/kotlin/com/merxury/blocker/core/ui/rule/RuleItem.kt
@@ -18,7 +18,6 @@ package com.merxury.blocker.core.ui.rule
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -35,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -110,20 +110,17 @@ private fun RuleInfo(
 
 @Composable
 private fun MatchedAppIcon(matchedAppCount: Int) {
-    Box(
+    Text(
+        text = matchedAppCount.toString(),
+        textAlign = TextAlign.Center,
+        fontSize = 12.sp,
         modifier = Modifier
             .size(24.dp)
             .background(
                 color = MaterialTheme.colorScheme.secondaryContainer,
                 shape = CircleShape,
             ),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(
-            text = matchedAppCount.toString(),
-            fontSize = 12.sp,
-        )
-    }
+    )
 }
 
 @Composable
