bfa25d3923e4531db6dff2441f448520286f20be
lihenggui <350699171@qq.com>
2024-05-25 13:27:59 -0700

    Update module graph

--- generateModuleGraphs.sh
+++ generateModuleGraphs.sh
@@ -34,14 +34,14 @@ fi
 # Check for a version of grep which supports Perl regex.
 # On MacOS the OS installed grep doesn't support Perl regex so check for the existence of the
 # GNU version instead which is prefixed with 'g' to distinguish it from the OS installed version.
-    if grep -P "" /dev/null > /dev/null 2>&1; then
+if grep -P "" /dev/null > /dev/null 2>&1; then
     GREP_COMMAND=grep
 elif command -v ggrep &> /dev/null; then
     GREP_COMMAND=ggrep
 else
     echo "You don't have a version of 'grep' installed which supports Perl regular expressions."
     echo "On MacOS you can install one using Homebrew with the command: 'brew install grep'"
-    exit 1
+    GREP_COMMAND=grep
 fi
 
 # Initialize an array to store excluded modules
