9e92011f65e2b328b8fde3f790a827d7721c4013
lihenggui <350699171@qq.com>
2024-06-01 19:00:00 -0700

    Bump version code

--- app-compose/build.gradle.kts
+++ app-compose/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     namespace = "com.merxury.blocker"
     defaultConfig {
         applicationId = "com.merxury.blocker"
-        versionCode = 4369
-        versionName = "2.0.4369-fdroid" // X.Y.Z; X = Major, Y = minor, Z = version code
+        versionCode = 4373
+        versionName = "2.0.4373-fdroid" // X.Y.Z; X = Major, Y = minor, Z = version code
 
         // Custom test runner to set up Hilt dependency graph
         testInstrumentationRunner = "com.merxury.blocker.core.testing.BlockerTestRunner"
