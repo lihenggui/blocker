352f70f0b5e20938b91b901380978b1cb84a1972
lihenggui <350699171@qq.com>
2024-06-23 22:04:11 -0700

    Remove unnecessary SuppressLint

--- core/designsystem/src/main/kotlin/com/merxury/blocker/core/designsystem/component/scrollbar/AppScrollbars.kt
+++ core/designsystem/src/main/kotlin/com/merxury/blocker/core/designsystem/component/scrollbar/AppScrollbars.kt
@@ -16,7 +16,6 @@
 
 package com.merxury.blocker.core.designsystem.component.scrollbar
 
-import android.annotation.SuppressLint
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.SpringSpec
@@ -165,9 +164,6 @@ private fun ScrollableState.DecorativeScrollbarThumb(
     )
 }
 
-// TODO: This lint is removed in 1.6 as the recommendation has changed
-// remove when project is upgraded
-@SuppressLint("ComposableModifierFactory")
 @Composable
 private fun Modifier.scrollThumb(
     scrollableState: ScrollableState,
