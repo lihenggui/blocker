ad1d10f369b3398a49c55dd063877b6d31bb626d
lihenggui <350699171@qq.com>
2024-07-11 22:41:21 -0700

    Add Licensee plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.licensee) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.firebase.perf) apply false
     alias(libs.plugins.gms) apply false
