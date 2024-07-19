ec6895aee80a76344b8e877b5d4e02ea822d04d6
juliewww <julie.www333@gmail.com>
2024-07-19 11:39:18 +0800

    Update LicenseScreen

--- feature/settings/src/main/kotlin/com/merxury/blocker/feature/licenses/LicensesScreen.kt
+++ feature/settings/src/main/kotlin/com/merxury/blocker/feature/licenses/LicensesScreen.kt
@@ -16,28 +16,47 @@
 
 package com.merxury.blocker.feature.licenses
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.merxury.blocker.core.designsystem.component.BlockerTopAppBar
 import com.merxury.blocker.core.designsystem.component.SnackbarHostState
+import com.merxury.blocker.core.designsystem.component.scrollbar.DraggableScrollbar
+import com.merxury.blocker.core.designsystem.component.scrollbar.rememberDraggableScroller
+import com.merxury.blocker.core.designsystem.component.scrollbar.scrollbarState
 import com.merxury.blocker.core.designsystem.theme.BlockerTheme
+import com.merxury.blocker.core.model.data.LicenseGroup
+import com.merxury.blocker.core.ui.BlockerSettingItem
+import com.merxury.blocker.core.ui.ItemHeader
 import com.merxury.blocker.core.ui.PreviewDevices
+import com.merxury.blocker.core.ui.TrackScrollJank
+import com.merxury.blocker.core.ui.previewparameter.LicensesPreviewParameterProvider
 import com.merxury.blocker.core.ui.screen.LoadingScreen
 import com.merxury.blocker.feature.licenses.LicensesUiState.Loading
 import com.merxury.blocker.feature.licenses.LicensesUiState.Success
@@ -92,22 +111,76 @@ fun LicenseScreen(
                 }
 
                 is Success -> {
-                    LicenseContent(uiState)
+                    LicenseContent(uiState.licenses)
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun LicenseContent(
-    uiState: LicensesUiState,
+    licenses: List<LicenseGroup>,
+    modifier: Modifier = Modifier,
 ) {
+    val listState = rememberLazyListState()
+    val scrollbarState = listState.scrollbarState(
+        itemsAvailable = licenses.size,
+    )
+    TrackScrollJank(scrollableState = listState, stateName = "app:list")
+    Box(modifier.fillMaxSize()) {
+        LazyColumn(state = listState) {
+            licenses.forEach { group ->
+                stickyHeader {
+                    ItemHeader(
+                        title = group.id,
+                        modifier = Modifier.fillMaxSize(),
+                    )
+                }
+                items(group.artifacts) { artifact ->
+                    BlockerSettingItem(
+                        title = (artifact.name ?: artifact.artifactId),
+                        itemDes = "${artifact.artifactId} v${artifact.version}",
+                        itemDes1 = artifact.spdxLicenses?.joinToString { it.name },
+                    )
+                }
+            }
+        }
+        listState.DraggableScrollbar(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(horizontal = 2.dp)
+                .align(Alignment.CenterEnd)
+                .testTag("appList:scrollbar"),
+            state = scrollbarState,
+            orientation = Orientation.Vertical,
+            onThumbMove = listState.rememberDraggableScroller(
+                itemsAvailable = licenses.size,
+            ),
+        )
+    }
 }
 
 @Composable
 @PreviewDevices
-private fun LicensesScreenPreview() {
+private fun LicensesScreenPreview(
+    @PreviewParameter(LicensesPreviewParameterProvider::class)
+    licenses: List<LicenseGroup>,
+) {
+
+    BlockerTheme {
+        Surface {
+            LicenseScreen(
+                uiState = Success(licenses),
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun LicensesScreenWithLoadingPreview() {
     BlockerTheme {
         Surface {
             LicenseScreen(
