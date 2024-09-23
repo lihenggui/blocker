c1606a709fdaa1bb0b79e4e76a7e6f0cfdcd1c7f
juliewww <julie.www333@gmail.com>
2024-09-23 13:18:29 +0800

    Fix ItemHeader color

--- core/ui/src/main/kotlin/com/merxury/blocker/core/ui/SettingItem.kt
+++ core/ui/src/main/kotlin/com/merxury/blocker/core/ui/SettingItem.kt
@@ -17,7 +17,6 @@
 package com.merxury.blocker.core.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -51,8 +50,7 @@ fun ItemHeader(
 ) {
     Row(
         modifier = modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.surface),
+            .fillMaxWidth(),
     ) {
         Text(
             text = title,
