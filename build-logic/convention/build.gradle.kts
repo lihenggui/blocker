2406d4c193ee15caaa41d61ee2253d31ee97dac6
lihenggui <lihenggui@users.noreply.github.com>
2024-09-30 15:59:35 -0700

    Use JDK 17 and toolchain
    Change-Id: Iceeaa71149f1f5a194f03d405b2459567ce255ba

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -23,14 +23,9 @@ plugins {
 
 group = "com.merxury.blocker.buildlogic"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
 kotlin {
     compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
+        jvmToolchain(17)
     }
 }
 
