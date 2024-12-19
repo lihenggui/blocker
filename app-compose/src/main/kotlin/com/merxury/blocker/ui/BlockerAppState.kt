e93a6fe11e89618575385abb721c6d6b1b6b4de7
lihenggui <lihenggui@users.noreply.github.com>
2024-12-19 11:24:39 -0800

    incorporate safe fallback destination to fix null destination causing UI flickering
    Change-Id: Ib4d057e9d82b5bb200d73b3cf00aa0fa869dae3a

--- app-compose/src/main/kotlin/com/merxury/blocker/ui/BlockerAppState.kt
+++ app-compose/src/main/kotlin/com/merxury/blocker/ui/BlockerAppState.kt
@@ -22,6 +22,8 @@ import androidx.compose.material.navigation.rememberBottomSheetNavigator
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.Lifecycle
@@ -91,9 +93,21 @@ class BlockerAppState(
     permissionMonitor: PermissionMonitor,
     timeZoneMonitor: TimeZoneMonitor,
 ) {
+    private val previousDestination = mutableStateOf<NavDestination?>(null)
+
     val currentDestination: NavDestination?
-        @Composable get() = navController
-            .currentBackStackEntryAsState().value?.destination
+        @Composable get() {
+            // Collect the currentBackStackEntryFlow as a state
+            val currentEntry = navController.currentBackStackEntryFlow
+                .collectAsState(initial = null)
+
+            // Fallback to previousDestination if currentEntry is null
+            return currentEntry.value?.destination.also { destination ->
+                if (destination != null) {
+                    previousDestination.value = destination
+                }
+            } ?: previousDestination.value
+        }
 
     val isOffline = networkMonitor.isOnline
         .map(Boolean::not)
