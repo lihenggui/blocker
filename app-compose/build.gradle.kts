4ca7ce08cf2ca7e96f11cbf3719a541da1eb9b3f
juliewww <julie.www333@gmail.com>
2025-03-18 15:47:22 +0800

    Delete sort module

--- app-compose/build.gradle.kts
+++ app-compose/build.gradle.kts
@@ -98,7 +98,6 @@ dependencies {
     implementation(projects.feature.ruledetail)
     implementation(projects.feature.search)
     implementation(projects.feature.settings)
-    implementation(projects.feature.sort)
 
     implementation(projects.core.analytics)
     implementation(projects.core.common)
