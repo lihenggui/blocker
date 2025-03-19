8dd8ee631a5e563e0a6d17c8817b4d27da25fa11
juliewww <julie.www333@gmail.com>
2025-03-19 14:06:55 +0800

    Remove scrimColor property from ModalBottomSheet in AppSortBottomSheet and ComponentSortBottomSheet

--- feature/appdetail/src/main/kotlin/com/merxury/blocker/feature/appdetail/bottomsheet/ComponentSortBottomSheet.kt
+++ feature/appdetail/src/main/kotlin/com/merxury/blocker/feature/appdetail/bottomsheet/ComponentSortBottomSheet.kt
@@ -31,7 +31,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -73,7 +72,6 @@ fun ComponentSortBottomSheetRoute(
         onDismissRequest = { dismissHandler() },
         sheetState = bottomSheetState,
         containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        scrimColor = Color.Transparent,
     ) {
         ComponentSortBottomSheet(
             uiState = uiState,
--- feature/applist/src/main/kotlin/com/merxury/blocker/feature/applist/component/AppSortBottomSheet.kt
+++ feature/applist/src/main/kotlin/com/merxury/blocker/feature/applist/component/AppSortBottomSheet.kt
@@ -31,7 +31,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -72,7 +71,6 @@ fun AppSortBottomSheetRoute(
     ModalBottomSheet(
         onDismissRequest = { dismissHandler() },
         sheetState = bottomSheetState,
-        scrimColor = Color.Transparent,
     ) {
         ComponentSortBottomSheet(
             uiState = uiState,
