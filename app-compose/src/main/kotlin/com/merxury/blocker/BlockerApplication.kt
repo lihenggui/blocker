ee69d9b5a3bc2e751fbc25af3c8fd510ad1bfe4e
Mercury Li <contact@mercury.li>
2025-07-29 12:58:53 -0700

    Remove death penalty from strict mode violations
    Change-Id: I3a90abe09b6009ae15bb50b6cf3a4d244c4397e0

--- app-compose/src/main/kotlin/com/merxury/blocker/BlockerApplication.kt
+++ app-compose/src/main/kotlin/com/merxury/blocker/BlockerApplication.kt
@@ -96,7 +96,6 @@ class BlockerApplication :
                         detectCustomSlowCalls()
                         detectNetwork()
                         penaltyLog()
-                        penaltyDeath()
                     }
                     .build(),
             )
