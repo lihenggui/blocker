8e7e37f36588d7ba4bcf5f9459e77113f5c64a84
Mercury Li <contact@mercury.li>
2025-10-31 13:06:34 -0700

    Add share filter module in features namespace
    Change-Id: If91cd85537dfd54b8d72e973407f0f50bc2f7a0a

---
+++ feature/sharefilter/src/main/kotlin/com/merxury/blocker/feature/sharefilter/ShareFilterScreen.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2025 Blocker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.merxury.blocker.feature.sharefilter
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.merxury.blocker.core.designsystem.component.SnackbarHostState
+
+@Composable
+internal fun ShareFilterRoute(
+    snackbarHostState: SnackbarHostState,
+    modifier: Modifier = Modifier,
+    viewModel: ShareFilterViewModel = hiltViewModel(),
+) {
+    ShareFilterScreen(
+        snackbarHostState = snackbarHostState,
+        modifier = modifier,
+    )
+}
+
+@Composable
+internal fun ShareFilterScreen(
+    snackbarHostState: SnackbarHostState,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = stringResource(R.string.feature_sharefilter_title))
+    }
+}
