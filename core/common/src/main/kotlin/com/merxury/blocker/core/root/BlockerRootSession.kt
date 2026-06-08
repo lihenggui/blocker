87fec2201113c00d4355d3f0659ea8799832b1d5
Mygod <contact-git@mygod.be>
2026-06-08 00:46:29 -0400

    Port root init throwable logging
    Change-Id: Id07418e4a96b0b2d42fd1ffda3ae77004b64a37b

--- core/common/src/main/kotlin/com/merxury/blocker/core/root/BlockerRootSession.kt
+++ core/common/src/main/kotlin/com/merxury/blocker/core/root/BlockerRootSession.kt
@@ -16,17 +16,30 @@
 
 package com.merxury.blocker.core.root
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.ParcelFileDescriptor
+import android.os.Parcelable
+import android.util.Log
 import be.mygod.librootkotlinx.Logger
+import be.mygod.librootkotlinx.ParcelableThrowable
+import be.mygod.librootkotlinx.RootFlow
 import be.mygod.librootkotlinx.RootServer
 import be.mygod.librootkotlinx.RootSession
 import be.mygod.librootkotlinx.io.awaitExit
 import com.merxury.blocker.core.di.ApplicationScope
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.parcelize.Parcelize
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -39,6 +52,66 @@ class BlockerRootSession @Inject constructor(
     @ApplicationScope private val applicationScope: CoroutineScope,
 ) : RootSession(),
     Logger {
+    @Parcelize
+    sealed class RootInitEvent : Parcelable {
+        @Parcelize
+        class Initialized : RootInitEvent()
+
+        @Parcelize
+        data class LoggedThrowable(
+            val priority: Int,
+            val throwable: ParcelableThrowable,
+        ) : RootInitEvent()
+    }
+
+    @Parcelize
+    class RootInit : RootFlow<RootInitEvent> {
+        override fun flow() = callbackFlow {
+            val channel = Channel<Pair<Int, Throwable>>(Channel.CONFLATED) { (priority, throwable) ->
+                if (priority >= Log.WARN) {
+                    throwable.printStackTrace(System.err)
+                }
+            }
+            var logThrowable = { priority: Int, throwable: Throwable ->
+                channel.trySend(priority to throwable).exceptionOrNull()?.printStackTrace(System.err)
+            }
+            Timber.plant(
+                object : Timber.DebugTree() {
+                    @SuppressLint("LogNotTimber")
+                    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                        if (priority >= Log.WARN) {
+                            System.err.println("$priority/$tag: $message")
+                            t?.printStackTrace()
+                        }
+                        if (t == null) {
+                            Log.println(priority, tag, message)
+                        } else {
+                            Log.println(priority, tag, message)
+                            Log.d(tag, message, t)
+                            logThrowable(priority, t)
+                        }
+                    }
+                },
+            )
+            Logger.me = RootLogger
+            val job = launch {
+                for ((priority, throwable) in channel) {
+                    send(RootInitEvent.LoggedThrowable(priority, ParcelableThrowable(throwable)))
+                }
+            }
+            send(RootInitEvent.Initialized())
+            awaitClose {
+                logThrowable = { priority, throwable ->
+                    if (priority >= Log.WARN) {
+                        throwable.printStackTrace(System.err)
+                    }
+                }
+                channel.close()
+                job.cancel()
+            }
+        }.buffer(Channel.UNLIMITED)
+    }
+
     override val context: Context
         get() = appContext
 
@@ -81,5 +154,41 @@ class BlockerRootSession @Inject constructor(
     override suspend fun initServer(server: RootServer) {
         Logger.me = this
         super.initServer(server)
+        val initialized = CompletableDeferred<Unit>()
+        applicationScope.launch(start = CoroutineStart.UNDISPATCHED) {
+            try {
+                server.flow(RootInit()).collect { event ->
+                    when (event) {
+                        is RootInitEvent.Initialized -> initialized.complete(Unit)
+                        is RootInitEvent.LoggedThrowable -> {
+                            Timber.tag(ROOT_REMOTE_TAG).log(event.priority, event.throwable.unwrap())
+                        }
+                    }
+                }
+                initialized.complete(Unit)
+            } catch (e: CancellationException) {
+                if (!initialized.isCompleted) {
+                    initialized.cancel(e)
+                }
+            } catch (e: Exception) {
+                if (!initialized.isCompleted) {
+                    initialized.completeExceptionally(e)
+                } else {
+                    Timber.w(e)
+                }
+            }
+        }
+        initialized.await()
+    }
+
+    private object RootLogger : Logger {
+        override fun d(m: String?, t: Throwable?) = Timber.d(t, m)
+        override fun e(m: String?, t: Throwable?) = Timber.e(t, m)
+        override fun i(m: String?, t: Throwable?) = Timber.i(t, m)
+        override fun w(m: String?, t: Throwable?) = Timber.w(t, m)
+    }
+
+    private companion object {
+        const val ROOT_REMOTE_TAG = "RootRemote"
     }
 }
