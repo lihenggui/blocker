073a9fc9c79500b5cf95029cf28f0195c6d44352
Mygod <contact-git@mygod.be>
2026-06-08 00:30:29 -0400

    Update librootkotlinx to alpha11
    Change-Id: If643595dc42688474390b865aedbb4243578f8d1

--- core/common/src/main/kotlin/com/merxury/blocker/core/root/BlockerRootSession.kt
+++ core/common/src/main/kotlin/com/merxury/blocker/core/root/BlockerRootSession.kt
@@ -22,14 +22,21 @@ import be.mygod.librootkotlinx.Logger
 import be.mygod.librootkotlinx.RootServer
 import be.mygod.librootkotlinx.RootSession
 import be.mygod.librootkotlinx.io.awaitExit
+import com.merxury.blocker.core.di.ApplicationScope
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.time.Duration.Companion.seconds
 
 @Singleton
 class BlockerRootSession @Inject constructor(
     @ApplicationContext private val appContext: Context,
+    @ApplicationScope private val applicationScope: CoroutineScope,
 ) : RootSession(),
     Logger {
     override val context: Context
@@ -40,16 +47,34 @@ class BlockerRootSession @Inject constructor(
     override fun i(m: String?, t: Throwable?) = Timber.i(t, m)
     override fun w(m: String?, t: Throwable?) = Timber.w(t, m)
 
+    override val rootLifecycleCoroutineContext get() = EmptyCoroutineContext
+
     override suspend fun handleRootLifecycle(
         process: Process,
         stdin: ParcelFileDescriptor,
         stdout: ParcelFileDescriptor,
         stderr: ParcelFileDescriptor,
-    ) {
+    ) = try {
         super.handleRootLifecycle(process, stdin, stdout, stderr)
-        val exit = process.awaitExit()
-        if (exit != 0) {
-            Timber.w("Root JVM unexpectedly exited with $exit")
+    } finally {
+        applicationScope.launch {
+            var exit = withTimeoutOrNull(10.seconds) { process.awaitExit() }
+            if (exit == null) {
+                process.destroy()
+                exit = withTimeoutOrNull(5.seconds) { process.awaitExit() }
+                Timber.w(
+                    Exception(
+                        if (exit == null) {
+                            process.destroyForcibly()
+                            "Root JVM refused to exit"
+                        } else {
+                            "Root JVM exited with $exit and timeout"
+                        },
+                    ),
+                )
+            } else if (exit != 0) {
+                Timber.w(Exception("Root JVM unexpectedly exited with $exit"))
+            }
         }
     }
 
